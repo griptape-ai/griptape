de803c3de2d10075c0360d2b2e54448daf99ffe6
Vasily Vasinov <des.elyon@gmail.com>
2023-01-23 09:56:16 -0700

    Add meta.speculate rule

--- galaxybrain/rules/meta.py
+++ galaxybrain/rules/meta.py
@@ -8,6 +8,14 @@ def be_truthful() -> Rule:
     )
 
 
+def speculate() -> Rule:
+    return Rule(
+        "say \"I don't know\" if you don't know the answer to the question but also be creative and speculate what the "
+        "possible answer could be",
+        validator=lambda result: True
+    )
+
+
 def your_name_is(name: str) -> Rule:
     return Rule(
         f"you are a chat bot that responds to name \"{name}\"",
