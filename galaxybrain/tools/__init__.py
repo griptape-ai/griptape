2f14f9401110e44ebd8917393ac0c7e61c62eb68
Vasily Vasinov <des.elyon@gmail.com>
2023-02-22 16:30:53 -0700

    Add the Wiki tool

--- galaxybrain/tools/__init__.py
+++ galaxybrain/tools/__init__.py
@@ -1,10 +1,12 @@
 from galaxybrain.tools.tool import Tool
 from galaxybrain.tools.calculator import Calculator
 from galaxybrain.tools.data_scientist import DataScientist
+from galaxybrain.tools.wiki import Wiki
 
 
 __all__ = [
     "Tool",
     "Calculator",
-    "DataScientist"
+    "DataScientist",
+    "Wiki"
 ]
