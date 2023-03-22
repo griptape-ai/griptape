ce0f47fef75607d773bd036526597fbf15012699
Vasily Vasinov <des.elyon@gmail.com>
2023-03-22 15:34:51 -0600

    Add myst_parser to docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ author = 'Vasily Vasinov'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["myst_parser"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
