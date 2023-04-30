00e054a22433a7a57584245015b1b004e31ddee3
Collin Dutter <collindutter@gmail.com>
2023-04-29 22:13:28 -0700

    feat: add dynamodb_driver

---
+++ griptape/drivers/storage/dynamodb_driver.py
@@ -0,0 +1,46 @@
+
+from uuid import uuid4
+from typing import Optional
+from boto3 import resource
+from griptape.drivers import BaseStorageDriver
+from attr import define, field
+
+
+@define
+class DynamoDBDriver(BaseStorageDriver):
+    aws_region: str = field(default=None, kw_only=True)
+    table_name: str = field(default=None, kw_only=True)
+    primary_key: str = field(default=None, kw_only=True)
+    value_attribute_key: str = field(default=None, kw_only=True)
+    extra_attributes: any = field(default={}, kw_only=True)
+
+    table: any = field(init=False)
+
+    def __attrs_post_init__(self):
+        dynamodb = resource(
+            "dynamodb",
+            region_name=self.aws_region,
+        )
+        table = dynamodb.Table(self.table_name)
+        self.table = table
+
+    def save(self, value: any) -> str:
+        key = uuid4().hex
+        self.table.put_item(
+            Item={
+                self.primary_key: key,
+                self.value_attribute_key: value,
+                **self.extra_attributes,
+            }
+        )
+
+        return key
+
+    def load(self, key: str) -> Optional[any]:
+        response = self.table.get_item(Key={"deviceId": key})
+        if "Item" in response:
+            return response["Item"]["a.value"]
+        return None
+
+    def delete(self, key: str) -> None:
+        return self.table.delete_item(Key={"deviceId": key})
