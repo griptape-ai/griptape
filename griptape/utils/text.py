611abeb5d005a1f8d5ea6f1b607b463e5bb18bcf
Vasily Vasinov <des.elyon@gmail.com>
2023-04-30 13:01:21 -0600

    In to_vector_index explicitly pass LLMPredictor

--- griptape/utils/text.py
+++ griptape/utils/text.py
@@ -1,13 +1,16 @@
-from __future__ import annotations
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from llama_index import GPTSimpleVectorIndex
+from decouple import config
+from langchain import OpenAI
+from llama_index import GPTSimpleVectorIndex, LLMPredictor
 
 
 def to_vector_index(text: str) -> GPTSimpleVectorIndex:
     from llama_index import GPTSimpleVectorIndex, Document
 
-    return GPTSimpleVectorIndex([
-        Document(text)
-    ])
+    return GPTSimpleVectorIndex(
+        [Document(text)],
+        llm_predictor=LLMPredictor(
+            llm=OpenAI(
+                openai_api_key=config("OPENAI_API_KEY")
+            )
+        )
+    )
--- tests/unit/utils/test_text.py
+++ tests/unit/utils/test_text.py
@@ -2,6 +2,6 @@ from llama_index import GPTSimpleVectorIndex
 from griptape.utils.text import to_vector_index
 
 
-class TestText():
+class TestText:
     def test_to_vector_index(self):
         assert isinstance(to_vector_index("foobar"), GPTSimpleVectorIndex)
