4f86608dc9c2eda173dfdc9e332222f5740341d4
Vasily Vasinov <des.elyon@gmail.com>
2023-05-08 13:53:42 -0600

    Remove Converters

--- griptape/converters/__init__.py
+++
@@ -1,9 +0,0 @@
-from .base_converter import BaseConverter
-from .langchain_tool_converter import LangchainToolConverter
-from .chatgpt_plugin_converter import ChatgptPluginConverter
-
-__all__ = [
-    "BaseConverter",
-    "LangchainToolConverter",
-    "ChatgptPluginConverter"
-]
--- griptape/converters/base_converter.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC
-from attr import define, field
-from griptape.executors import BaseExecutor, LocalExecutor
-
-
-@define
-class BaseConverter(ABC):
-    executor: BaseExecutor = field(default=LocalExecutor(), kw_only=True)
