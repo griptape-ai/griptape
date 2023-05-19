c93f1dd7ac040771036cdbb5fde860626bd6204e
Vasily Vasinov <des.elyon@gmail.com>
2023-05-19 10:13:05 +0300

    Revert "Remove PineconeVectorStorageDriver fixture in unit tests"
    This reverts commit e8d586eedc6488c5ac0cf262ace236d92ceedf59.

--- tests/unit/drivers/storage/vector/test_pinecone_vector_storage_driver.py
+++ tests/unit/drivers/storage/vector/test_pinecone_vector_storage_driver.py
@@ -4,11 +4,6 @@ from griptape.drivers import PineconeVectorStorageDriver
 
 
 class TestOpenAiEmbeddingDriver:
-    """
-    Don't move the driver into a fixture. There is something wrong with how it gets
-    instantiated, which results in GitHub test actions to hang forever.
-    """
-
     @pytest.fixture(autouse=True)
     def mock_openai_embedding_create(self, mocker):
         # Create a fake response
@@ -30,47 +25,29 @@ class TestOpenAiEmbeddingDriver:
         mocker.patch('pinecone.Index.query', return_value=fake_query_response)
         mocker.patch('pinecone.create_index', return_value=None)
 
-    def test_insert_test_artifact(self):
-        driver = PineconeVectorStorageDriver(
+    @pytest.fixture
+    def driver(self):
+        return PineconeVectorStorageDriver(
             api_key="foobar",
             index_name="test"
         )
-        
+
+    def test_insert_test_artifact(self, driver):
         assert driver.insert_text_artifact(
             TextArtifact("foo"),
             vector_id="foo"
         ) == "foo"
 
-    def test_insert_vector(self):
-        driver = PineconeVectorStorageDriver(
-            api_key="foobar",
-            index_name="test"
-        )
-
+    def test_insert_vector(self, driver):
         assert driver.insert_vector([0, 1, 2], vector_id="foo") == "foo"
         assert isinstance(driver.insert_vector([0, 1, 2]), str)
 
-    def test_insert_text(self):
-        driver = PineconeVectorStorageDriver(
-            api_key="foobar",
-            index_name="test"
-        )
-
+    def test_insert_text(self, driver):
         assert driver.insert_text("foo", vector_id="foo") == "foo"
         assert isinstance(driver.insert_text("foo"), str)
 
-    def test_query(self):
-        driver = PineconeVectorStorageDriver(
-            api_key="foobar",
-            index_name="test"
-        )
-
+    def test_query(self, driver):
         assert driver.query("test")[0].vector == [0, 1, 0]
 
-    def test_create_index(self):
-        driver = PineconeVectorStorageDriver(
-            api_key="foobar",
-            index_name="test"
-        )
-
+    def test_create_index(self, driver):
         assert driver.create_index("test") is None
