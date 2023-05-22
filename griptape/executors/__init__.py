72050daeb8c3d82c8a87755d6183c198767310ab
Collin Dutter <collindutter@gmail.com>
2023-05-22 11:39:23 -0700

    refactor: delete lambda_executor

--- griptape/executors/__init__.py
+++ griptape/executors/__init__.py
@@ -1,11 +1,9 @@
 from .base_executor import BaseExecutor
 from .local_executor import LocalExecutor
 from .docker_executor import DockerExecutor
-from .lambda_executor import LambdaExecutor
 
 __all__ = [
     "BaseExecutor",
     "LocalExecutor",
     "DockerExecutor",
-    "LambdaExecutor"
 ]
