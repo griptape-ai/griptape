9ef208782efe73f1d382aca7997623235544b16c
Vasily Vasinov <des.elyon@gmail.com>
2023-05-22 19:22:34 +0200

    Add chunker fixture to TestTextChunker

--- tests/unit/chunkers/test_text_chunker.py
+++ tests/unit/chunkers/test_text_chunker.py
@@ -1,36 +1,43 @@
+import pytest
+
 from griptape.chunkers import TextChunker
 from griptape.tokenizers import BaseTokenizer
 
 
-def gen_paragraph(max_tokens: int, tokenizer: BaseTokenizer, sentence_separator: str) -> str:
+def gen_paragraph(MAX_TOKENS: int, tokenizer: BaseTokenizer, sentence_separator: str) -> str:
     all_text = ""
     word = "foo"
     index = 0
     add_word = lambda base, w, i: sentence_separator.join([base, f"{w}-{i}"])
 
-    while max_tokens >= tokenizer.token_count(add_word(all_text, word, index)):
+    while MAX_TOKENS >= tokenizer.token_count(add_word(all_text, word, index)):
         all_text = f"{word}-{index}" if all_text == "" else add_word(all_text, word, index)
         index += 1
 
     return all_text + sentence_separator
 
 
-class TestTextChunker:
-    def test_small_chunks(self):
-        max_tokens = 50
-        chunker = TextChunker(
-            max_tokens_per_chunk=max_tokens
+MAX_TOKENS = 50
+
+
+class TestTextChunker:    
+    @pytest.fixture
+    def chunker(self):        
+        return TextChunker(
+            max_tokens_per_chunk=MAX_TOKENS
         )
+    
+    def test_small_chunks(self, chunker):
         text = [
-            gen_paragraph(max_tokens, chunker.tokenizer, "? "),
+            gen_paragraph(MAX_TOKENS, chunker.tokenizer, "? "),
             "\n\n",
-            gen_paragraph(max_tokens, chunker.tokenizer, ". ")
+            gen_paragraph(MAX_TOKENS, chunker.tokenizer, ". ")
         ]
         chunks = chunker.chunk("".join(text))
 
         assert len(chunks) == 2
-        assert chunker.tokenizer.token_count(chunks[0]) <= max_tokens
-        assert chunker.tokenizer.token_count(chunks[1]) <= max_tokens
+        assert chunker.tokenizer.token_count(chunks[0]) <= MAX_TOKENS
+        assert chunker.tokenizer.token_count(chunks[1]) <= MAX_TOKENS
 
         assert chunks[0].startswith("foo-0?")
         assert chunks[1].startswith("foo-0.")
@@ -38,23 +45,19 @@ class TestTextChunker:
         assert chunks[0].endswith("? foo-11?")
         assert chunks[1].endswith(". foo-11.")
 
-    def test_large_chunks(self):
-        max_tokens = 50
-        chunker = TextChunker(
-            max_tokens_per_chunk=max_tokens
-        )
+    def test_large_chunks(self, chunker):
         text = [
-            gen_paragraph(max_tokens * 2, chunker.tokenizer, "! "),
+            gen_paragraph(MAX_TOKENS * 2, chunker.tokenizer, "! "),
             "\n\n",
-            gen_paragraph(max_tokens, chunker.tokenizer, ". ")
+            gen_paragraph(MAX_TOKENS, chunker.tokenizer, ". ")
         ]
         chunks = chunker.chunk("".join(text))
 
         assert len(chunks) == 4
-        assert chunker.tokenizer.token_count(chunks[0]) <= max_tokens
-        assert chunker.tokenizer.token_count(chunks[1]) <= max_tokens
-        assert chunker.tokenizer.token_count(chunks[2]) <= max_tokens
-        assert chunker.tokenizer.token_count(chunks[3]) <= max_tokens
+        assert chunker.tokenizer.token_count(chunks[0]) <= MAX_TOKENS
+        assert chunker.tokenizer.token_count(chunks[1]) <= MAX_TOKENS
+        assert chunker.tokenizer.token_count(chunks[2]) <= MAX_TOKENS
+        assert chunker.tokenizer.token_count(chunks[3]) <= MAX_TOKENS
 
         assert chunks[0].startswith("foo-0!")
         assert chunks[1].startswith("foo-10!")
@@ -66,31 +69,27 @@ class TestTextChunker:
         assert chunks[2].endswith("! foo-24!")
         assert chunks[3].endswith(". foo-11.")
 
-    def test_separators(self):
-        max_tokens = 50
-        chunker = TextChunker(
-            max_tokens_per_chunk=max_tokens
-        )
+    def test_separators(self, chunker):
         text = [
-            gen_paragraph(max_tokens * 2, chunker.tokenizer, "! "),
+            gen_paragraph(MAX_TOKENS * 2, chunker.tokenizer, "! "),
             "\n\n",
-            gen_paragraph(max_tokens, chunker.tokenizer, ". "),
+            gen_paragraph(MAX_TOKENS, chunker.tokenizer, ". "),
             "\n",
-            gen_paragraph(max_tokens + 1, chunker.tokenizer, "? "),
+            gen_paragraph(MAX_TOKENS + 1, chunker.tokenizer, "? "),
             "\n\n",
-            gen_paragraph(max_tokens + 1, chunker.tokenizer, " ")
+            gen_paragraph(MAX_TOKENS + 1, chunker.tokenizer, " ")
         ]
         chunks = chunker.chunk("".join(text))
 
         assert len(chunks) == 7
 
-        assert chunker.tokenizer.token_count(chunks[0]) <= max_tokens
-        assert chunker.tokenizer.token_count(chunks[1]) <= max_tokens
-        assert chunker.tokenizer.token_count(chunks[2]) <= max_tokens
-        assert chunker.tokenizer.token_count(chunks[3]) <= max_tokens
-        assert chunker.tokenizer.token_count(chunks[4]) <= max_tokens
-        assert chunker.tokenizer.token_count(chunks[5]) <= max_tokens
-        assert chunker.tokenizer.token_count(chunks[6]) <= max_tokens
+        assert chunker.tokenizer.token_count(chunks[0]) <= MAX_TOKENS
+        assert chunker.tokenizer.token_count(chunks[1]) <= MAX_TOKENS
+        assert chunker.tokenizer.token_count(chunks[2]) <= MAX_TOKENS
+        assert chunker.tokenizer.token_count(chunks[3]) <= MAX_TOKENS
+        assert chunker.tokenizer.token_count(chunks[4]) <= MAX_TOKENS
+        assert chunker.tokenizer.token_count(chunks[5]) <= MAX_TOKENS
+        assert chunker.tokenizer.token_count(chunks[6]) <= MAX_TOKENS
 
         assert chunks[0].startswith("foo-0!")
         assert chunks[1].startswith("foo-10!")
