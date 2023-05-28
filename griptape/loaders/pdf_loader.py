eaaca1e2fb76f9bd099534f831a1860093e574e3
Vasily Vasinov <des.elyon@gmail.com>
2023-05-28 10:48:21 -0700

    Add optional password to PdfLoader

--- griptape/loaders/pdf_loader.py
+++ griptape/loaders/pdf_loader.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Union, IO
+from typing import Union, IO, Optional
 from PyPDF2 import PdfReader
 from attr import define
 from griptape.artifacts import ListArtifact
@@ -8,8 +8,8 @@ from griptape.loaders import TextLoader
 
 @define
 class PdfLoader(TextLoader):
-    def load(self, stream: Union[str, IO, Path]) -> ListArtifact:
-        reader = PdfReader(stream)
+    def load(self, stream: Union[str, IO, Path], password: Optional[str] = None) -> ListArtifact:
+        reader = PdfReader(stream, password=password)
         text = "".join([p.extract_text() for p in reader.pages])
 
         return self.text_to_list_artifact(text)
