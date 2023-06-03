c3dc2e612a5e74e80afe6067ef2dda5f582ed9b1
Collin Dutter <collindutter@gmail.com>
2023-06-03 09:01:21 -0700

    feat: add claude prompt driver

---
+++ griptape/drivers/prompt/claude_prompt_driver.py
@@ -0,0 +1,33 @@
+from typing import Optional
+import anthropic
+from attr import define, field, Factory
+from griptape.artifacts import TextArtifact
+from griptape.drivers import BasePromptDriver
+from griptape.tokenizers import TiktokenTokenizer
+
+
+@define
+class ClaudePromptDriver(BasePromptDriver):
+    api_key: str = field(kw_only=True, metadata={"env": "CLAUDE_API_KEY"})
+
+    tokenizer: TiktokenTokenizer = field(
+        default=Factory(lambda self: TiktokenTokenizer(model=self.model), takes_self=True),
+        kw_only=True
+    )
+
+    def try_run(self, value: any) -> TextArtifact:
+        return self.__run_completion(value)
+
+    def __run_completion(self, value: str) -> TextArtifact:
+        client = anthropic.Client(self.api_key)
+
+        response = client.completion(
+            prompt=f"{anthropic.HUMAN_PROMPT}{value}{anthropic.AI_PROMPT}",
+            stop_sequences = [anthropic.HUMAN_PROMPT],
+            model=self.model,
+            max_tokens_to_sample=100,
+        )
+
+        return TextArtifact(
+            value=response['completion']
+        )
