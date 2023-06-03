2888e2b727c60b207c0b15a9a5ec552f67cde848
Collin Dutter <collindutter@gmail.com>
2023-06-03 12:48:52 -0700

    Simplify max token check

--- griptape/tokenizers/anthropic_tokenizer.py
+++ griptape/tokenizers/anthropic_tokenizer.py
@@ -7,29 +7,18 @@ from griptape.tokenizers import BaseTokenizer
 class AnthropicTokenizer(BaseTokenizer):
     DEFAULT_MODEL = "claude-v1"
     DEFAULT_MAX_TOKENS = 8000
-    MODEL_TO_MAX_TOKENS = {
-        "claude-v1": 8000,
-        "claude-v1-100k": 100000,
-        "claude-instant-v1": 8000,
-        "claude-instant-v1-100k": 100000,
-        "claude-v1.3": 8000,
-        "claude-v1.3-100k": 100000,
-        "claude-v1.2": 8000,
-        "claude-v1.0": 8000,
-        "claude-instant-v1.1": 8000,
-        "claude-instant-v1.1-100k": 100000,
-        "claude-instant-v1.0": 8000,
+    MODEL_SUFFIXES_TO_MAX_TOKENS = {
+        "100k": 100000,
     }
 
     model: str = field(default=DEFAULT_MODEL, kw_only=True)
 
     @property
     def max_tokens(self) -> int:
-        return (
-            self.MODEL_TO_MAX_TOKENS[self.model]
-            if self.model in self.MODEL_TO_MAX_TOKENS
-            else self.DEFAULT_MAX_TOKENS
-        )
+        for suffix, token_limit in self.MODEL_SUFFIXES_TO_MAX_TOKENS.items():
+            if self.model.endswith(suffix):
+                return token_limit
+        return self.DEFAULT_MAX_TOKENS
 
     def token_count(self, text: str) -> int:
         return len(self.encode(text))
