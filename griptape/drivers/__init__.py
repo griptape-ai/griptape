2d58dc103e9137d5d21fd02c908bfc0206199765
Collin Dutter <collindutter@gmail.com>
2023-06-08 16:34:03 -0700

    Relocate dynamodb conversation memory driver

--- griptape/drivers/__init__.py
+++ griptape/drivers/__init__.py
@@ -32,6 +32,7 @@ __all__ = [
 
     "BaseConversationMemoryDriver",
     "DiskConversationMemoryDriver",
+    "DynamoDbConversationMemoryDriver",
 
     "BaseBlobToolMemoryDriver",
     "MemoryBlobToolMemoryDriver",
