0219c30d6f25bc746646ed27c69bda1a31464a27
Stav Sapir <stavsap@gmail.com>
2023-07-07 14:40:55 +0300

    Add Prompt Driver for the text-generation-webui

--- griptape/drivers/__init__.py
+++ griptape/drivers/__init__.py
@@ -5,6 +5,7 @@ from .prompt.cohere_prompt_driver import CoherePromptDriver
 from .prompt.hugging_face_pipeline_prompt_driver import HuggingFacePipelinePromptDriver
 from .prompt.hugging_face_hub_prompt_driver import HuggingFaceHubPromptDriver
 from .prompt.anthropic_prompt_driver import AnthropicPromptDriver
+from .prompt.text_gen_driver import TextGenPromptDriver
 
 from .memory.conversation.base_conversation_memory_driver import BaseConversationMemoryDriver
 from .memory.conversation.local_conversation_memory_driver import LocalConversationMemoryDriver
@@ -33,6 +34,7 @@ __all__ = [
     "HuggingFacePipelinePromptDriver",
     "HuggingFaceHubPromptDriver",
     "AnthropicPromptDriver",
+    "TextGenPromptDriver",
 
     "BaseConversationMemoryDriver",
     "LocalConversationMemoryDriver",
