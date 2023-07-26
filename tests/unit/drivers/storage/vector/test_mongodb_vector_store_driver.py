8f7e3bf5d63095a3e87105de09743b00ba8b1440
Trisha Mandal <trishama@usc.edu>
2023-07-26 14:16:53 -0700

    fixess

--- tests/unit/drivers/storage/vector/test_mongodb_vector_store_driver.py
+++ tests/unit/drivers/storage/vector/test_mongodb_vector_store_driver.py
@@ -4,30 +4,36 @@ from griptape.artifacts import TextArtifact
 from griptape.drivers import MongoDbAtlasVectorStoreDriver
 from tests.mocks.mock_embedding_driver import MockEmbeddingDriver
 
+
 class TestMongoDbAtlasVectorStoreDriver:
 
     @pytest.fixture
     def driver(self, monkeypatch):
         monkeypatch.setattr('pymongo.MongoClient', mongomock.MongoClient)
         embedding_driver = MockEmbeddingDriver()
-        return MongoDbAtlasVectorStoreDriver(embedding_driver=embedding_driver)
+        return MongoDbAtlasVectorStoreDriver(
+            embedding_driver=embedding_driver,
+            connection_string="mongodb+srv://mock:mock@mockcluster.mongodb.net/test",  # Mock Connection String
+            database_name="test_database",  # Mock Database Name
+            collection_name="test_collection"  # Mock Collection Name
+        )
 
     def test_upsert_vector(self, driver):
         vector = [0.5, 0.5, 0.5]
-        vector_id = "123"
-        id = driver.upsert_vector(vector, vector_id=vector_id)
-        assert id is not None
+        vector_id_str = "123"
+        test_id = driver.upsert_vector(vector, vector_id=vector_id_str)
+        assert test_id is not None
 
     def test_upsert_text_artifact(self, driver):
         artifact = TextArtifact("foo")
-        id = driver.upsert_text_artifact(artifact)
-        assert id is not None
+        test_id = driver.upsert_text_artifact(artifact)
+        assert test_id is not None
 
     def test_upsert_text(self, driver):
         text = "foo"
-        vector_id = "foo"
-        id = driver.upsert_text(text, vector_id=vector_id)
-        assert id is not None
+        vector_id_str = "foo"
+        test_id = driver.upsert_text(text, vector_id=vector_id_str)
+        assert test_id is not None
 
     def test_query(self, driver):
         query = "test"
@@ -35,8 +41,8 @@ class TestMongoDbAtlasVectorStoreDriver:
         assert result is not None
 
     def test_load_entry(self, driver):
-        vector_id = "123"
-        result = driver.load_entry(vector_id)
+        vector_id_str = "123"
+        result = driver.load_entry(vector_id_str)
         assert result is not None
 
     def test_load_entries(self, driver):
