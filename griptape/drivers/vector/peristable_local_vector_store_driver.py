97e935ccf49d6f34f6393d718f4ce10e68b6e8ec
Tariq Baig-Meininghaus <tariq.baig@ds.mpg.de>
2023-07-26 23:16:53 +0200

    Implemented serialization to json / zip.
    Changed the serialisation method from pickle to json to avoid possible code injection on load.
    Made zipping dependent on provided filename, added filename validator. Refactored tests.
    
    Since the vector store uses dataclasses to store its entries, and those are not default json serializable I needed to add a simple json decoder that simply stores the dataclasses dict and a special key that allow to reconstruct the class on deserialization.
    
    Note: If the BaseVectorStore.Entry dataclass gains members that are not json serializable this will break

--- griptape/drivers/vector/peristable_local_vector_store_driver.py
+++ griptape/drivers/vector/peristable_local_vector_store_driver.py
@@ -1,31 +1,80 @@
+import os.path
 import pathlib
-import pickle
+import json
 from pathlib import Path
-from typing import Union
+from typing import Union, Literal, NewType, cast, Dict
+from zipfile import ZipFile
 
 from attr import define, field
-from griptape.drivers import LocalVectorStoreDriver
+from griptape.drivers import LocalVectorStoreDriver, BaseVectorStoreDriver
+
+import dataclasses, json
+
+
+class EntryAwareJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, BaseVectorStoreDriver.Entry):
+            d_dict = dataclasses.asdict(o)
+            d_dict["__Entry__"] = True
+            return d_dict
+        return super().default(o)
+
+
+def as_entry(dct: Dict):
+
+    if "__Entry__" in dct:
+        dct.pop("__Entry__")
+        return BaseVectorStoreDriver.Entry(**dct)
+
+    return dct
 
 
 @define
 class PersistableLocalVectorStoreDriver(LocalVectorStoreDriver):
     """
-    Version of the LocalVectorStoreDriver that allows to serialize to a local file using pickle.
+    Version of the LocalVectorStoreDriver that allows to serialize to a local file using either json or a zip containing
+    depending on the suffix of the given file path.
 
-    :param file_path: `pathlib.Path` or path as string. Intended location of the pickle.
+    :param file_path: `pathlib.Path` or path as string. Name and location of the save file. Extension must be zip or json,
+     if zip the generated file is compressed.
     """
 
-    file_path: Union[Path, str] = field(kw_only=True)
+    file_path: Union[Path, str] = field(kw_only=True, converter=Path)
+    file_is_zip: bool = field(init=False)
+
+    @file_path.validator
+    def _check_file_path(self, attribute, value):
+        if not (value.suffix == ".json" or value.suffix == ".zip"):
+            raise ValueError(
+                f"file must either be a json or a zip but file name is {self.file_path}"
+            )
+
+    def __attrs_post_init__(self):
+        self.file_is_zip = self.file_path.suffix == ".zip"
 
     def store(self, overwrite=False):
         """Serialize the entries of the vectorstore. Will not overwrite an existing file unless overwrite=True"""
-        with open(self.file_path, "xb" if not overwrite else "wb") as f:
-            pickle.dump(self.entries, f)
+
+        write_mode = cast(Literal["w", "x"], "x" if not overwrite else "w")
+
+        if self.file_is_zip:
+            with ZipFile(self.file_path, write_mode) as zf:
+                json_data = json.dumps(self.entries, cls=EntryAwareJSONEncoder)
+                zf.writestr("data.json", json_data)
+        else:
+            with open(self.file_path, write_mode) as f:
+                json.dump(self.entries, f, cls=EntryAwareJSONEncoder)
 
     def load(self) -> None:
         """Read entries that have been serialized using pickle. Will overwrite internal entries with the loaded ones"""
-        with open(self.file_path, "rb") as f:
-            self.entries = pickle.load(f)
+        if self.file_is_zip:
+            with ZipFile(self.file_path) as zf:
+                json_data = zf.read("data.json")
+                self.entries = json.loads(json_data, object_hook=as_entry)
+
+        else:
+            with open(self.file_path, "r") as f:
+                self.entries = json.load(f, object_hook=as_entry)
 
     @classmethod
     def from_saved(cls, file_path) -> "PersistableLocalVectorStoreDriver":
--- tests/unit/drivers/storage/vector/test_persitable_local_vector_store_driver.py
+++ tests/unit/drivers/storage/vector/test_persitable_local_vector_store_driver.py
@@ -1,15 +1,17 @@
 import tempfile
 
+import pytest
+
 from griptape.artifacts import TextArtifact
 from griptape.drivers import PersistableLocalVectorStoreDriver
 from tests.mocks.mock_embedding_driver import MockEmbeddingDriver
 
-
 class Tests:
-    def test_persistable_vector_store(self):
+    def t_persistable_vector_store(self,suffix : str):
         with tempfile.NamedTemporaryFile() as f:
+            fn = f.name+f".{suffix}"
             vector_store = PersistableLocalVectorStoreDriver(
-                file_path=f.name, embedding_driver=MockEmbeddingDriver()
+                file_path=fn, embedding_driver=MockEmbeddingDriver()
             )
             artifact = TextArtifact(value="Long live the queen!")
             vector_store.upsert_text_artifact(artifact)
@@ -19,8 +21,21 @@ class Tests:
             old_vector_store_entries = vector_store.entries
             del vector_store
             new_vector_store = PersistableLocalVectorStoreDriver.from_saved(
-                file_path=f.name
+                file_path=fn
             )
             k, v = old_vector_store_entries.popitem()
             kn, vn = new_vector_store.entries.popitem()
             assert v == vn
+
+    def test_save_load_json(self):
+        self.t_persistable_vector_store("json")
+
+    def test_save_load_zip(self):
+        self.t_persistable_vector_store("json")
+    def test_suffix_raises(self):
+        with pytest.raises(ValueError) as e_info:
+            with tempfile.NamedTemporaryFile() as f:
+                vector_store = PersistableLocalVectorStoreDriver(
+                    file_path=f.name, embedding_driver=MockEmbeddingDriver()
+                )
+            assert str(e_info.value) == "file must either be a json or a zip"
