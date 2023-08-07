ef71bcbe3877ca6065396f2f4c3ff7fa664fab8f
Tariq Baig-Meininghaus <tariq.baig@ds.mpg.de>
2023-08-07 16:33:49 +0200

    moved EntryAwareJSONEncoder into PersistableLocalVectorStoreDriver to conform to company style guide

--- griptape/drivers/vector/peristable_local_vector_store_driver.py
+++ griptape/drivers/vector/peristable_local_vector_store_driver.py
@@ -9,24 +9,6 @@ from attr import define, field
 from griptape.drivers import LocalVectorStoreDriver
 
 
-class EntryAwareJSONEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, PersistableLocalVectorStoreDriver.Entry):
-            d_dict = dataclasses.asdict(o)
-            d_dict["__Entry__"] = True
-            return d_dict
-        return super().default(o)
-
-
-def as_entry(dct: Dict):
-
-    if "__Entry__" in dct:
-        dct.pop("__Entry__")
-        return PersistableLocalVectorStoreDriver.Entry(**dct)
-
-    return dct
-
-
 @define
 class PersistableLocalVectorStoreDriver(LocalVectorStoreDriver):
     """
@@ -42,13 +24,33 @@ class PersistableLocalVectorStoreDriver(LocalVectorStoreDriver):
 
     @file_path.validator
     def _check_file_path(self, attribute, value):
-        if not value.suffix in (".json", ".zip"):
+        if value.suffix not in (".json", ".zip"):
             raise ValueError(
                 f"file must either be a json or a zip but file name is {self.file_path}"
             )
 
     def __attrs_post_init__(self):
         self.file_is_zip = self.file_path.suffix == ".zip"
+
+    class _EntryAwareJSONEncoder(json.JSONEncoder):
+        """Internal JSON encoder class that enables json serialization of `LocalVectorStore`s internal Entry structure."""
+
+        def default(self, o):
+            if isinstance(o, PersistableLocalVectorStoreDriver.Entry):
+                d_dict = dataclasses.asdict(o)
+                d_dict["__Entry__"] = True
+                return d_dict
+            return super().default(o)
+
+        @staticmethod
+        def json_deserialize_vector_store_entry(dct: Dict):
+            """Deserialization hook"""
+
+            if "__Entry__" in dct:
+                dct.pop("__Entry__")
+                return PersistableLocalVectorStoreDriver.Entry(**dct)
+
+            return dct
 
     def store(self, overwrite=False):
         """Serialize the entries of the vectorstore. Will not overwrite an existing file unless overwrite=True"""
@@ -57,23 +59,29 @@ class PersistableLocalVectorStoreDriver(LocalVectorStoreDriver):
 
         if self.file_is_zip:
             with ZipFile(self.file_path, write_mode) as z_f:
-                json_data = json.dumps(self.entries, cls=EntryAwareJSONEncoder)
+                json_data = json.dumps(self.entries, cls=self._EntryAwareJSONEncoder)
                 z_f.writestr("data.json", json_data)
         else:
             with open(self.file_path, write_mode) as f:
-                json.dump(self.entries, f, cls=EntryAwareJSONEncoder)
+                json.dump(self.entries, f, cls=self._EntryAwareJSONEncoder)
 
     def load(self) -> None:
-        """Read entries that have been serialized, automaticaly recognizes ziped saves by suffix.
+        """Read entries that have been serialized, automatically recognizes zipped saves by suffix.
         Will overwrite internal entries with the loaded ones"""
         if self.file_is_zip:
             with ZipFile(self.file_path) as zf:
                 json_data = zf.read("data.json")
-                self.entries = json.loads(json_data, object_hook=as_entry)
+                self.entries = json.loads(
+                    json_data,
+                    object_hook=self._EntryAwareJSONEncoder.json_deserialize_vector_store_entry,
+                )
 
         else:
             with open(self.file_path, "r") as f:
-                self.entries = json.load(f, object_hook=as_entry)
+                self.entries = json.load(
+                    f,
+                    object_hook=self._EntryAwareJSONEncoder.json_deserialize_vector_store_entry,
+                )
 
     @classmethod
     def from_saved(cls, file_path) -> "PersistableLocalVectorStoreDriver":
