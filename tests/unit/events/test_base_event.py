1076e4ba2e0166010310caff7f25fd0301d1f7c9
Vasily Vasinov <vasily@griptape.ai>
2023-08-13 10:00:11 -0700

    Add timestamps to all events (#160)

--- tests/unit/events/test_base_event.py
+++ tests/unit/events/test_base_event.py
@@ -1,7 +1,10 @@
-import pytest
-from griptape.events import BaseEvent
+import time
+from tests.mocks.mock_event import MockEvent
+
 
 class TestBaseEvent:
-    @pytest.fixture
-    def base_event(self):
-        return BaseEvent()
+    def test_timestamp(self):
+        dt = time.time()
+
+        assert MockEvent(timestamp=dt).timestamp == dt
+        assert MockEvent().timestamp >= dt
