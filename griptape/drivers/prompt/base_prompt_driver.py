89f02ac8e86af50cdd8888b64ac23497f207097c
Vasily Vasinov <des.elyon@gmail.com>
2023-08-15 18:11:49 -0600

    Turn BasePromptDriver.prompt_stack_to_string into a property

--- griptape/drivers/prompt/base_prompt_driver.py
+++ griptape/drivers/prompt/base_prompt_driver.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
-from typing import TYPE_CHECKING, Optional
-from attr import define, field
+from typing import TYPE_CHECKING, Optional, Callable
+from attr import define, field, Factory
 from griptape.core import ExponentialBackoffMixin, PromptStack
 from griptape.tokenizers import BaseTokenizer
 
@@ -15,6 +15,13 @@ class BasePromptDriver(ExponentialBackoffMixin, ABC):
     temperature: float = field(default=0.1, kw_only=True)
     max_tokens: Optional[int] = field(default=None, kw_only=True)
     structure: Optional[Structure] = field(default=None, kw_only=True)
+    prompt_stack_to_string: Callable[[PromptStack], str] = field(
+        default=Factory(
+            lambda self: self.default_prompt_stack_to_string_converter,
+            takes_self=True
+        ),
+        kw_only=True
+    )
 
     model: str
     tokenizer: BaseTokenizer
@@ -32,7 +39,7 @@ class BasePromptDriver(ExponentialBackoffMixin, ABC):
 
                 return result
 
-    def prompt_stack_to_string(self, prompt_stack: PromptStack) -> str:
+    def default_prompt_stack_to_string_converter(self, prompt_stack: PromptStack) -> str:
         prompt_lines = []
 
         for i in prompt_stack.inputs:
