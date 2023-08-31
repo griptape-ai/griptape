d86fff1fad3e7cad36cfbfcc04b529390372d88f
Trisha Mandal <97489901+trish11953@users.noreply.github.com>
2023-08-31 11:03:42 -0700

    formatting

--- griptape/drivers/vector/amazon_opensearch_vector_store_driver.py
+++ griptape/drivers/vector/amazon_opensearch_vector_store_driver.py
@@ -11,18 +11,24 @@ class AmazonOpenSearchVectorStoreDriver(OpenSearchVectorStoreDriver):
     session: boto3.session.Session = field(kw_only=True)
 
     http_auth: Optional[Union[str, Tuple[str, str]]] = field(
-        default=Factory(lambda self: AWS4Auth(self.session.get_credentials().access_key,
-                                              self.session.get_credentials().secret_key,
-                                              self.session.region_name, 'es'), takes_self=True)
+        default=Factory(
+            lambda self: AWS4Auth(
+                self.session.get_credentials().access_key,
+                self.session.get_credentials().secret_key,
+                self.session.region_name, 'es'
+            ),
+            takes_self=True)
     )
 
-    client: OpenSearch = field(default=Factory(
-        lambda self: OpenSearch(
-            hosts=[{'host': self.host, 'port': self.port}],
-            http_auth=self.http_auth,
-            use_ssl=self.use_ssl,
-            verify_certs=self.verify_certs,
-            connection_class=RequestsHttpConnection
-        ),
-        takes_self=True
-    ))
+    client: OpenSearch = field(
+        default=Factory(
+            lambda self: OpenSearch(
+                hosts=[{'host': self.host, 'port': self.port}],
+                http_auth=self.http_auth,
+                use_ssl=self.use_ssl,
+                verify_certs=self.verify_certs,
+                connection_class=RequestsHttpConnection
+            ),
+            takes_self=True
+        )
+    )
