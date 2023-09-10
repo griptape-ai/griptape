b6ca98cc712030f5b014cd19d3e4c26fe9fe5f34
Vasily Vasinov <des.elyon@gmail.com>
2023-09-10 11:04:59 -0600

    Add two separate FileManager activities to support saving files frmo memory as well as direct content

--- griptape/tools/file_manager/tool.py
+++ griptape/tools/file_manager/tool.py
@@ -4,7 +4,7 @@ from attr import define, field
 from griptape.artifacts import ErrorArtifact, BlobArtifact, InfoArtifact, ListArtifact
 from griptape.tools import BaseTool
 from griptape.utils.decorators import activity
-from schema import Schema, Literal, Or
+from schema import Schema, Literal
 
 
 @define
@@ -45,66 +45,83 @@ class FileManager(BaseTool):
         return list_artifact
 
     @activity(config={
-        "description": "Can be used to save data to disk",
-        "schema": Schema({
-            Literal(
-                "path",
-                description="Destination path on disk in the POSIX format. For example, ['foo/bar/file.txt']"
-            ): str,
-            "data": Or(
-                {
-                    "memory_name": str,
-                    "artifact_namespace": str
-                },
-                {
-                    Literal(
-                        "content",
-                        description="Content to be written into the destination path."
-                    ): str
-                },
-                only_one=True
-            )
-        })
+        "description": "Can be used to save memory artifacts to disk",
+        "schema": Schema(
+            {
+                Literal(
+                    "dirname",
+                    description="Destination directory name on disk in the POSIX format. For example, 'foo/bar'"
+                ): str,
+                Literal(
+                    "filename",
+                    description="Destination filename. For example, 'baz.txt'"
+                ): str,
+                "memory_name": str,
+                "artifact_namespace": str
+            }
+        )
     })
-    def save_file_to_disk(self, params: dict) -> ErrorArtifact | InfoArtifact:
-        is_memory = params["values"]["data"].get("memory_name") is not None
+    def save_memory_artifacts_to_disk(self, params: dict) -> ErrorArtifact | InfoArtifact:
+        memory = self.find_input_memory(params["values"]["memory_name"])
+        artifact_namespace = params["values"]["artifact_namespace"]
+        dirname = params["values"]["dirname"]
+        filename = params["values"]["filename"]
+
+        if memory:
+            artifacts = memory.load_artifacts(artifact_namespace)
+
+            if len(artifacts) == 0:
+                return ErrorArtifact("no artifacts found")
+            elif len(artifacts) == 1:
+                try:
+                    self._save_to_disk(
+                        os.path.join(self.dir, dirname, filename),
+                        artifacts[0].to_text()
+                    )
+
+                    return InfoArtifact(f"saved successfully")
+                except Exception as e:
+                    return ErrorArtifact(f"error writing file to disk: {e}")
+            else:
+                try:
+                    for a in artifacts:
+                        self._save_to_disk(
+                            os.path.join(self.dir, dirname, f"{a.name}-{filename}"),
+                            a.to_text()
+                        )
+
+                    return InfoArtifact(f"saved successfully")
+                except Exception as e:
+                    return ErrorArtifact(f"error writing file to disk: {e}")
+        else:
+            return ErrorArtifact("memory not found")
+
+    @activity(config={
+        "description": "Can be used to save content to a file",
+        "schema": Schema(
+            {
+                Literal(
+                    "path",
+                    description="Destination file path on disk in the POSIX format. For example, 'foo/bar/baz.txt'"
+                ): str,
+                "content": str
+            }
+        )
+    })
+    def save_content_to_file(self, params: dict) -> ErrorArtifact | InfoArtifact:
+        content = params["values"]["content"]
         new_path = params["values"]["path"]
         full_path = os.path.join(self.dir, new_path)
 
-        if is_memory:
-            artifact_namespace = params["values"]["data"]["artifact_namespace"]
-            memory = self.find_input_memory(params["values"]["data"]["memory_name"])
+        try:
+            self._save_to_disk(full_path, content)
 
-            if memory:
-                artifacts = memory.load_artifacts(artifact_namespace)
+            return InfoArtifact(f"saved successfully")
+        except Exception as e:
+            return ErrorArtifact(f"error writing file to disk: {e}")
 
-                if len(artifacts) == 0:
-                    return ErrorArtifact("no artifacts found")
-                else:
-                    try:
-                        os.makedirs(os.path.dirname(full_path), exist_ok=True)
+    def _save_to_disk(self, path: str, value: any) -> None:
+        os.makedirs(os.path.dirname(path), exist_ok=True)
 
-                        with open(full_path, "wb") as file:
-                            value = "\n".join([a.to_text() for a in artifacts])
-
-                            file.write(value.encode() if isinstance(value, str) else value)
-
-                            return InfoArtifact(f"saved successfully")
-                    except Exception as e:
-                        return ErrorArtifact(f"error writing file to disk: {e}")
-            else:
-                return ErrorArtifact("memory not found")
-        else:
-            content = params["values"]["data"]["content"]
-
-            try:
-                os.makedirs(os.path.dirname(full_path), exist_ok=True)
-
-                with open(full_path, "wb") as file:
-                    value = content
-
-                    file.write(value.encode() if isinstance(value, str) else value)
-
-                    return InfoArtifact(f"saved successfully")
-            except Exception as e:
-                return ErrorArtifact(f"error writing file to disk: {e}")
+        with open(path, "wb") as file:
+            file.write(value.encode() if isinstance(value, str) else value)
--- tests/unit/tools/test_file_manager.py
+++ tests/unit/tools/test_file_manager.py
@@ -1,5 +1,6 @@
 import os.path
 import tempfile
+from pathlib import Path
 from griptape.artifacts import BlobArtifact, TextArtifact, ListArtifact
 from griptape.drivers import LocalVectorStoreDriver
 from griptape.engines import VectorQueryEngine
@@ -19,47 +20,78 @@ class TestFileManager:
         assert len(result.value) == 1
         assert isinstance(result.value[0], BlobArtifact)
 
-    def test_save_file_to_disk_from_memory(self):
+    def test_save_memory_artifacts_to_disk_for_one_artifact(self):
         with tempfile.TemporaryDirectory() as temp_dir:
             memory = TextToolMemory(
                 query_engine=VectorQueryEngine(
                     vector_store_driver=LocalVectorStoreDriver(
                         embedding_driver=MockEmbeddingDriver())))
             artifact = TextArtifact("foobar")
-            path = os.path.join(temp_dir, "foobar.txt")
 
             memory.query_engine.vector_store_driver.upsert_text_artifact(artifact, namespace="foobar")
 
             result = FileManager(
                 input_memory=[memory]
-            ).save_file_to_disk(
+            ).save_memory_artifacts_to_disk(
                 {
                     "values":
                         {
-                            "path": path,
-                            "data": {
-                                "memory_name": memory.name,
-                                "artifact_namespace": "foobar"
-                            }
+                            "path": temp_dir,
+                            "filename": "foobar.txt",
+                            "memory_name": memory.name,
+                            "artifact_namespace": "foobar"
                         }
                 }
             )
 
+            assert Path(os.path.join(temp_dir, "foobar.txt")).read_text() == "foobar"
             assert result.value == "saved successfully"
 
-    def test_save_file_to_disk_from_prompt(self):
+    def test_save_memory_artifacts_to_disk_for_multiple_artifacts(self):
+        filename = "foobar.txt"
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            memory = TextToolMemory(
+                query_engine=VectorQueryEngine(
+                    vector_store_driver=LocalVectorStoreDriver(
+                        embedding_driver=MockEmbeddingDriver())))
+            artifacts = [
+                TextArtifact("foobar"),
+                TextArtifact("baz")
+            ]
+
+            for a in artifacts:
+                memory.query_engine.vector_store_driver.upsert_text_artifact(a, namespace="foobar")
+
+            result = FileManager(
+                input_memory=[memory]
+            ).save_memory_artifacts_to_disk(
+                {
+                    "values":
+                        {
+                            "path": temp_dir,
+                            "filename": filename,
+                            "memory_name": memory.name,
+                            "artifact_namespace": "foobar"
+                        }
+                }
+            )
+
+            assert Path(os.path.join(temp_dir, f"{artifacts[0].name}-{filename}")).read_text() == "foobar"
+            assert Path(os.path.join(temp_dir, f"{artifacts[1].name}-{filename}")).read_text() == "baz"
+            assert result.value == "saved successfully"
+
+    def test_save_content_to_file(self):
         with tempfile.TemporaryDirectory() as temp_dir:
             path = os.path.join(temp_dir, "foobar.txt")
 
             result = FileManager(
-            ).save_file_to_disk(
+            ).save_content_to_file(
                 {
                     "values":
                         {
                             "path": path,
-                            "data": {
-                                "content": "foobar"
-                            }
+                            "content": "foobar"
                         }
                 }
             )
