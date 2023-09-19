e50b02781ebe81b7ce60b13816ca548d71de80e7
Collin Dutter <collin@griptape.ai>
2023-09-19 14:42:10 -0700

    Add bedrock prompt driver (#220)

---
+++ tests/unit/drivers/prompt/test_amazon_bedrock_prompt_driver.py
@@ -0,0 +1,41 @@
+from unittest import mock
+import pytest
+import boto3
+from griptape.drivers import AmazonBedrockPromptDriver
+from griptape.drivers import BedrockClaudePromptModelDriver, BedrockTitanPromptModelDriver
+from griptape.tokenizers import AnthropicTokenizer, BedrockTitanTokenizer
+
+
+class TestAmazonBedrockPromptDriver:
+    @pytest.fixture(autouse=True)
+    def mock_session(self, mocker):
+        mock_session_class = mocker.patch("boto3.Session")
+
+        mock_session_object = mock.Mock()
+        mock_client = mock.Mock()
+        mock_response = mock.Mock()
+
+        mock_client.invoke_model.return_value = mock_response
+        mock_session_object.client.return_value = mock_client
+        mock_session_class.return_value = mock_session_object
+
+    def test_init(self):
+        assert AmazonBedrockPromptDriver(
+            model="foo",
+            session=boto3.Session(region_name="us-east-1"),
+            prompt_model_driver=BedrockClaudePromptModelDriver()
+        )
+
+    def test_custom_tokenizer(self):
+        assert isinstance(AmazonBedrockPromptDriver(
+            model="foo",
+            session=boto3.Session(region_name="us-east-1"),
+            prompt_model_driver=BedrockClaudePromptModelDriver()
+        ).tokenizer, AnthropicTokenizer)
+
+        assert isinstance(AmazonBedrockPromptDriver(
+            model="foo",
+            session=boto3.Session(region_name="us-east-1"),
+            tokenizer=BedrockTitanTokenizer(model="amazon.titan-tg1-large"),
+            prompt_model_driver=BedrockTitanPromptModelDriver()
+        ).tokenizer, BedrockTitanTokenizer)
