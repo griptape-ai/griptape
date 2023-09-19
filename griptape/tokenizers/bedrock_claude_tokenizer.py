1088d2d9d66c9e703e02deaecbffc8102db03ea6
Collin Dutter <collin@griptape.ai>
2023-09-19 13:55:48 -0700

    Extend default stop sequences

--- griptape/tokenizers/bedrock_claude_tokenizer.py
+++ griptape/tokenizers/bedrock_claude_tokenizer.py
@@ -1,5 +1,5 @@
 from attr import define, field, Factory
-from griptape.tokenizers import AnthropicTokenizer
+from griptape.tokenizers import BaseTokenizer, AnthropicTokenizer
 
 
 @define(frozen=True)
@@ -7,4 +7,6 @@ class BedrockClaudeTokenizer(AnthropicTokenizer):
     DEFAULT_MODEL = 'anthropic.claude-v2'
     DEFAULT_MAX_TOKENS = 8192
 
-    stop_sequences: list[str] = field(default=Factory(lambda: ["\n\nHuman:"]), kw_only=True)
+    stop_sequences: list[str] = field(
+        default=Factory(lambda: BaseTokenizer.DEFAULT_STOP_SEQUENCES + ["\n\nHuman:"]), kw_only=True
+    )
