cf885826c78ac45bb50467c9b0773c13f8a4c7ef
Collin Dutter <collin@griptape.ai>
2023-09-19 10:07:37 -0700

    Update tokenizer to support multiple models

--- griptape/tokenizers/amazon_bedrock_tokenizer.py
+++ griptape/tokenizers/amazon_bedrock_tokenizer.py
@@ -1,4 +1,3 @@
-import os
 import json
 import boto3
 from attr import define, field, Factory
@@ -8,18 +7,21 @@ from griptape.tokenizers import BaseTokenizer
 
 @define(frozen=True)
 class AmazonBedrockTokenizer(BaseTokenizer):
-    DEFAULT_MODEL = 'amazon.titan-e1t-medium'
-    DEFAULT_MAX_TOKENS = 4096
-    
-    stop_sequences: list[str] = field(
-        default=Factory(lambda: []),
-        kw_only=True
-    )
+    MODEL_TO_MAX_TOKENS = {
+        "anthropic.claude-v1": 8192,
+        "anthropic.claude-v2": 8192,
+        "anthropic.claude-instant-v1": 8192,
+        "amazon.titan-tg1-large": 4096,
+        "ai21.j2-ultra": 8192,
+        "ai21.j2-mid": 8192,
+    }
 
     session: boto3.Session = field(
         default=Factory(lambda: boto3.Session()), kw_only=True
     )
-    model: str = field(default=DEFAULT_MODEL, kw_only=True)
+    stop_sequences: list[str] = field(factory=list, kw_only=True)
+
+    model: str = field(kw_only=True)
     bedrock_client: Any = field(
         default=Factory(
             lambda self: self.session.client("bedrock"),
@@ -30,12 +32,10 @@ class AmazonBedrockTokenizer(BaseTokenizer):
 
     @property
     def max_tokens(self) -> int:
-        return self.DEFAULT_MAX_TOKENS
+        return self.MODEL_TO_MAX_TOKENS[self.model]
 
     def token_count(self, text: str) -> int:
-        text = text.replace(os.linesep, " ")
-
-        payload = {"inputText": text}
+        payload = self.__model_to_input_params(text)
 
         response = self.bedrock_client.invoke_model(
             body=json.dumps(payload),
@@ -45,10 +45,26 @@ class AmazonBedrockTokenizer(BaseTokenizer):
         )
         response_body = json.loads(response.get("body").read())
 
-        return response_body.get("inputTextTokenCount")
+        return self.__process_output(response_body)
 
     def encode(self, _: list[int]) -> str:
         raise NotImplementedError("Method is not implemented: Amazon Bedrock does not provide a compatible tokenization API.")
 
     def decode(self, _: list[int]) -> str:
         raise NotImplementedError("Method is not implemented: Amazon Bedrock does not provide a compatible de-tokenization API.")
+
+    def __model_to_input_params(self, text: str) -> dict:
+        if self.model.startswith("ai21"):
+            return {"prompt": text}
+        elif self.model.startswith("amazon"):
+            return {"inputText": text}
+        else:
+            raise ValueError(f"{self.model} is not a valid Amazon Bedrock model for tokenization.")
+
+    def __process_output(self, output: dict) -> int:
+        if self.model.startswith("ai21"):
+            return len(output["prompt"]["tokens"])
+        elif self.model.startswith("amazon"):
+            return output["inputTextTokenCount"]
+        else:
+            raise ValueError(f"{self.model} is not a valid Amazon Bedrock model for tokenization.")
