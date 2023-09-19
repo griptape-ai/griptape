308fc6a6dc0b0dae3be7275c4d81fe06e437ec0e
Collin Dutter <collin@griptape.ai>
2023-09-19 13:45:13 -0700

    Fix tokenizer model

--- griptape/tokenizers/bedrock_jurassic_tokenizer.py
+++ griptape/tokenizers/bedrock_jurassic_tokenizer.py
@@ -14,7 +14,7 @@ class BedrockJurassicTokenizer(BaseTokenizer):
         default=Factory(lambda: boto3.Session()), kw_only=True
     )
     stop_sequences: list[str] = field(factory=list, kw_only=True)
-    model: str = field(default=DEFAULT_MAX_TOKENS, kw_only=True)
+    model: str = field(default=DEFAULT_MODEL, kw_only=True)
     bedrock_client: Any = field(
         default=Factory(
             lambda self: self.session.client("bedrock"),
