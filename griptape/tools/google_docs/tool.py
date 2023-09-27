e36b7ef97798d3d7c0ccf141c2d1d8516bd00c3c
Trisha Mandal <97489901+trish11953@users.noreply.github.com>
2023-09-26 17:42:39 -0700

    content

--- griptape/tools/google_docs/tool.py
+++ griptape/tools/google_docs/tool.py
@@ -1,12 +1,11 @@
 from __future__ import annotations
 import logging
 from attr import field, define
-from schema import Schema, Literal, Or, Optional
+from schema import Schema, Optional
 from griptape.artifacts import ErrorArtifact, InfoArtifact, BlobArtifact, ListArtifact
 from griptape.utils.decorators import activity
 from griptape.tools import BaseGoogleClient
 from googleapiclient.discovery import Resource
-
 
 @define
 class GoogleDocsClient(BaseGoogleClient):
@@ -19,12 +18,11 @@ class GoogleDocsClient(BaseGoogleClient):
     @activity(
         config={
             "description": "Can be used to create a new Google Doc in Drive",
-            "schema": Schema({"file_path": Or(str)}),
+            "schema": Schema({"file_name": str}),
         }
     )
-    def create_google_doc(self, file_path: str) -> InfoArtifact | ErrorArtifact:
+    def create_google_doc(self, file_name: str) -> InfoArtifact | ErrorArtifact:
         try:
-            file_name = file_path.split("/")[-1]
             service = self._build_client(self.DOCS_SCOPES, "v1", "docs")
             doc = service.documents().create(body={"title": file_name}).execute()
             return InfoArtifact(f"Google Doc '{file_name}' created with ID: {doc['documentId']}")
@@ -36,10 +34,10 @@ class GoogleDocsClient(BaseGoogleClient):
     @activity(
         config={
             "description": "Can be used to append text to a Google Doc",
-            "schema": Schema({"file_path": Or(str), "text": Or(str)}),
+            "schema": Schema({"file_path": str, "text": str}),
         }
     )
-    def append_text(self, file_path: str, text: str) -> InfoArtifact | ErrorArtifact:
+    def append_text_to_google_doc(self, file_path: str, text: str) -> InfoArtifact | ErrorArtifact:
         try:
             service = self._build_client(self.DOCS_SCOPES, "v1", "docs")
             drive_service = self._build_client(self.DRIVE_SCOPES, "v3", "drive")
@@ -66,10 +64,10 @@ class GoogleDocsClient(BaseGoogleClient):
     @activity(
         config={
             "description": "Can be used to prepend text to a Google Doc",
-            "schema": Schema({"file_path": Or(str), "text": Or(str)}),
+            "schema": Schema({"file_path": str, "text": str}),
         }
     )
-    def prepend_text(self, file_path: str, text: str) -> InfoArtifact | ErrorArtifact:
+    def prepend_text_to_google_doc(self, file_path: str, text: str) -> InfoArtifact | ErrorArtifact:
         try:
             service = self._build_client(self.DOCS_SCOPES, "v1", "docs")
             drive_service = self._build_client(self.DRIVE_SCOPES, "v3", "drive")
@@ -94,11 +92,32 @@ class GoogleDocsClient(BaseGoogleClient):
 
     @activity(
         config={
-            "description": "Can be used to download multiple Google Docs from Google Drive based on their paths",
-            "schema": Schema({"file_paths": [Or(str)]}),
+            "description": "Can be used to create a new Google Doc and save content to it on Google Drive",
+            "schema": Schema({"file_name": str, "content": str}),
         }
     )
-    def load_docs_from_drive(self, file_paths: list[str]) -> ListArtifact | ErrorArtifact:
+    def save_content_to_google_doc(self, params: dict) -> ErrorArtifact | InfoArtifact:
+        doc_creation_result = self.create_google_doc(params["file_name"])
+    
+        if isinstance(doc_creation_result, ErrorArtifact):
+            return ErrorArtifact(f"Error creating Google Doc: {doc_creation_result.value}")
+    
+        document_id = doc_creation_result.value.split()[-1]
+        save_content_params = {"document_id": document_id, "content": params["content"]}
+    
+        try:
+            saved_document_id = self._save_to_doc(save_content_params)
+            return InfoArtifact(saved_document_id)
+        except Exception as e:
+            return ErrorArtifact(f"Error saving content to Google Doc with ID {document_id}: {str(e)}")
+
+    @activity(
+        config={
+            "description": "Can be used to download multiple Google Docs from Google Drive based on their paths",
+            "schema": Schema({"file_paths": [str]}),
+        }
+    )
+    def download_google_docs(self, file_paths: list[str]) -> ListArtifact | ErrorArtifact:
         downloaded_files = []
 
         for path in file_paths:
@@ -110,22 +129,23 @@ class GoogleDocsClient(BaseGoogleClient):
         return ListArtifact(downloaded_files)
 
     def _download_document(self, file_path: str) -> BlobArtifact | ErrorArtifact:
+
         try:
             service = self._build_client(self.DRIVE_SCOPES, "v3", "drive")
             file_id = self._path_to_file_id(service, file_path)
             if file_id:
                 results = service.files().list(q=f"name='{file_path}'", spaces="drive", pageSize=10).execute()
                 items = results.get("files", [])
-                if not items:
+                if items:
+                    file_id = items[0]["id"]
+                    request = service.files().export(fileId=file_id, mimeType="text/plain")
+                    response = request.execute()
+                    return BlobArtifact(response)
+                else:
                     return ErrorArtifact(f"Error: File not found for path {file_path}")
-                file_id = items[0]["id"]
-                request = service.files().export(fileId=file_id, mimeType="text/plain")
-                response = request.execute()
-
-                return BlobArtifact(response)
             else:
                 return ErrorArtifact(f"Error: File not found for path {file_path}")
-
+    
         except Exception as e:
             logging.error(e)
             return ErrorArtifact(f"Error downloading Google Doc: {e}")
@@ -144,27 +164,48 @@ class GoogleDocsClient(BaseGoogleClient):
     def _path_to_file_id(self, service, path: str) -> Optional[str]:
         parts = path.split("/")
         current_id = "root"
-
+    
         for idx, part in enumerate(parts):
             if idx == len(parts) - 1:
                 query = f"name='{part}' and '{current_id}' in parents"
             else:
                 query = f"name='{part}' and '{current_id}' in parents and mimeType='application/vnd.google-apps.folder'"
-
+    
             response = service.files().list(q=query).execute()
             files = response.get("files", [])
-
-            if not files and idx != len(parts) - 1:
-                folder_metadata = {
-                    "name": part,
-                    "mimeType": "application/vnd.google-apps.folder",
-                    "parents": [current_id],
-                }
-                folder = service.files().create(body=folder_metadata, fields="id").execute()
-                current_id = folder.get("id")
-            elif files:
-                current_id = files[0]["id"]
+    
+            if not files:
+                if idx != len(parts) - 1:
+                    folder_metadata = {
+                        "name": part,
+                        "mimeType": "application/vnd.google-apps.folder",
+                        "parents": [current_id],
+                    }
+                    folder = service.files().create(body=folder_metadata, fields="id").execute()
+                    current_id = folder.get("id")
+                else:
+                    current_id = None
             else:
-                return None
-
+                current_id = files[0]["id"]
+    
         return current_id
+
+    def _save_to_doc(self, params: dict) -> str:
+        service = self._build_client(
+            self.DOCS_SCOPES,
+            version="v1",
+            tool="docs",
+        )
+    
+        requests = [
+            {
+                "insertText": {
+                    "location": {
+                        "index": 1,
+                    },
+                    "text": params["content"],
+                }
+            }
+        ]
+        service.documents().batchUpdate(documentId=params["document_id"], body={"requests": requests}).execute()
+        return params["document_id"]
--- tests/unit/tools/test_google_docs.py
+++ tests/unit/tools/test_google_docs.py
@@ -1,63 +1,39 @@
 from griptape.tools import GoogleDocsClient
 from unittest.mock import patch
 from google.auth.exceptions import MalformedError
+import pytest
 
 
 class TestGoogleDocsClient:
 
-    def test_create_google_doc(self):
-        value = {
-            "file_name": "test_document"
-        }
-        assert "Error creating Google Doc" in GoogleDocsClient(
+    @pytest.fixture
+    def mock_docs_client(self):
+        return GoogleDocsClient(
             owner_email="tony@griptape.ai",
             service_account_credentials={}
-        ).create_google_doc(file_name=value["file_name"]).value
+        )
 
-    def test_append_text(self):
-        params = {
-            "values": {
-                "document_id": "test_doc_id"
-            }
-        }
+    def test_create_google_doc(self, mock_docs_client):
+        file_name = "test_document"
+        assert "Error creating Google Doc" in mock_docs_client.create_google_doc(file_name=file_name).value
+
+    def test_append_text(self, mock_docs_client):
+        file_path = "test_folder/test_document"
         text = "Appending this text"
-        assert "Error appending text to Google Doc" in GoogleDocsClient(
-            owner_email="tony@griptape.ai",
-            service_account_credentials={}
-        ).append_text(params, text).value
+        assert "Error appending text to Google Doc" in mock_docs_client.append_text_to_google_doc(file_path=file_path, text=text).value
 
     @patch('google.oauth2.service_account.Credentials.from_service_account_info')
-    def test_prepend_text(self, mock_auth):
+    def test_prepend_text(self, mock_auth, mock_docs_client):
         mock_auth.side_effect = MalformedError('Mocked Error')
-        params = {
-            "values": {
-                "document_id": "test_doc_id"
-            }
-        }
+        file_path = "test_folder/test_document"
         text = "Prepending this text"
-        assert "Error prepending text to Google Doc" in GoogleDocsClient(
-            owner_email="tony@griptape.ai",
-            service_account_credentials={}
-        ).prepend_text(params, text).value
+        assert "Error prepending text to Google Doc" in mock_docs_client.prepend_text_to_google_doc(file_path=file_path, text=text).value
 
-    @patch('google.oauth2.service_account.Credentials.from_service_account_info')
-    def test_upload_document(self, mock_auth):
-        mock_auth.side_effect = MalformedError('Mocked Error')
-        params = {
-            "values": {
-                "file_path": "/path/to/your/file.txt",
-                "file_name": "uploaded_file.txt",
-                "mime_type": "text/plain"
-            }
-        }
-        assert "error uploading document to Google Docs" in GoogleDocsClient(
-            owner_email="tony@griptape.ai",
-            service_account_credentials={}
-        ).upload_document(params).value
+    def test_download_google_docs(self, mock_docs_client):
+        file_paths = ["folder_name/document_name"]
+        assert "Error downloading Google Doc" in mock_docs_client.download_google_docs(file_paths=file_paths).value
 
-    def test_download_document(self):
-        file_path = "folder_name/document_name"
-        assert "Error downloading Google Doc" in GoogleDocsClient(
-            owner_email="tony@griptape.ai",
-            service_account_credentials={}
-        ).download_document(file_path).value
+    def test_save_content_to_google_doc(self, mock_docs_client):
+        params = {"file_name": "test_document", "content": "Sample content"}
+        expected_error_msg = "Error creating Google Doc"
+        assert expected_error_msg in mock_docs_client.save_content_to_google_doc(params).value
