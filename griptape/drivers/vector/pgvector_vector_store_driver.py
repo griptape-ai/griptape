df6b80b5bccaa1657deee786f5363fa931e1da04
Andrew French <andrew@afren.ch>
2023-09-28 20:20:09 -0700

    Improvements

--- griptape/drivers/vector/pgvector_vector_store_driver.py
+++ griptape/drivers/vector/pgvector_vector_store_driver.py
@@ -1,9 +1,9 @@
 import uuid
 from typing import Optional
-from attr import define, field, Factory 
+from attr import define, field, Factory
 from griptape.drivers import BaseVectorStoreDriver
 from sqlalchemy.engine import Engine
-from sqlalchemy import create_engine, Column, String, JSON, select
+from sqlalchemy import create_engine, Column, String, JSON
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Session
@@ -20,15 +20,16 @@ class PgVectorVectorStoreDriver(BaseVectorStoreDriver):
         create_engine_params: Additional configuration params passed when creating the database connection.
         engine: An optional sqlalchemy Postgres engine to use.
     """
-    connection_string: str = field(kw_only=True)
+
+    connection_string: Optional[str] = field(default=None, kw_only=True)
     create_engine_params: Optional[dict] = field(factory=dict, kw_only=True)
     engine: Optional[Engine] = field(
-        default=Factory(lambda self: create_engine(self.connection_string, **self.create_engine_params), takes_self=True)
+        default=Factory(
+            lambda self: create_engine(self.connection_string, **self.create_engine_params), takes_self=True
+        )
     )
 
-    Base = declarative_base()
-
-    class Model(Base):
+    class VectorModel(declarative_base()):
         __tablename__ = "griptape_vectors"
 
         id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
@@ -36,22 +37,32 @@ class PgVectorVectorStoreDriver(BaseVectorStoreDriver):
         namespace = Column(String)
         meta = Column(JSON)
 
-    def prepare_database(self) -> None:
+    def install_postgres_extensions(self) -> None:
+        """
+        Ensures the uuid-ossp and vector extensions are installed in the database.
+        """
         self.engine.execute('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
         self.engine.execute('CREATE EXTENSION IF NOT EXISTS "vector";')
 
-        self.Base.metadata.create_all(self.engine)
+    def create_schema(self) -> None:
+        """
+        Creates the schema for the vectors table.
+        """
+        self.VectorModel.metadata.create_all(self.engine)
 
     def upsert_vector(
-            self,
-            vector: list[float],
-            vector_id: Optional[str] = None,
-            namespace: Optional[str] = None,
-            meta: Optional[dict] = None,
-            **kwargs
+        self,
+        vector: list[float],
+        vector_id: Optional[str] = None,
+        namespace: Optional[str] = None,
+        meta: Optional[dict] = None,
+        **kwargs
     ) -> str:
+        """
+        Inserts or updates a vector in the collection.
+        """
         with Session(self.engine) as session:
-            obj = self.Model(
+            obj = self.VectorModel(
                 id=vector_id,
                 vector=vector,
                 namespace=namespace,
@@ -63,13 +74,12 @@ class PgVectorVectorStoreDriver(BaseVectorStoreDriver):
 
             return obj.id
 
-    def load_entry(
-            self,
-            vector_id: str,
-            namespace: Optional[str] = None
-    ) -> BaseVectorStoreDriver.Entry:
+    def load_entry(self, vector_id: str, namespace: Optional[str] = None) -> BaseVectorStoreDriver.Entry:
+        """
+        Retrieves a specific vector entry from the collection based on its identifier and optional namespace.
+        """
         with Session(self.engine) as session:
-            result = session.get(self.Model, vector_id)
+            result = session.get(self.VectorModel, vector_id)
 
             return BaseVectorStoreDriver.Entry(
                 id=result.id,
@@ -79,8 +89,16 @@ class PgVectorVectorStoreDriver(BaseVectorStoreDriver):
             )
 
     def load_entries(self, namespace: Optional[str] = None) -> list[BaseVectorStoreDriver.Entry]:
+        """
+        Retrieves all vector entries from the collection, optionally filtering to only
+        those that match the provided namespace.
+        """
         with Session(self.engine) as session:
-            results = session.query(self.Model).filter_by(namespace=namespace).all()
+            query = session.query(self.VectorModel)
+            if namespace:
+                query = query.filter_by(namespace=namespace)
+
+            results = query.all()
 
             return [
                 BaseVectorStoreDriver.Entry(
@@ -93,33 +111,37 @@ class PgVectorVectorStoreDriver(BaseVectorStoreDriver):
             ]
 
     def query(
-            self,
-            query: str,
-            count: Optional[int] = None,
-            namespace: Optional[str] = None,
-            include_vectors: bool = False,
-            distance_metric: str = "cosine_distance",
-            **kwargs
+        self,
+        query: str,
+        count: Optional[int] = None,
+        namespace: Optional[str] = None,
+        include_vectors: bool = False,
+        distance_metric: str = "cosine_distance",
+        **kwargs
     ) -> list[BaseVectorStoreDriver.QueryResult]:
+        """
+        Performs a search on the collection to find vectors similar to the provided input vector,
+        optionally filtering to only those that match the provided namespace.
+        """
         count = count if count else BaseVectorStoreDriver.DEFAULT_QUERY_COUNT
-        vector = self.embedding_driver.embed_string(query)
 
-        if distance_metric == "cosine_distance":
-            op = self.Model.vector.cosine_distance(vector)
-        elif distance_metric == "l2_distance":
-            op = self.Model.vector.l2_distance(vector)
-        elif distance_metric == "inner_product":
-            op = self.Model.vector.max_inner_product(vector)
-        else:
+        distance_metrics = {
+            "cosine_distance": self.VectorModel.vector.cosine_distance,
+            "l2_distance": self.VectorModel.vector.l2_distance,
+            "inner_product": self.VectorModel.vector.max_inner_product,
+        }
+        op = distance_metrics[distance_metric]
+        if op is None:
             raise ValueError("Invalid distance metric provided")
 
         with Session(self.engine) as session:
+            vector = self.embedding_driver.embed_string(query)
+
+            # The query should return both the vector and the distance metric score.
             query = session.query(
-                self.Model,
-                op.label("score")
-            ).order_by(
-                op
-            )
+                self.VectorModel,
+                op(vector).label("score"),
+            ).order_by(op(vector))
 
             if namespace:
                 query = query.filter_by(namespace=namespace)
