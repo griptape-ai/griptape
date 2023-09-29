6df5af565d49a496a912e17813fa4e38d4d93cba
Andrew French <andrew@afren.ch>
2023-09-29 16:27:41 -0700

    Parameterize connection test with sane defaults

--- tests/utils/postgres.py
+++ tests/utils/postgres.py
@@ -1,9 +1,9 @@
 from psycopg2 import connect, OperationalError
 
 
-def can_connect_to_postgres():
+def can_connect_to_postgres(user="postgres", password="postgres", host="localhost", port="5432", database="postgres"):
     try:
-        connection = connect("postgresql://postgres:postgres@localhost:5432/postgres")
+        connection = connect(f"postgresql://{user}:{password}@{host}:{port}/{database}")
         connection.close()
 
         return True
