4763bfc7edc43eae91d6870509717ee673699424
Andrew French <andrew@afren.ch>
2023-10-05 14:07:57 -0700

    Add support for PGVector vector store driver (#313)

---
+++ tests/utils/postgres.py
@@ -0,0 +1,12 @@
+from psycopg2 import connect, OperationalError
+
+
+def can_connect_to_postgres(user="postgres", password="postgres", host="localhost", port="5432", database="postgres"):
+    try:
+        connection = connect(f"postgresql://{user}:{password}@{host}:{port}/{database}")
+        connection.close()
+
+        return True
+
+    except OperationalError:
+        return False
