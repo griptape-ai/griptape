2150388ab329a08b74ebfda1b1bbef3ad4c0e257
Andrew French <andrew@afren.ch>
2023-10-06 08:44:40 -0700

    OpenAiChatPromptDriver: only set max_tokens when requested (#333)
    * Remove max_tokens from OpenAiPromptDriver
    
    * Remove debugging method
    
    * Optionally send max_tokens if provided by caller

--- tests/unit/drivers/prompt/test_openai_chat_prompt_driver.py
+++ tests/unit/drivers/prompt/test_openai_chat_prompt_driver.py
@@ -4,32 +4,32 @@ from griptape.tokenizers import OpenAiTokenizer
 from unittest.mock import ANY, Mock
 import pytest
 
+
 class TestOpenAiChatPromptDriverFixtureMixin:
     @pytest.fixture(autouse=True)
     def mock_chat_completion_create(self, mocker):
-        mock_chat_create = mocker.patch('openai.ChatCompletion').create
-        mock_chat_create.return_value.choices = [ 
-            {'message': {'content': 'model-output'}}
-        ]
+        mock_chat_create = mocker.patch("openai.ChatCompletion").create
+        mock_chat_create.return_value.choices = [{"message": {"content": "model-output"}}]
         return mock_chat_create
-    
+
     @pytest.fixture
     def prompt_stack(self):
         prompt_stack = PromptStack()
-        prompt_stack.add_generic_input('generic-input')
-        prompt_stack.add_system_input('system-input')
-        prompt_stack.add_user_input('user-input')
-        prompt_stack.add_assistant_input('assistant-input')
+        prompt_stack.add_generic_input("generic-input")
+        prompt_stack.add_system_input("system-input")
+        prompt_stack.add_user_input("user-input")
+        prompt_stack.add_assistant_input("assistant-input")
         return prompt_stack
-    
+
     @pytest.fixture
     def messages(self):
         return [
-            {'role': 'user', 'content': 'generic-input'},
-            {'role': 'system', 'content': 'system-input'},
-            {'role': 'user', 'content': 'user-input'},
-            {'role': 'assistant', 'content': 'assistant-input'},
+            {"role": "user", "content": "generic-input"},
+            {"role": "system", "content": "system-input"},
+            {"role": "user", "content": "user-input"},
+            {"role": "assistant", "content": "assistant-input"},
         ]
+
 
 class TestOpenAiChatPromptDriver(TestOpenAiChatPromptDriverFixtureMixin):
     def test_init(self):
@@ -47,7 +47,6 @@ class TestOpenAiChatPromptDriver(TestOpenAiChatPromptDriverFixtureMixin):
         # Then
         mock_chat_completion_create.assert_called_once_with(
             model=driver.model,
-            max_tokens=ANY,
             temperature=driver.temperature,
             stop=driver.tokenizer.stop_sequences,
             user=driver.user,
@@ -56,9 +55,64 @@ class TestOpenAiChatPromptDriver(TestOpenAiChatPromptDriverFixtureMixin):
             api_version=driver.api_version,
             api_base=driver.api_base,
             api_type=driver.api_type,
-            messages=messages
+            messages=messages,
         )
-        assert text_artifact.value == 'model-output'
+        assert text_artifact.value == "model-output"
+
+    def test_try_run_with_max_tokens(self, mock_chat_completion_create, prompt_stack, messages):
+        # Given
+        driver = OpenAiChatPromptDriver(
+            model=OpenAiTokenizer.DEFAULT_OPENAI_GPT_3_CHAT_MODEL,
+            max_tokens=1,
+        )
+
+        # When
+        text_artifact = driver.try_run(prompt_stack)
+
+        # Then
+        mock_chat_completion_create.assert_called_once_with(
+            model=driver.model,
+            temperature=driver.temperature,
+            stop=driver.tokenizer.stop_sequences,
+            user=driver.user,
+            api_key=driver.api_key,
+            organization=driver.organization,
+            api_version=driver.api_version,
+            api_base=driver.api_base,
+            api_type=driver.api_type,
+            messages=messages,
+            max_tokens=1,
+        )
+        assert text_artifact.value == "model-output"
+
+    def test_try_run_max_tokens_limited_by_tokenizer(self, mock_chat_completion_create, prompt_stack, messages):
+        # Given
+        max_tokens_request = 9999999
+        driver = OpenAiChatPromptDriver(
+            model=OpenAiTokenizer.DEFAULT_OPENAI_GPT_3_CHAT_MODEL,
+            max_tokens=max_tokens_request,
+        )
+        tokens_left = driver.tokenizer.tokens_left(driver._prompt_stack_to_messages(prompt_stack))
+
+        # When
+        text_artifact = driver.try_run(prompt_stack)
+
+        # Then
+        mock_chat_completion_create.assert_called_once_with(
+            model=driver.model,
+            temperature=driver.temperature,
+            stop=driver.tokenizer.stop_sequences,
+            user=driver.user,
+            api_key=driver.api_key,
+            organization=driver.organization,
+            api_version=driver.api_version,
+            api_base=driver.api_base,
+            api_type=driver.api_type,
+            messages=messages,
+            max_tokens=tokens_left,
+        )
+        assert max_tokens_request > tokens_left
+        assert text_artifact.value == "model-output"
 
     def test_try_run_throws_when_prompt_stack_is_string(self):
         # Given
@@ -68,12 +122,12 @@ class TestOpenAiChatPromptDriver(TestOpenAiChatPromptDriverFixtureMixin):
 
         # When
         with pytest.raises(Exception) as e:
-            driver.try_run('prompt-stack')
+            driver.try_run("prompt-stack")
 
         # Then
         assert e.value.args[0] == "'str' object has no attribute 'inputs'"
 
-    @pytest.mark.parametrize('choices', [[], [1, 2]])
+    @pytest.mark.parametrize("choices", [[], [1, 2]])
     def test_try_run_throws_when_multiple_choices_returned(self, choices, mock_chat_completion_create, prompt_stack):
         # Given
         driver = OpenAiChatPromptDriver(model=OpenAiTokenizer.DEFAULT_OPENAI_GPT_3_CHAT_MODEL)
@@ -84,14 +138,14 @@ class TestOpenAiChatPromptDriver(TestOpenAiChatPromptDriverFixtureMixin):
             driver.try_run(prompt_stack)
 
         # Then
-        e.value.args[0] == 'Completion with more than one choice is not supported yet.'
+        e.value.args[0] == "Completion with more than one choice is not supported yet."
 
     def test_token_count(self, prompt_stack, messages):
         # Given
         mock_tokenizer = Mock()
         mock_tokenizer.token_count.return_value = 42
         driver = OpenAiChatPromptDriver(model=OpenAiTokenizer.DEFAULT_OPENAI_GPT_3_CHAT_MODEL, tokenizer=mock_tokenizer)
-        
+
         # When
         token_count = driver.token_count(prompt_stack)
 
@@ -104,7 +158,7 @@ class TestOpenAiChatPromptDriver(TestOpenAiChatPromptDriverFixtureMixin):
         mock_tokenizer = Mock()
         mock_tokenizer.tokens_left.return_value = 42
         driver = OpenAiChatPromptDriver(model=OpenAiTokenizer.DEFAULT_OPENAI_GPT_3_CHAT_MODEL, tokenizer=mock_tokenizer)
-        
+
         # When
         max_output_tokens = driver.max_output_tokens(messages)
 
@@ -113,4 +167,6 @@ class TestOpenAiChatPromptDriver(TestOpenAiChatPromptDriverFixtureMixin):
         assert max_output_tokens == 42
 
     def test_max_output_tokens_with_max_tokens(self, messages):
-        OpenAiChatPromptDriver(model=OpenAiTokenizer.DEFAULT_OPENAI_GPT_3_CHAT_MODEL, max_tokens=42).max_output_tokens(messages) == 42
+        OpenAiChatPromptDriver(model=OpenAiTokenizer.DEFAULT_OPENAI_GPT_3_CHAT_MODEL, max_tokens=42).max_output_tokens(
+            messages
+        ) == 42
