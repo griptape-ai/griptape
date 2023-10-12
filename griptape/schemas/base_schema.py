4fe392412738669a7337f10626e84d50b8e10b91
Collin Dutter <collin@griptape.ai>
2023-10-12 11:03:54 -0700

    Filter out null values before serializing

--- griptape/schemas/base_schema.py
+++ griptape/schemas/base_schema.py
@@ -1,5 +1,12 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, post_dump
 
 
 class BaseSchema(Schema):
     schema_namespace = fields.Str(allow_none=True)
+
+    @post_dump
+    def remove_null_values(self, data, **kwargs):
+        return {
+            key: value for key, value in data.items()
+            if value is not None
+        }
