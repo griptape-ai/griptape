ea89cd044a117875b3fbe8b707df6f0b101a0dbc
Collin Dutter <collin@griptape.ai>
2023-10-18 11:40:11 -0600

    Simplify none filtering

--- griptape/schemas/base_schema.py
+++ griptape/schemas/base_schema.py
@@ -1,12 +1,5 @@
-from marshmallow import Schema, fields, post_dump
+from marshmallow import Schema, fields
 
 
 class BaseSchema(Schema):
     schema_namespace = fields.Str(allow_none=True)
-
-    @post_dump
-    def remove_null_values(self, data, **kwargs):
-        return {
-            key: value for key, value in data.items()
-            if value is not None
-        }
