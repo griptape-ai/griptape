b17ed80f7015272654c8dd96bbb984121860eb5c
KaushikIyer16 <kaushiiyer@gmail.com>
2023-10-26 16:20:47 -0700

    fix: formatting

--- griptape/drivers/embedding/sagemaker_embedding_driver.py
+++ griptape/drivers/embedding/sagemaker_embedding_driver.py
@@ -4,33 +4,32 @@ from typing import Any
 from attr import define, field, Factory
 from griptape.drivers import BaseEmbeddingDriver
 
+
 @define
 class AmazonSagemakerEmbeddingDriver(BaseEmbeddingDriver):
-
-  endpoint: str = field(kw_only=True)
-  session: boto3.Session = field(
-    default = Factory(lambda: boto3.Session()),
-    kw_only = True
-  )
-  sagemaker_client: Any = field(
-    default = Factory(lambda self: self.session.client("runtime.sagemaker"), takes_self=True),
-    kw_only = True
-  )
-
-  def try_embed_string(self, string: str) -> list[float]:
-
-    # text_inputs can take a list of string and generate a list of embeddings
-    # the length of the embedding key in the response will be the same as the text_inputs array
-    payload = {
-      "text_inputs": string
-    }
-    endpoint_response = self.sagemaker_client.invoke_endpoint(
-        EndpointName=self.endpoint, 
-        ContentType="application/json", 
-        Body=json.dumps(payload).encode("utf-8")
+    endpoint: str = field(kw_only=True)
+    session: boto3.Session = field(
+        default=Factory(lambda: boto3.Session()), kw_only=True
+    )
+    sagemaker_client: Any = field(
+        default=Factory(
+            lambda self: self.session.client("runtime.sagemaker"),
+            takes_self=True,
+        ),
+        kw_only=True,
     )
 
-    response = json.loads(endpoint_response.get("Body").read().decode("utf-8"))
-    return response.get("embedding")[0]
+    def try_embed_string(self, string: str) -> list[float]:
+        # text_inputs can take a list of string and generate a list of embeddings
+        # the length of the embedding key in the response will be the same as the text_inputs array
+        payload = {"text_inputs": string}
+        endpoint_response = self.sagemaker_client.invoke_endpoint(
+            EndpointName=self.endpoint,
+            ContentType="application/json",
+            Body=json.dumps(payload).encode("utf-8"),
+        )
 
-
+        response = json.loads(
+            endpoint_response.get("Body").read().decode("utf-8")
+        )
+        return response.get("embedding")[0]
--- tests/unit/drivers/embedding/test_sagemaker_embedding_driver.py
+++ tests/unit/drivers/embedding/test_sagemaker_embedding_driver.py
@@ -2,24 +2,27 @@ import pytest
 from unittest import mock
 from griptape.drivers import AmazonSagemakerEmbeddingDriver
 
+
 class TestAmazonSagemakerEmbeddingDriver:
-  @pytest.fixture(autouse=True)
-  def mock_session(self, mocker):
-    fake_embeddings = '{"embedding": [[0, 1, 0]]}'.encode("utf-8")   
-    mock_session_class = mocker.patch("boto3.Session")
-    mock_session_object = mock.Mock()
-    mock_client = mock.Mock()
-    mock_response = mock.Mock()
+    @pytest.fixture(autouse=True)
+    def mock_session(self, mocker):
+        fake_embeddings = '{"embedding": [[0, 1, 0]]}'.encode("utf-8")
+        mock_session_class = mocker.patch("boto3.Session")
+        mock_session_object = mock.Mock()
+        mock_client = mock.Mock()
+        mock_response = mock.Mock()
 
-    mock_response.get().read.return_value = fake_embeddings
-    mock_client.invoke_endpoint.return_value = mock_response
-    mock_session_object.client.return_value = mock_client
-    mock_session_class.return_value = mock_session_object
-  
-  def test_init(self):
-    assert AmazonSagemakerEmbeddingDriver(endpoint="test-endpoint", dimensions=4096)
+        mock_response.get().read.return_value = fake_embeddings
+        mock_client.invoke_endpoint.return_value = mock_response
+        mock_session_object.client.return_value = mock_client
+        mock_session_class.return_value = mock_session_object
 
-  def test_try_embed_string(self):
-      assert AmazonSagemakerEmbeddingDriver(endpoint="test-endpoint", dimensions=4096).try_embed_string(
-          "foobar"
-      ) == [0, 1, 0]
+    def test_init(self):
+        assert AmazonSagemakerEmbeddingDriver(
+            endpoint="test-endpoint", dimensions=4096
+        )
+
+    def test_try_embed_string(self):
+        assert AmazonSagemakerEmbeddingDriver(
+            endpoint="test-endpoint", dimensions=4096
+        ).try_embed_string("foobar") == [0, 1, 0]
