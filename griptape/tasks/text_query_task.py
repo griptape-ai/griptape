ccfa184c26ce17f714eaa33d5fa020297724cdc3
Collin Dutter <collin@griptape.ai>
2023-11-02 15:47:57 -0700

    Add namespace to text query task

--- griptape/tasks/text_query_task.py
+++ griptape/tasks/text_query_task.py
@@ -1,24 +1,25 @@
 from attr import define, field, Factory
+from typing import Any
 from griptape.artifacts import TextArtifact
 from griptape.engines import BaseQueryEngine
-from griptape.loaders import BaseLoader, TextLoader
+from griptape.loaders import  TextLoader
 from griptape.tasks import BaseTextInputTask
 
 
 @define
 class TextQueryTask(BaseTextInputTask):
     query_engine: BaseQueryEngine = field(kw_only=True)
-    loader: BaseLoader = field(
+    loader: TextLoader = field(
         default=Factory(lambda: TextLoader()), kw_only=True
     )
+    namespace: str = field(kw_only=True)
 
     def run(self) -> TextArtifact:
-        return self.query_engine.query(self.input.to_text())
+        return self.query_engine.query(self.input.to_text(), namespace=self.namespace)
 
-    def load(self, content: any, namespace: str) -> list[TextArtifact]:
-        result = self.loader.load(content)
-        artifacts = result if isinstance(result, list) else [result]
+    def load(self, content: Any) -> list[TextArtifact]:
+        artifacts = self.loader.load(content)
 
-        self.query_engine.upsert_text_artifacts(artifacts, namespace=namespace)
+        self.query_engine.upsert_text_artifacts(artifacts, namespace=self.namespace)
 
         return artifacts
