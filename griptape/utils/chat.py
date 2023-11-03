e0e164b97c16ded18bd2c52935a154ec5a9426e0
mattma1970 <mattma1970@gmail.com>
2023-11-03 01:48:05 +0000

    f/b from PR

--- griptape/utils/chat.py
+++ griptape/utils/chat.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING, Optional, Callable
 from attr import define, field, Factory
+from copy import deepcopy
 
 if TYPE_CHECKING:
     from griptape.structures import Structure
-from griptape.utils import Stream
+from griptape.utils.stream import Stream
 
 
-@define
+@define(frozen=True)
 class Chat:
     structure: Structure = field()
     exit_keywords: list[str] = field(default=["exit"], kw_only=True)
@@ -16,39 +17,39 @@ class Chat:
     intro_text: Optional[str] = field(default=None, kw_only=True)
     prompt_prefix: str = field(default="Q: ", kw_only=True)
     response_prefix: str = field(default="A: ", kw_only=True)
-    output_fn: Callable[[str], None] = field(
-        default=lambda x: print(x, end=""), kw_only=True
+    streaming_output_fn: Callable[[str], None] = field(
+        default=Factory(lambda: lambda x: print(x, end="")), kw_only=True
     )
-    stream: bool = field(default=False, kw_only=True)
+    output_fn: Callable[[str], None] = field(
+        default=Factory(lambda: print), kw_only=True
+    )
 
     def start(self, initial_turn: str = None) -> None:
         if initial_turn:
-            self.output_fn(initial_turn + "\n")
-
-        self.structure.stream = self.stream
-        self.structure.prompt_driver.stream = self.stream
-        if self.stream:
-            self.structure = Stream(self.structure)
+            self.output_fn(initial_turn)
 
         if self.intro_text:
-            self.output_fn(self.intro_text + "\n")
+            self.output_fn(self.intro_text)
         while True:
             question = input(self.prompt_prefix)
 
             if question.lower() in self.exit_keywords:
-                self.output_fn(self.exiting_text + "\n")
+                self.output_fn(self.exiting_text)
                 break
             else:
-                self.output_fn(self.processing_text + "\n")
+                self.output_fn(self.processing_text)
 
-            if self.stream:
-                self.output_fn(f"{self.response_prefix}")
-                self.structure.structure.event_listeners = (
+            if self.structure.stream:
+                self.structure.event_listeners = (
                     []
-                )  # To address https://github.com/griptape-ai/griptape/issues/408
-                for chunk in self.structure.run(question):
-                    self.output_fn(chunk.value)
-                self.output_fn("\n")
+                )  # To address https://github.com/griptape-ai/griptape/issues/408 (pending PR)
+                first_chunk: bool = True
+                for chunk in Stream(self.structure).run(question):
+                    if first_chunk:
+                        self.streaming_output_fn(f"{self.response_prefix}")
+                    first_chunk = False
+                    self.streaming_output_fn(chunk.value)
+                self.streaming_output_fn("\n")
             else:
                 self.output_fn(
                     f"{self.response_prefix}{self.structure.run(question).output.to_text()}"
