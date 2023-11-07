48b00e38b55b04641b671fdcb8dd542d238ea848
Vasily Vasinov <des.elyon@gmail.com>
2023-11-07 12:06:00 -0700

    Remove API generator

--- griptape/api/__init__.py
+++
@@ -1,3 +0,0 @@
-from .tool_api_generator import ToolApiGenerator
-
-__all__ = ["ToolApiGenerator"]
--- griptape/api/extensions/__init__.py
+++
@@ -1,4 +0,0 @@
-from .base_api_extension import BaseApiExtension
-from .chatgpt_plugin_api_extension import ChatGptPluginApiExtension
-
-__all__ = ["BaseApiExtension", "ChatGptPluginApiExtension"]
