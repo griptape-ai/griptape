eab92cc81a989579b7102e34132ddd40cdddcc5e
Vasily Vasinov <des.elyon@gmail.com>
2023-11-09 17:02:49 -0700

    Fix formatting

--- griptape/tools/tool_memory_client/tool.py
+++ griptape/tools/tool_memory_client/tool.py
@@ -47,8 +47,6 @@ class ToolMemoryClient(BaseTool):
         query = params["values"]["query"]
 
         if memory:
-            return memory.query_namespace(
-                namespace=artifact_namespace, query=query
-            )
+            return memory.query_namespace(namespace=artifact_namespace, query=query)
         else:
             return ErrorArtifact("memory not found")
