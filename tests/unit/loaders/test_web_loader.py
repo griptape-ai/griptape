0be868b53dca84f130ce29a6e026dfc8a1a67ecb
Collin Dutter <collin@griptape.ai>
2023-11-16 11:12:58 -0800

    Add check for empty pages (#453)

--- tests/unit/loaders/test_web_loader.py
+++ tests/unit/loaders/test_web_loader.py
@@ -41,3 +41,21 @@ class TestWebLoader:
         assert "foobar" in [a.value for artifact_list in artifacts.values() for a in artifact_list][0].lower()
 
         assert list(artifacts.values())[0][0].embedding == [0, 1]
+
+    def test_empty_page_string_response(self, loader, mocker):
+        fake_response = {"status": 200, "data": "foobar"}
+
+        mocker.patch("trafilatura.fetch_url", return_value=fake_response)
+        mocker.patch("trafilatura.extract", return_value="")
+
+        with pytest.raises(Exception, match="can't extract page"):
+            loader.load("https://example.com/")
+
+    def test_empty_page_none_response(self, loader, mocker):
+        fake_response = {"status": 200, "data": "foobar"}
+
+        mocker.patch("trafilatura.fetch_url", return_value=fake_response)
+        mocker.patch("trafilatura.extract", return_value=None)
+
+        with pytest.raises(Exception, match="can't extract page"):
+            loader.load("https://example.com/")
