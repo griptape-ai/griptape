80e0f66339a5ef4123a128db612dc90d495364b1
Andrew French <andrew@afren.ch>
2023-11-20 12:55:06 -0800

    Support writing image to disk in tool

--- griptape/tools/image_generator/tool.py
+++ griptape/tools/image_generator/tool.py
@@ -1,4 +1,8 @@
 from __future__ import annotations
+
+from os import path
+from typing import Optional
+
 from attrs import define, field
 from schema import Schema, Literal
 from griptape.artifacts import ErrorArtifact, ImageArtifact
@@ -9,7 +13,16 @@ from griptape.utils.decorators import activity
 
 @define
 class ImageGenerator(BaseTool):
+    """ImageGenerator is a tool that can be used to generate an image.
+
+    Attributes:
+        image_generation_engine: The engine used to generate the image.
+        output_dir: If provided, the generated image will be written to disk in output_dir.
+
+    """
+
     image_generation_engine: ImageGenerationEngine = field(kw_only=True)
+    output_dir: Optional[str] = field(default=None, kw_only=True)
 
     @activity(
         config={
@@ -33,7 +46,19 @@ class ImageGenerator(BaseTool):
         negative_prompts = params["values"]["negative_prompts"]
 
         try:
-            return self.image_generation_engine.generate_image(prompts=prompts, negative_prompts=negative_prompts)
+            image_artifact = self.image_generation_engine.generate_image(
+                prompts=prompts, negative_prompts=negative_prompts
+            )
+
+            if self.output_dir is not None:
+                self._write_to_file(image_artifact)
+
+            return image_artifact
 
         except Exception as e:
             return ErrorArtifact(value=str(e))
+
+    def _write_to_file(self, image_artifact: ImageArtifact) -> None:
+        outfile = path.join(self.output_dir, image_artifact.name)
+        with open(outfile, "wb") as f:
+            f.write(image_artifact.value)
