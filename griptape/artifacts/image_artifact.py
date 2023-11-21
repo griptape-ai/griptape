59bf741f2e72f2fd19884fb84a1ba2ef281373d3
Andrew French <andrew@afren.ch>
2023-11-20 17:49:45 -0800

    Serialize image artifacts as base64 while retaining underlying bytes

--- griptape/artifacts/image_artifact.py
+++ griptape/artifacts/image_artifact.py
@@ -2,15 +2,32 @@ from __future__ import annotations
 from typing import Optional
 from attr import define, field
 from griptape.artifacts import BlobArtifact
+import base64
 
 
 @define(frozen=True)
 class ImageArtifact(BlobArtifact):
+    """ImageArtifact is a type of BlobArtifact that represents an image.
+
+    Attributes:
+        value: Raw bytes representing the image.
+        name: Artifact name, generated using creation time and a random string.
+        mime_type: The mime type of the image, like image/png or image/jpeg.
+        width: The width of the image in pixels.
+        height: The height of the image in pixels.
+        model: Optionally specify the model used to generate the image.
+        prompt: Optionally specify the prompt used to generate the image.
+    """
+
     mime_type: str = field(kw_only=True)
     width: int = field(kw_only=True)
     height: int = field(kw_only=True)
     model: Optional[str] = field(default=None, kw_only=True)
     prompt: Optional[str] = field(default=None, kw_only=True)
+
+    @property
+    def base64(self) -> str:
+        return base64.b64encode(self.value).decode("utf-8")
 
     def to_text(self) -> str:
         return f"Image, dimensions: {self.width}x{self.height}, type: {self.mime_type}, size: {len(self.value)} bytes"
--- griptape/schemas/artifacts/image_artifact_schema.py
+++ griptape/schemas/artifacts/image_artifact_schema.py
@@ -3,7 +3,7 @@ from griptape.schemas import BaseArtifactSchema
 
 
 class ImageArtifactSchema(BaseArtifactSchema):
-    value = fields.Str()
+    base64 = fields.Str()
     mime_type = fields.Str()
     width = fields.Int()
     height = fields.Int()
@@ -13,5 +13,12 @@ class ImageArtifactSchema(BaseArtifactSchema):
     @post_load
     def make_obj(self, data, **kwargs):
         from griptape.artifacts import ImageArtifact
+
+        if "base64" in data:
+            import base64
+
+            image_bytes = base64.b64decode(data["base64"])
+            data["value"] = image_bytes
+            del data["base64"]
 
         return ImageArtifact(**data)
