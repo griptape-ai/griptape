2df10d30fb134b9bb474b8ff7b507f66670fbb5c
Collin Dutter <collin@griptape.ai>
2023-12-05 09:12:35 -0800

    Update example

--- hello-world.py
+++ hello-world.py
@@ -1,3 +1,4 @@
+import random
 from griptape.drivers import OpenAiDalleImageGenerationDriver
 from griptape.structures import Pipeline
 from griptape.tools import WebScraper, FileManager
@@ -5,24 +6,28 @@ from griptape.tasks import ToolTask, TextSummaryTask, ImageGenerationTask
 from griptape.engines import ImageGenerationEngine
 from griptape.drivers import OpenAiChatPromptDriver
 
+
+def random_input():
+    if random.random() < 0.5:
+        return "Save the image artifact to a file called griptape_0.png in the images directory"
+    else:
+        return "Save the image artifact to a file called griptape_1.png in the images directory"
+
+
 pipeline = Pipeline(
     prompt_driver=OpenAiChatPromptDriver(model="gpt-3.5-turbo"),
     tasks=[
-        ToolTask("Scrape https://griptape.ai", output_artifact_namespace="WebResults", tool=WebScraper()),
-        TextSummaryTask(input_artifact_namespace="WebResults", output_artifact_namespace="WebSummary", off_prompt=True),
+        ToolTask("Scrape https://griptape.ai", tool=WebScraper(), id="WebResults"),
+        TextSummaryTask(input_generator_fn=lambda: pipeline.find_task("WebResults"), off_prompt=True, id="WebSummary"),
         ImageGenerationTask(
-            input_artifact_namespace="WebSummary",
-            output_artifact_namespace="SummaryImage",
+            id="ImageGeneration",
+            input_generator_fn=lambda: pipeline.find_task("WebSummary"),
             image_generation_engine=ImageGenerationEngine(
                 image_generation_driver=OpenAiDalleImageGenerationDriver(model="dall-e-3")
             ),
             off_prompt=True,
         ),
-        ToolTask(
-            "Save the image artifact to a file called griptape.png in the images directory",
-            off_prompt=False,
-            tool=FileManager(),
-        ),
+        ToolTask(input_generator_fn=random_input, off_prompt=False, tool=FileManager()),
     ],
 )
 
