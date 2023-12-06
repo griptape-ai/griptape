2bc8a29445dc98049953b05b567642260bae73b1
Collin Dutter <collin@griptape.ai>
2023-12-06 11:26:28 -0800

    Subclass SimpleTokenizer

--- griptape/tokenizers/bedrock_jurassic_tokenizer.py
+++ griptape/tokenizers/bedrock_jurassic_tokenizer.py
@@ -1,24 +1,14 @@
 from __future__ import annotations
 from attr import define, field
-from .base_tokenizer import BaseTokenizer
 from .simple_tokenizer import SimpleTokenizer
 
 
 @define(frozen=True)
-class BedrockJurassicTokenizer(BaseTokenizer):
+class BedrockJurassicTokenizer(SimpleTokenizer):
     DEFAULT_MODEL = "ai21.j2-ultra-v1"
     DEFAULT_MAX_TOKENS = 8192
     DEFAULT_CHARACTERS_PER_TOKEN = 6  # https://docs.aws.amazon.com/bedrock/latest/userguide/model-customization-prepare.html#model-customization-prepare-finetuning
 
+    characters_per_token: int = field(default=DEFAULT_CHARACTERS_PER_TOKEN, kw_only=True)
+    max_tokens: int = field(default=DEFAULT_MAX_TOKENS, kw_only=True)
     model: str = field(kw_only=True)
-    tokenizer: SimpleTokenizer = field(
-        default=SimpleTokenizer(max_tokens=DEFAULT_MAX_TOKENS, characters_per_token=DEFAULT_CHARACTERS_PER_TOKEN),
-        kw_only=True,
-    )
-
-    @property
-    def max_tokens(self) -> int:
-        return self.DEFAULT_MAX_TOKENS
-
-    def count_tokens(self, text: str) -> int:
-        return self.tokenizer.count_tokens(text)
--- griptape/tokenizers/bedrock_titan_tokenizer.py
+++ griptape/tokenizers/bedrock_titan_tokenizer.py
@@ -1,24 +1,14 @@
 from __future__ import annotations
 from attr import define, field
-from .base_tokenizer import BaseTokenizer
 from .simple_tokenizer import SimpleTokenizer
 
 
 @define(frozen=True)
-class BedrockTitanTokenizer(BaseTokenizer):
+class BedrockTitanTokenizer(SimpleTokenizer):
     DEFAULT_MODEL = "amazon.titan-text-express-v1"
     DEFAULT_CHARACTERS_PER_TOKEN = 6  # https://docs.aws.amazon.com/bedrock/latest/userguide/model-customization-prepare.html#model-customization-prepare-finetuning
     DEFAULT_MAX_TOKENS = 4096
 
+    characters_per_token: int = field(default=DEFAULT_CHARACTERS_PER_TOKEN, kw_only=True)
+    max_tokens: int = field(default=DEFAULT_MAX_TOKENS, kw_only=True)
     model: str = field(kw_only=True)
-    tokenizer: SimpleTokenizer = field(
-        default=SimpleTokenizer(max_tokens=DEFAULT_MAX_TOKENS, characters_per_token=DEFAULT_CHARACTERS_PER_TOKEN),
-        kw_only=True,
-    )
-
-    @property
-    def max_tokens(self) -> int:
-        return self.DEFAULT_MAX_TOKENS
-
-    def count_tokens(self, text: str) -> int:
-        return self.tokenizer.count_tokens(text)
