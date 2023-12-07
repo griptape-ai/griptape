190fc4ac647471cf082c1b476fac19e66127b3a9
Collin Dutter <collin@griptape.ai>
2023-12-06 16:39:04 -0800

    Fix incorrectly named file

--- griptape/drivers/__init__.py
+++ griptape/drivers/__init__.py
@@ -19,7 +19,7 @@ from .embedding.base_embedding_driver import BaseEmbeddingDriver
 from .embedding.openai_embedding_driver import OpenAiEmbeddingDriver
 from .embedding.azure_openai_embedding_driver import AzureOpenAiEmbeddingDriver
 from .embedding.bedrock_titan_embedding_driver import BedrockTitanEmbeddingDriver
-from .embedding.huggingface_hub_embedding_driver import HuggingFaceHubEmbeddingDriver
+from .embedding.hugging_face_hub_embedding_driver import HuggingFaceHubEmbeddingDriver
 
 from .vector.base_vector_store_driver import BaseVectorStoreDriver
 from .vector.local_vector_store_driver import LocalVectorStoreDriver
