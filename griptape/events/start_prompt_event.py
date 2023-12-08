0b60e396ec57a95faef06a04fdf7ff1800c34dba
Collin Dutter <collin@griptape.ai>
2023-12-08 12:27:34 -0800

    Fix circular dependency (#491)

--- griptape/events/start_prompt_event.py
+++ griptape/events/start_prompt_event.py
@@ -1,7 +1,11 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
 from attrs import define
-from griptape.utils import PromptStack
 from attrs import field
 from griptape.events.base_prompt_event import BasePromptEvent
+
+if TYPE_CHECKING:
+    from griptape.utils import PromptStack
 
 
 @define
