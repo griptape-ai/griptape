b30287be51b5e2dd2fb3524a1f8b1ab18c8bd9a1
Collin Dutter <collin@griptape.ai>
2023-12-13 11:32:05 -0800

    Revert import removal

--- griptape/drivers/embedding/azure_openai_embedding_driver.py
+++ griptape/drivers/embedding/azure_openai_embedding_driver.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from typing import Optional
 from attr import define, field, Factory
 from griptape.drivers import OpenAiEmbeddingDriver
 from griptape.tokenizers import OpenAiTokenizer
