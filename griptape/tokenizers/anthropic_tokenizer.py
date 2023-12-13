870fc82907040f82d90ba6af1f368fce5bdcbee2
Collin Dutter <collin@griptape.ai>
2023-12-13 14:03:48 -0800

    Upgrade anthropic sdk, add 2.1 support

--- griptape/tokenizers/anthropic_tokenizer.py
+++ griptape/tokenizers/anthropic_tokenizer.py
@@ -1,20 +1,25 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
 from attr import define, field
 from griptape.utils import import_optional_dependency
 from griptape.tokenizers import BaseTokenizer
 
+if TYPE_CHECKING:
+    from anthropic import Anthropic
+
 
 @define(frozen=True)
 class AnthropicTokenizer(BaseTokenizer):
-    DEFAULT_MODEL = "claude-2"
-    DEFAULT_MAX_TOKENS = 100000
+    MODEL_PREFIXES_TO_MAX_TOKENS = {"claude-2.1": 200000, "claude": 100000}
 
     model: str = field(kw_only=True)
+    client: Anthropic = field(default=import_optional_dependency("anthropic").Anthropic(), kw_only=True)
 
     @property
     def max_tokens(self) -> int:
-        return self.DEFAULT_MAX_TOKENS
+        tokens = next(v for k, v in self.MODEL_PREFIXES_TO_MAX_TOKENS.items() if self.model.startswith(k))
+
+        return tokens
 
     def count_tokens(self, text: str) -> int:
-        anthropic = import_optional_dependency("anthropic")
-
-        return len(anthropic._client.sync_get_tokenizer().encode(text).ids)
+        return self.client.count_tokens(text)
