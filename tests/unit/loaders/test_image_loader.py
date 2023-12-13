191fd31bde0593deb437357b4aedb61ad93d2e1c
Andrew French <andrew@afren.ch>
2023-12-13 09:28:22 -0800

    review: add test proving jpg to png loader

--- tests/unit/loaders/test_image_loader.py
+++ tests/unit/loaders/test_image_loader.py
@@ -25,3 +25,15 @@ class TestImageLoader:
         assert artifact.height == 1024
         assert artifact.width == 1024
         assert artifact.mime_type == "image/png"
+
+    def test_load_jpg(self, loader):
+        path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "../../resources/pig-balloon.jpg")
+
+        artifact = loader.load(path)
+
+        assert isinstance(artifact, ImageArtifact)
+        assert artifact.name == "pig-balloon.jpg"
+        assert artifact.dir_name.endswith("/resources")
+        assert artifact.height == 1024
+        assert artifact.width == 1024
+        assert artifact.mime_type == "image/png"
