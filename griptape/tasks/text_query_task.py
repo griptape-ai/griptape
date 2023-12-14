f7ffc6f500dd8735152e80a29d21729002638996
Vasily Vasinov <des.elyon@gmail.com>
2023-12-14 14:54:12 -0700

    Remove TextQueryTask.loader

--- griptape/tasks/text_query_task.py
+++ griptape/tasks/text_query_task.py
@@ -1,15 +1,13 @@
-from attr import define, field, Factory
+from attr import define, field
 from typing import Optional
 from griptape.artifacts import TextArtifact
 from griptape.engines import BaseQueryEngine
-from griptape.loaders import TextLoader
 from griptape.tasks import BaseTextInputTask
 
 
 @define
 class TextQueryTask(BaseTextInputTask):
     query_engine: BaseQueryEngine = field(kw_only=True)
-    loader: TextLoader = field(default=Factory(lambda: TextLoader()), kw_only=True)
     namespace: Optional[str] = field(default=None, kw_only=True)
 
     def run(self) -> TextArtifact:
