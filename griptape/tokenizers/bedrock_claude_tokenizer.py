a3047914b6f76aa1d5c634891b388947c5b87876
Collin Dutter <collin@griptape.ai>
2023-12-14 09:48:40 -0800

    Simplify prefixes

--- griptape/tokenizers/bedrock_claude_tokenizer.py
+++ griptape/tokenizers/bedrock_claude_tokenizer.py
@@ -4,9 +4,4 @@ from griptape.tokenizers import AnthropicTokenizer
 
 @define(frozen=True)
 class BedrockClaudeTokenizer(AnthropicTokenizer):
-    MODEL_PREFIXES_TO_MAX_TOKENS = {
-        "anthropic.claude-v2:1": 200000,
-        "anthropic.claude-v2": 100000,
-        "anthropic.claude-v1": 100000,
-        "anthropic.claude-instant-v1": 100000,
-    }
+    MODEL_PREFIXES_TO_MAX_TOKENS = {"anthropic.claude-v2:1": 200000, "anthropic.claude": 100000}
