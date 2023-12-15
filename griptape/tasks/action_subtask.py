4f9632c595ba04bdec1f0a6f790987494d77e0b9
Collin Dutter <collin@griptape.ai>
2023-12-15 10:11:21 -0800

    Fix action subtasks

--- griptape/tasks/action_subtask.py
+++ griptape/tasks/action_subtask.py
@@ -33,6 +33,7 @@ class ActionSubtask(PromptTask):
         },
     )
 
+    _input: str | None = field(default=None)
     parent_task_id: str | None = field(default=None, kw_only=True)
     thought: str | None = field(default=None, kw_only=True)
     action_name: str | None = field(default=None, kw_only=True)
@@ -41,6 +42,10 @@ class ActionSubtask(PromptTask):
 
     _tool: BaseTool | None = None
     _memory: TaskMemory | None = None
+
+    @property
+    def input(self) -> TextArtifact:
+        return TextArtifact(self._input)
 
     @property
     def origin_task(self) -> ActionSubtaskOriginMixin | None:
