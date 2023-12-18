f6698fc140cc9b31fff841caa2cfc44aa8a513e4
Andrew French <andrew@afren.ch>
2023-12-18 15:54:44 -0800

    Add test for callable input in TextToImageTask

--- tests/unit/tasks/test_text_to_image_task.py
+++ tests/unit/tasks/test_text_to_image_task.py
@@ -1,10 +1,21 @@
 from unittest.mock import Mock
 
-from griptape.tasks import TextToImageTask
+from griptape.tasks import TextToImageTask, BaseTask
+from griptape.artifacts import TextArtifact
 
 
 class TestTextToImageTask:
-    def test_accepts_string_input(self):
+    def test_string_input(self):
         task = TextToImageTask(input="string input", image_generation_engine=Mock())
 
         assert task.input.value == "string input"
+
+    def test_callable_input(self):
+        input_artifact = TextArtifact("some text input")
+
+        def callable(task: BaseTask) -> TextArtifact:
+            return input_artifact
+
+        task = TextToImageTask(input=callable, image_generation_engine=Mock())
+
+        assert task.input == input_artifact
