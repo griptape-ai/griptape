3339ff51f573d6198867b2b425a474675afe7bb5
Collin Dutter <collin@griptape.ai>
2023-12-22 09:15:41 -0800

    Fix/activity type errors (#522)

--- griptape/drivers/prompt_model/bedrock_claude_prompt_model_driver.py
+++ griptape/drivers/prompt_model/bedrock_claude_prompt_model_driver.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import Optional
 import json
 from attr import define, field
@@ -12,7 +13,7 @@ class BedrockClaudePromptModelDriver(BasePromptModelDriver):
     top_p: float = field(default=0.999, kw_only=True)
     top_k: int = field(default=250, kw_only=True)
     _tokenizer: BedrockClaudeTokenizer = field(default=None, kw_only=True)
-    prompt_driver: Optional[AmazonBedrockPromptDriver] = field(default=None, kw_only=True)
+    prompt_driver: AmazonBedrockPromptDriver | None = field(default=None, kw_only=True)
 
     @property
     def tokenizer(self) -> BedrockClaudeTokenizer:
@@ -60,7 +61,10 @@ class BedrockClaudePromptModelDriver(BasePromptModelDriver):
             "top_k": self.top_k,
         }
 
-    def process_output(self, response_body: bytes) -> TextArtifact:
-        body = json.loads(response_body.decode())
+    def process_output(self, output: list[dict] | str | bytes) -> TextArtifact:
+        if isinstance(output, bytes):
+            body = json.loads(output.decode())
+        else:
+            raise Exception("Output must be bytes.")
 
         return TextArtifact(body["completion"])
