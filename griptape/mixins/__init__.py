a8753ce2274bd7605f467cc1aa57c9a53cab8ab7
Andrew French <andrew@afren.ch>
2024-01-04 15:26:11 -0800

    Refaactor task rules to RuleMixin

--- griptape/mixins/__init__.py
+++ griptape/mixins/__init__.py
@@ -1,5 +1,6 @@
 from .activity_mixin import ActivityMixin
 from .exponential_backoff_mixin import ExponentialBackoffMixin
 from .action_subtask_origin_mixin import ActionSubtaskOriginMixin
+from .rule_mixin import RuleMixin
 
-__all__ = ["ActivityMixin", "ExponentialBackoffMixin", "ActionSubtaskOriginMixin"]
+__all__ = ["ActivityMixin", "ExponentialBackoffMixin", "ActionSubtaskOriginMixin", "RuleMixin"]
--- tests/mocks/mock_image_generation_task.py
+++ tests/mocks/mock_image_generation_task.py
@@ -6,5 +6,7 @@ from griptape.tasks import BaseImageGenerationTask
 
 @define
 class MockImageGenerationTask(BaseImageGenerationTask):
+    input: str = field(default="input")  # pyright: ignore
+
     def run(self) -> ImageArtifact:
         return ImageArtifact(value=b"image data", mime_type="image/png", width=512, height=512)
