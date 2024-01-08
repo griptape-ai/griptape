c8c3fa2bac8aa656529d33edd975dbd1fe767b3e
Aodhan Roche <aodhan@griptape.ai>
2024-01-08 10:06:18 -0800

    Added new test for no-op, added check for ErrorArtifact type, and cleaned up messages

--- tests/unit/tasks/test_python_task.py
+++ tests/unit/tasks/test_python_task.py
@@ -1,17 +1,22 @@
-from griptape.artifacts import BaseArtifact, TextArtifact
-from griptape.tasks import PythonTask, BaseTextInputTask, BaseTask
-from tests.mocks.mock_prompt_driver import MockPromptDriver
+from griptape.artifacts import BaseArtifact, TextArtifact, ErrorArtifact
 from griptape.structures import Pipeline
+from griptape.tasks import PythonTask, BaseTask
+from tests.mocks.mock_prompt_driver import MockPromptDriver
 
 
 def hello_world(task: BaseTask) -> BaseArtifact:
     greeting = "Hello World!"
-    # We don't want to create an artifact, so just pass the parent task's input back
     return TextArtifact(greeting)
 
 
+def non_outputting(task: BaseTask) -> BaseArtifact:
+    # If your task function doesn't have an output, return task.input
+    temp = task.input
+    return temp
+
+
 def deliberate_exception(task: BaseTask) -> BaseArtifact:
-    raise ValueError("ugh blub")
+    raise ValueError("Intentional Error")
 
 
 class TestPromptSubtask:
@@ -20,7 +25,19 @@ class TestPromptSubtask:
 
         assert task.run().value == "Hello World!"
 
+    # Using a Pipeline
+    # Overriding the input because we are implementing the task not the Pipeline
+    def test_noop_fn(self):
+        pipeline = Pipeline(prompt_driver=MockPromptDriver())
+        task = PythonTask("No Op", run_fn=non_outputting)
+        pipeline.add_task(task)
+        temp = task.run()
+        assert temp.value == "No Op"
+
     def test_error_fn(self):
         task = PythonTask(run_fn=deliberate_exception)
 
-        assert task.run().value == "error during Python Task execution: ugh blub"
+        result = task.run()
+
+        assert isinstance(result, ErrorArtifact)
+        assert result.value == "error during Python Task execution: Intentional Error"
