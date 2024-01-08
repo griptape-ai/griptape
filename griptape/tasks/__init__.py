fc504836e741f44a2b6132ceee123c6794e45f3e
Aodhan Roche <aodhan@griptape.ai>
2024-01-08 13:53:56 -0800

    Renamed CallableTask

--- griptape/tasks/__init__.py
+++ griptape/tasks/__init__.py
@@ -12,7 +12,7 @@ from .text_to_image_task import TextToImageTask
 from .image_inpainting_task import ImageInpaintingTask
 from .image_outpainting_task import ImageOutpaintingTask
 from .image_variation_task import ImageVariationTask
-from .python_task import CallableTask
+from .callable_task import CallableTask
 
 __all__ = [
     "BaseTask",
