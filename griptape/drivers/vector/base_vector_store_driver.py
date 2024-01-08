b259e2f8db3dfaea0599b9c0b875853039aac2e4
Collin Dutter <collin@griptape.ai>
2024-01-08 13:49:55 -0800

    Fix MongoDb Driver Issues (#542)

--- griptape/drivers/vector/base_vector_store_driver.py
+++ griptape/drivers/vector/base_vector_store_driver.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from abc import ABC, abstractmethod
 from concurrent import futures
 from dataclasses import dataclass
@@ -5,7 +6,7 @@ from typing import Optional
 from attr import define, field, Factory
 from griptape import utils
 from griptape.artifacts import TextArtifact
-from griptape.drivers import BaseEmbeddingDriver, OpenAiEmbeddingDriver
+from griptape.drivers import BaseEmbeddingDriver
 
 
 @define
@@ -17,21 +18,21 @@ class BaseVectorStoreDriver(ABC):
         id: str
         vector: list[float]
         score: float
-        meta: Optional[dict] = None
-        namespace: Optional[str] = None
+        meta: dict | None = None
+        namespace: str | None = None
 
     @dataclass
     class Entry:
         id: str
         vector: list[float]
-        meta: Optional[dict] = None
-        namespace: Optional[str] = None
+        meta: dict | None = None
+        namespace: str | None = None
 
     embedding_driver: BaseEmbeddingDriver = field(kw_only=True)
     futures_executor: futures.Executor = field(default=Factory(lambda: futures.ThreadPoolExecutor()), kw_only=True)
 
     def upsert_text_artifacts(
-        self, artifacts: dict[str, list[TextArtifact]], meta: Optional[dict] = None, **kwargs
+        self, artifacts: dict[str, list[TextArtifact]], meta: dict | None = None, **kwargs
     ) -> None:
         utils.execute_futures_dict(
             {
@@ -42,7 +43,7 @@ class BaseVectorStoreDriver(ABC):
         )
 
     def upsert_text_artifact(
-        self, artifact: TextArtifact, namespace: Optional[str] = None, meta: Optional[dict] = None, **kwargs
+        self, artifact: TextArtifact, namespace: str | None = None, meta: dict | None = None, **kwargs
     ) -> str:
         if not meta:
             meta = {}
@@ -59,45 +60,45 @@ class BaseVectorStoreDriver(ABC):
     def upsert_text(
         self,
         string: str,
-        vector_id: Optional[str] = None,
-        namespace: Optional[str] = None,
-        meta: Optional[dict] = None,
-        **kwargs
+        vector_id: str | None = None,
+        namespace: str | None = None,
+        meta: dict | None = None,
+        **kwargs,
     ) -> str:
         return self.upsert_vector(
             self.embedding_driver.embed_string(string),
             vector_id=vector_id,
             namespace=namespace,
             meta=meta if meta else {},
-            **kwargs
+            **kwargs,
         )
 
     @abstractmethod
     def upsert_vector(
         self,
         vector: list[float],
-        vector_id: Optional[str] = None,
-        namespace: Optional[str] = None,
-        meta: Optional[dict] = None,
-        **kwargs
+        vector_id: str | None = None,
+        namespace: str | None = None,
+        meta: dict | None = None,
+        **kwargs,
     ) -> str:
         ...
 
     @abstractmethod
-    def load_entry(self, vector_id: str, namespace: Optional[str] = None) -> Entry:
+    def load_entry(self, vector_id: str, namespace: str | None = None) -> Entry | None:
         ...
 
     @abstractmethod
-    def load_entries(self, namespace: Optional[str] = None) -> list[Entry]:
+    def load_entries(self, namespace: str | None = None) -> list[Entry]:
         ...
 
     @abstractmethod
     def query(
         self,
         query: str,
-        count: Optional[int] = None,
-        namespace: Optional[str] = None,
+        count: int | None = None,
+        namespace: str | None = None,
         include_vectors: bool = False,
-        **kwargs
+        **kwargs,
     ) -> list[QueryResult]:
         ...
