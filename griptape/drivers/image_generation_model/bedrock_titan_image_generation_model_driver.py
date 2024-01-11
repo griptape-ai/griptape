0ca8eb8d68df5535e65c9a826a04cac246925ef6
Andrew French <andrew@afren.ch>
2024-01-10 20:38:16 -0800

    Add image to image modes for Titan image generation

--- griptape/drivers/image_generation_model/bedrock_titan_image_generation_model_driver.py
+++ griptape/drivers/image_generation_model/bedrock_titan_image_generation_model_driver.py
@@ -13,14 +13,14 @@ class BedrockTitanImageGenerationModelDriver(BaseImageGenerationModelDriver):
     """Image Generation Model Driver for Amazon Bedrock Titan Image Generator.
 
     Attributes:
-        task_type: The type of image generation task to perform, defaults to TEXT_IMAGE (text-to-image).
         quality: The quality of the generated image, defaults to standard.
         cfg_scale: Specifies how strictly image generation follows the provided prompt. Defaults to 7, (1.0 to 10.0].
+        outpainting_mode: Specifies the outpainting mode, defaults to PRECISE.
     """
 
-    task_type: str = field(default="TEXT_IMAGE", kw_only=True)
     quality: str = field(default="standard", kw_only=True)
     cfg_scale: int = field(default=7, kw_only=True)
+    outpainting_mode: str = field(default="PRECISE", kw_only=True)
 
     def text_to_image_request_parameters(
         self,
@@ -33,7 +33,7 @@ class BedrockTitanImageGenerationModelDriver(BaseImageGenerationModelDriver):
         prompt = ", ".join(prompts)
 
         request = {
-            "taskType": self.task_type,
+            "taskType": "TEXT_IMAGE",
             "textToImageParams": {"text": prompt},
             "imageGenerationConfig": {
                 "numberOfImages": 1,
@@ -59,7 +59,27 @@ class BedrockTitanImageGenerationModelDriver(BaseImageGenerationModelDriver):
         negative_prompts: list[str] | None = None,
         seed: int | None = None,
     ) -> dict:
-        raise NotImplementedError(f"{self.__class__.__name__} does not support variation")
+        prompt = ", ".join(prompts)
+
+        request = {
+            "taskType": "IMAGE_VARIATION",
+            "imageVariationParams": {"text": prompt, "images": [image.base64]},
+            "imageGenerationConfig": {
+                "numberOfImages": 1,
+                "quality": self.quality,
+                "width": image.width,
+                "height": image.height,
+                "cfgScale": self.cfg_scale,
+            },
+        }
+
+        if negative_prompts:
+            request["imageVariationParams"]["negativeText"] = ", ".join(negative_prompts)
+
+        if seed:
+            request["imageGenerationConfig"]["seed"] = seed
+
+        return request
 
     def image_inpainting_request_parameters(
         self,
@@ -69,7 +89,27 @@ class BedrockTitanImageGenerationModelDriver(BaseImageGenerationModelDriver):
         negative_prompts: list[str] | None = None,
         seed: int | None = None,
     ) -> dict:
-        raise NotImplementedError(f"{self.__class__.__name__} does not support inpainting")
+        prompt = ", ".join(prompts)
+
+        request = {
+            "taskType": "INPAINTING",
+            "inPaintingParams": {"text": prompt, "image": image.base64, "maskImage": mask.base64},
+            "imageGenerationConfig": {
+                "numberOfImages": 1,
+                "quality": self.quality,
+                "width": image.width,
+                "height": image.height,
+                "cfgScale": self.cfg_scale,
+            },
+        }
+
+        if negative_prompts:
+            request["inPaintingParams"]["negativeText"] = ", ".join(negative_prompts)
+
+        if seed:
+            request["imageGenerationConfig"]["seed"] = seed
+
+        return request
 
     def image_outpainting_request_parameters(
         self,
@@ -79,7 +119,32 @@ class BedrockTitanImageGenerationModelDriver(BaseImageGenerationModelDriver):
         negative_prompts: list[str] | None = None,
         seed: int | None = None,
     ) -> dict:
-        raise NotImplementedError(f"{self.__class__.__name__} does not support outpainting")
+        prompt = ", ".join(prompts)
+
+        request = {
+            "taskType": "OUTPAINTING",
+            "outPaintingParams": {
+                "text": prompt,
+                "image": image.base64,
+                "maskImage": mask.base64,
+                "outPaintingMode": self.outpainting_mode,
+            },
+            "imageGenerationConfig": {
+                "numberOfImages": 1,
+                "quality": self.quality,
+                "width": image.width,
+                "height": image.height,
+                "cfgScale": self.cfg_scale,
+            },
+        }
+
+        if negative_prompts:
+            request["outPaintingParams"]["negativeText"] = ", ".join(negative_prompts)
+
+        if seed:
+            request["imageGenerationConfig"]["seed"] = seed
+
+        return request
 
     def get_generated_image(self, response: dict) -> bytes:
         b64_image_data = response["images"][0]
