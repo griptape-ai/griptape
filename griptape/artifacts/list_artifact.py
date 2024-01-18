e13384258724cff4091d637b57d9d6a77b6fa3c1
Collin Dutter <collin@griptape.ai>
2024-01-18 15:29:38 -0800

    Add back missing metadata

--- griptape/artifacts/list_artifact.py
+++ griptape/artifacts/list_artifact.py
@@ -1,12 +1,13 @@
-from typing import Optional, Sequence
+from typing import Optional
+from collections.abc import Sequence
 from attr import field, define
 from griptape.artifacts import BaseArtifact
 
 
 @define
 class ListArtifact(BaseArtifact):
-    value: Sequence[BaseArtifact] = field(factory=list)
-    item_separator: str = field(default="\n\n", kw_only=True)
+    value: Sequence[BaseArtifact] = field(factory=list, metadata={"serializable": True})
+    item_separator: str = field(default="\n\n", kw_only=True, metadata={"serializable": True})
 
     @value.validator  # pyright: ignore
     def validate_value(self, _, value: list[BaseArtifact]) -> None:
