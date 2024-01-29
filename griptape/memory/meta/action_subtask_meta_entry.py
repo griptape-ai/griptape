64528d219832a61f3c6ac2472ccae86e3070d955
Aodhan Roche <aodhan@griptape.ai>
2024-01-29 14:58:07 -0800

    Made thought optional in action_subtask_meta_entry.py

--- griptape/memory/meta/action_subtask_meta_entry.py
+++ griptape/memory/meta/action_subtask_meta_entry.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Optional
+
 from attr import field, define
 from griptape.memory.meta import BaseMetaEntry
 
@@ -16,6 +18,6 @@ class ActionSubtaskMetaEntry(BaseMetaEntry):
 
     type: str = field(default=BaseMetaEntry.__name__, kw_only=True, metadata={"serializable": False})
 
-    thought: str = field(kw_only=True, metadata={"serializable": True})
+    thought: Optional[str] = field(kw_only=True, metadata={"serializable": True})
     action: str = field(kw_only=True, metadata={"serializable": True})
     answer: str = field(kw_only=True, metadata={"serializable": True})
