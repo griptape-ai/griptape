b741479344b484404b1bd4c14fed2e0498ef3ac2
Igor Morozov <39986119+igor-2lemetry@users.noreply.github.com>
2024-02-07 00:53:34 +0300

    Added top_n attribute to TextQueryTask (#635)

--- griptape/tasks/text_query_task.py
+++ griptape/tasks/text_query_task.py
@@ -1,16 +1,19 @@
 from attr import define, field, Factory
 from typing import Optional
 from griptape.artifacts import TextArtifact
-from griptape.engines import BaseQueryEngine
+from griptape.engines import VectorQueryEngine
 from griptape.loaders import TextLoader
 from griptape.tasks import BaseTextInputTask
 
 
 @define
 class TextQueryTask(BaseTextInputTask):
-    query_engine: BaseQueryEngine = field(kw_only=True)
+    query_engine: VectorQueryEngine = field(kw_only=True)
     loader: TextLoader = field(default=Factory(lambda: TextLoader()), kw_only=True)
     namespace: Optional[str] = field(default=None, kw_only=True)
+    top_n: Optional[int] = field(default=None, kw_only=True)
 
     def run(self) -> TextArtifact:
-        return self.query_engine.query(self.input.to_text(), namespace=self.namespace, rulesets=self.all_rulesets)
+        return self.query_engine.query(
+            self.input.to_text(), namespace=self.namespace, rulesets=self.all_rulesets, top_n=self.top_n
+        )
