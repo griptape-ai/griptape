4771fa37c180555ee5ded5dfbef68a24a93db112
Vasily Vasinov <des.elyon@gmail.com>
2024-02-12 16:42:30 -0700

    Add MetadataModule

--- griptape/data/modules/__init__.py
+++ griptape/data/modules/__init__.py
@@ -1,9 +1,11 @@
 from .base_module import BaseModule
 from .text_query_module import TextQueryModule
 from .ruleset_module import RulesetModule
+from.metadata_module import MetadataModule
 
 __all__ = [
     "BaseModule",
     "TextQueryModule",
-    "RulesetModule"
+    "RulesetModule",
+    "MetadataModule"
 ]
---
+++ griptape/data/modules/metadata_module.py
@@ -0,0 +1,18 @@
+from attr import define, field
+from griptape.data.modules import BaseModule
+from griptape.utils import J2
+
+
+@define
+class MetadataModule(BaseModule):
+    metadata: str = field(kw_only=True)
+
+    def process(self, context: dict) -> dict:
+        metadata_text = J2("data/modules/metadata/system.j2").render(metadata=self.metadata)
+
+        if not context.get("before_text_query"):
+            context["before_text_query"] = ""
+
+        context["before_text_query"] += metadata_text
+
+        return context
