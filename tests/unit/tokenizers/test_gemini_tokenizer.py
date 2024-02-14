777b56fb8387f8b23cf41de918d5df1a52b027a8
Robin Braumann <robin.braumann@softwaredefinedautomation.io>
2024-02-14 12:18:17 +0100

    Fix Gemini Tokenizer Testcases
    As the Gemini Tokenizer is using the Google API, Mocks are required to test the GeminiTokenizer Class

--- tests/unit/tokenizers/test_gemini_tokenizer.py
+++ tests/unit/tokenizers/test_gemini_tokenizer.py
@@ -3,18 +3,18 @@ from griptape.tokenizers import GeminiTokenizer
 
 
 class TestGeminiTokenizer:
-    def test_token_count(self):
-        tokenizer = GeminiTokenizer()
-        assert tokenizer.count_tokens("foo bar huzzah") == 5
+    @pytest.fixture
+    def tokenizer(self, request):
+        return GeminiTokenizer(model=request.param)
 
-    def test_tokens_left(self):
-        tokenizer = GeminiTokenizer()
-        assert tokenizer.count_tokens_left("foo bar huzzah") == 30715
+    @pytest.mark.parametrize(
+        "tokenizer,expected", [("gemini-pro", 5), ("gemini-pro-vision", 5)], indirect=["tokenizer"]
+    )
+    def test_token_count(self, tokenizer, expected):
+        assert tokenizer.count_tokens("foo bar huzzah") == expected
 
-    def test_token_count_vision(self):
-        tokenizer = GeminiTokenizer(model=GeminiTokenizer.DEFAULT_GEMINI_VISION_MODEL)
-        assert tokenizer.count_tokens("foo bar huzzah") == 5
-
-    def test_tokens_left_vision(self):
-        tokenizer = GeminiTokenizer(model=GeminiTokenizer.DEFAULT_GEMINI_VISION_MODEL)
-        assert tokenizer.count_tokens_left("foo bar huzzah") == 12283
+    @pytest.mark.parametrize(
+        "tokenizer,expected", [("gemini-pro", 30715), ("gemini-pro-vision", 12283)], indirect=["tokenizer"]
+    )
+    def test_tokens_left(self, tokenizer, expected):
+        assert tokenizer.count_tokens_left("foo bar huzzah") == expected
