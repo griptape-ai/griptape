33598641bfab06f99f198c709271c877cfd4a035
Robin Braumann <robin.braumann@softwaredefinedautomation.io>
2024-02-14 13:32:33 +0100

    Refactor GeminiTokenizer Testclass

--- tests/unit/tokenizers/test_gemini_tokenizer.py
+++ tests/unit/tokenizers/test_gemini_tokenizer.py
@@ -3,12 +3,14 @@ from unittest import mock
 from griptape.tokenizers import GeminiTokenizer
 from vertexai.preview.generative_models import GenerativeModel
 
+EXPECTED_TOTAL_TOKENS = 5
+
 
 class TestGeminiTokenizer:
     @pytest.fixture(autouse=True)
     def mock_gemini(self):
         mock_gemini = mock.Mock(GenerativeModel)
-        mock_gemini.count_tokens.return_value = mock.Mock(total_tokens=5)
+        mock_gemini.count_tokens.return_value = mock.Mock(total_tokens=EXPECTED_TOTAL_TOKENS)
 
         return mock_gemini
 
@@ -17,7 +19,8 @@ class TestGeminiTokenizer:
         return GeminiTokenizer(model=request.param, gemini=mock_gemini)
 
     @pytest.mark.parametrize(
-        "tokenizer,expected", [("gemini-pro", 5), ("gemini-pro-vision", 5)], indirect=["tokenizer"]
+        "tokenizer,expected", [("gemini-pro", EXPECTED_TOTAL_TOKENS), ("gemini-pro-vision", EXPECTED_TOTAL_TOKENS)],
+        indirect=["tokenizer"]
     )
     def test_token_count(self, tokenizer, expected):
         assert tokenizer.count_tokens("foo bar huzzah") == expected
