8d67b6e235eceb5e10db53f3b5da8e8ce8eca976
Robin Braumann <robin.braumann@softwaredefinedautomation.io>
2024-03-07 01:20:56 +0100

    start with claude 3 prompt model driver

--- griptape/tokenizers/bedrock_claude_tokenizer.py
+++ griptape/tokenizers/bedrock_claude_tokenizer.py
@@ -5,4 +5,8 @@ from griptape.tokenizers import AnthropicTokenizer
 @define(frozen=True)
 class BedrockClaudeTokenizer(AnthropicTokenizer):
     DEFAULT_MODEL = "anthropic.claude-v2:1"
-    MODEL_PREFIXES_TO_MAX_TOKENS = {"anthropic.claude-v2:1": 200000, "anthropic.claude": 100000}
+    MODEL_PREFIXES_TO_MAX_TOKENS = {
+        "anthropic.claude-3-sonnet-20240229-v1:0": 200000,
+        "anthropic.claude-v2:1": 200000,
+        "anthropic.claude": 100000,
+    }
