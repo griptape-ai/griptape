f070f545773d86b8aca5bcbfd6a78c0226e83363
Collin Dutter <collin@griptape.ai>
2024-03-14 09:42:22 -0700

    Fix deprecation warning, dummy exception message

--- griptape/utils/__init__.py
+++ griptape/utils/__init__.py
@@ -15,6 +15,7 @@ from .import_utils import import_optional_dependency
 from .stream import Stream
 from .constants import Constants as constants
 from .load_artifact_from_memory import load_artifact_from_memory
+from .deprecation import deprecation_warn
 
 
 def minify_json(value: str) -> str:
@@ -39,4 +40,5 @@ __all__ = [
     "Stream",
     "constants",
     "load_artifact_from_memory",
+    "deprecation_warn",
 ]
