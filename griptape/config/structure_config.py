1ef27d18d6172b0f9c6f683c23effcdb18d62797
Collin Dutter <collin@griptape.ai>
2024-03-14 10:30:13 -0700

    Clean up whitespace

--- griptape/config/structure_config.py
+++ griptape/config/structure_config.py
@@ -18,7 +18,6 @@ class StructureConfig(BaseStructureConfig):
     global_drivers: StructureGlobalDriversConfig = field(
         default=Factory(lambda: StructureGlobalDriversConfig()), kw_only=True, metadata={"serializable": True}
     )
-
     task_memory: StructureTaskMemoryConfig = field(
         default=Factory(
             lambda self: StructureTaskMemoryConfig(
