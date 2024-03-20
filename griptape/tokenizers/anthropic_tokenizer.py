4f36ca6eb0b4d1c0fea8e7455fae330eeb7a7e71
Collin Dutter <collin@griptape.ai>
2024-03-20 11:56:15 -0700

    Add Claude 3 Support (#694)
    Co-authored-by: Robin Braumann <robin.braumann@softwaredefinedautomation.io>

--- griptape/tokenizers/anthropic_tokenizer.py
+++ griptape/tokenizers/anthropic_tokenizer.py
@@ -10,7 +10,7 @@ if TYPE_CHECKING:
 
 @define()
 class AnthropicTokenizer(BaseTokenizer):
-    MODEL_PREFIXES_TO_MAX_INPUT_TOKENS = {"claude-2.1": 200000, "claude": 100000}
+    MODEL_PREFIXES_TO_MAX_INPUT_TOKENS = {"claude-3": 200000, "claude-2.1": 200000, "claude": 100000}
     MODEL_PREFIXES_TO_MAX_OUTPUT_TOKENS = {"claude": 4096}
 
     client: Anthropic = field(
