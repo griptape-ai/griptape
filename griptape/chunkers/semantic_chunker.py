85f05494df5c42145d491fe243fd2646ad31d1ed
Collin Dutter <collin@griptape.ai>
2024-03-27 09:10:22 -0700

    Fixes

--- griptape/chunkers/semantic_chunker.py
+++ griptape/chunkers/semantic_chunker.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import re
-from typing import Optional, Literal, cast
+from typing import Optional, Literal, cast, TYPE_CHECKING
 
 import numpy as np
 from attr import Factory, define, field
@@ -9,7 +9,9 @@ from numpy.linalg import norm
 
 from griptape.artifacts import TextArtifact
 from griptape.chunkers import BaseChunker
-from griptape.drivers.embedding.base_embedding_driver import BaseEmbeddingDriver
+
+if TYPE_CHECKING:
+    from griptape.drivers import BaseEmbeddingDriver
 
 
 @define
@@ -41,18 +43,25 @@ class SemanticChunker(BaseChunker):
 
     def try_chunk(self, text: TextArtifact | str) -> list[TextArtifact]:
         sentences = self._text_to_sentences(text.value if isinstance(text, TextArtifact) else text)
-        print(f"Num sentences: {len(sentences)}")
+        print([sentence.sentence.value for sentence in sentences][:3])
+
         combined_sentences = self._combine_sentences(sentences)
-        print(f"Num combined sentences: {len(combined_sentences)}")
+        print(
+            [
+                {"sentence": sentence.sentence.value, "combined_sentence": sentence.combined_sentence.value}
+                for sentence in combined_sentences
+            ][:3]
+        )
         embedded_sentences = self._embed_sentences(combined_sentences)
-        print(f"Num embedded sentences: {len(embedded_sentences)}")
+        # print(f"Num embedded sentences: {len(embedded_sentences)}")
         distances, sentences_with_distances = self._calculate_cosine_distances(embedded_sentences)
+        print(distances[:3])
         chunks = self._combine_sentences_to_chunks(distances, sentences_with_distances)
 
         return chunks
 
     def _text_to_sentences(self, text: str) -> list[SemanticChunker.Sentence]:
-        sentences = re.split(r"(?<=[.?!])\s+", text)  # TODO: replace with a better sentence tokenizer
+        sentences = self.split_into_sentences(text)
 
         return [
             SemanticChunker.Sentence(sentence=TextArtifact(sentence), index=index)
@@ -163,3 +172,58 @@ class SemanticChunker(BaseChunker):
             return np.mean(distances) + self.breakpoint_threshold_amount * iqr
         else:
             raise ValueError(f"Got unexpected `breakpoint_threshold_type`: " f"{self.breakpoint_threshold_type}")
+
+    def split_into_sentences(self, text: str) -> list[str]:
+        """
+        Split the text into sentences.
+
+        If the text contains substrings "<prd>" or "<stop>", they would lead
+        to incorrect splitting because they are used as markers for splitting.
+
+        :param text: text to be split into sentences
+        :type text: str
+
+        :return: list of sentences
+        :rtype: list[str]
+        """
+        alphabets = "([A-Za-z])"
+        prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
+        suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+        starters = r"(Mr|Mrs|Ms|Dr|Prof|Capt|Cpt|Lt|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+        acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+        websites = "[.](com|net|org|io|gov|edu|me)"
+        digits = "([0-9])"
+        multiple_dots = r"\.{2,}"
+
+        text = " " + text + "  "
+        text = text.replace("\n", " ")
+        text = re.sub(prefixes, "\\1<prd>", text)
+        text = re.sub(websites, "<prd>\\1", text)
+        text = re.sub(digits + "[.]" + digits, "\\1<prd>\\2", text)
+        text = re.sub(multiple_dots, lambda match: "<prd>" * len(match.group(0)) + "<stop>", text)
+        if "Ph.D" in text:
+            text = text.replace("Ph.D.", "Ph<prd>D<prd>")
+        text = re.sub(r"\s" + alphabets + "[.] ", " \\1<prd> ", text)
+        text = re.sub(acronyms + " " + starters, "\\1<stop> \\2", text)
+        text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]", "\\1<prd>\\2<prd>\\3<prd>", text)
+        text = re.sub(alphabets + "[.]" + alphabets + "[.]", "\\1<prd>\\2<prd>", text)
+        text = re.sub(" " + suffixes + "[.] " + starters, " \\1<stop> \\2", text)
+        text = re.sub(" " + suffixes + "[.]", " \\1<prd>", text)
+        text = re.sub(" " + alphabets + "[.]", " \\1<prd>", text)
+        if "”" in text:
+            text = text.replace(".”", "”.")
+        if '"' in text:
+            text = text.replace('."', '".')
+        if "!" in text:
+            text = text.replace('!"', '"!')
+        if "?" in text:
+            text = text.replace('?"', '"?')
+        text = text.replace(".", ".<stop>")
+        text = text.replace("?", "?<stop>")
+        text = text.replace("!", "!<stop>")
+        text = text.replace("<prd>", ".")
+        sentences = text.split("<stop>")
+        sentences = [s.strip() for s in sentences]
+        if sentences and not sentences[-1]:
+            sentences = sentences[:-1]
+        return sentences
