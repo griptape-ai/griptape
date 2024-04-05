21e9db27dd5080020b24089fff8dc4477d8ad76f
Collin Dutter <collin@griptape.ai>
2024-04-05 14:01:21 -0700

    Amazon

--- griptape/drivers/event_listener/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base_event_listener_driver import BaseEventListenerDriver
-from .amazon_sqs_event_listener_driver import AmazonSqsEventListenerDriver
-from .webhook_event_listener_driver import WebhookEventListenerDriver
-
-__all__ = ["BaseEventListenerDriver", "AmazonSqsEventListenerDriver", "WebhookEventListenerDriver"]
---
+++ griptape/drivers/event_listener/amazon_iot_event_listener_driver.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from attr import Factory, define, field
+
+from griptape.drivers.event_listener.base_event_listener_driver import BaseEventListenerDriver
+from griptape.events.base_event import BaseEvent
+from griptape.utils import import_optional_dependency
+
+if TYPE_CHECKING:
+    import boto3
+
+
+@define
+class AmazonIotEventListenerDriver(BaseEventListenerDriver):
+    iot_endpoint: str = field(kw_only=True)
+    topic: str = field(kw_only=True)
+    session: boto3.Session = field(default=Factory(lambda: import_optional_dependency("boto3").Session()), kw_only=True)
+
+    def try_publish_event(self, event: BaseEvent) -> None:
+        self.session.client("iot-data").publish(topic=self.topic, payload=event.to_json())
