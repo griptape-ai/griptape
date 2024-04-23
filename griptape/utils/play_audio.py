c0572ef2b2bc04aee070ff048b70e8c8edb4842e
Collin Dutter <collin@griptape.ai>
2024-04-23 10:41:31 -0700

    Return the played artifact

--- griptape/utils/play_audio.py
+++ griptape/utils/play_audio.py
@@ -3,5 +3,7 @@ import elevenlabs
 from griptape.artifacts import AudioArtifact
 
 
-def play_audio(artifact: AudioArtifact):
+def play_audio(artifact: AudioArtifact) -> AudioArtifact:
     elevenlabs.play(artifact.value)
+
+    return artifact
