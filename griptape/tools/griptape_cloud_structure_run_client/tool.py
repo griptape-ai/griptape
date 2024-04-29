ff4d92594a1d7a5c38ffd85e862f0d3a2e310aa8
Zach Giordano <zgmx22@gmail.com>
2024-04-29 17:35:06 -0400

    Address PR comments

--- griptape/tools/griptape_cloud_structure_run_client/tool.py
+++ griptape/tools/griptape_cloud_structure_run_client/tool.py
@@ -1,97 +1,83 @@
 from __future__ import annotations
-import time
-from typing import List, Optional
+from typing import Optional
 from urllib.parse import urljoin
 from schema import Schema, Literal
 from attr import define, field
+from griptape.mixins.exponential_backoff_mixin import ExponentialBackoffMixin
 from griptape.tools.base_griptape_cloud_client import BaseGriptapeCloudClient
 from griptape.utils.decorators import activity
 from griptape.artifacts import TextArtifact, ErrorArtifact
 
 
 @define
-class GriptapeCloudStructureRunClient(BaseGriptapeCloudClient):
+class GriptapeCloudStructureRunClient(BaseGriptapeCloudClient, ExponentialBackoffMixin):
     """
     Attributes:
         description: LLM-friendly structure description.
         structure_id: ID of the Griptape Cloud Structure.
-        structure_run_wait_time_attempts: Number of attempts to wait for the structure run to complete.
-        structure_run_wait_time_multiplier: Multiplier for the wait time between attempts.
     """
 
     description: Optional[str] = field(default=None, kw_only=True)
     structure_id: str = field(kw_only=True)
-    structure_run_wait_time_attempts: int = field(default=20, kw_only=True)
-    structure_run_wait_time_multiplier: float = field(default=1.5, kw_only=True)
 
     @activity(
         config={
-            "description": "Can be used to submit a run to a structure with the following description: {{ _self._get_structure_description() }}",
+            "description": "Can be used to execute a Run of a Structure with the following description: {{ _self._get_structure_description() }}",
             "schema": Schema(
-                {Literal("args", description="A list of arguments to submit to the structure run"): list[str]}
+                {Literal("args", description="A list of string arguments to submit to the Structure Run"): list}
             ),
         }
     )
-    def submit_structure_run(self, params: dict) -> TextArtifact | ErrorArtifact:
-        from requests import post, exceptions
+    def execute_structure_run(self, params: dict) -> TextArtifact | ErrorArtifact:
+        from requests import post, exceptions, HTTPError, Response
 
         args: list[str] = params["values"]["args"]
         url = urljoin(self.base_url.strip("/"), f"/api/structures/{self.structure_id}/runs")
 
         try:
-            response = post(url, json={"args": args}, headers=self.headers)
+            response: Response = post(url, json={"args": args}, headers=self.headers)
+            response.raise_for_status()
+            response_json = response.json()
+            return self._get_structure_run_result(response_json["structure_run_id"])
 
-            return TextArtifact(response.json())
-        except exceptions.RequestException as err:
+        except (exceptions.RequestException, HTTPError) as err:
             return ErrorArtifact(str(err))
 
-    @activity(
-        config={
-            "description": "Can be used to get the result of a structure run",
-            "schema": Schema({Literal("structure_run_id", description="The ID of the structure run"): str}),
-        }
-    )
-    def get_structure_run_result(self, params: dict) -> TextArtifact | ErrorArtifact:
-        from requests import get, exceptions
-
-        structure_run_id = params["values"]["structure_run_id"]
+    def _get_structure_run_result(self, structure_run_id: str) -> TextArtifact | ErrorArtifact:
         url = urljoin(self.base_url.strip("/"), f"/api/structure-runs/{structure_run_id}")
 
-        try:
-            response = get(url, headers=self.headers)
-            content = response.json()
+        for attempt in self.retrying():
+            with attempt:
+                return self._get_structure_run_result_attempt(url)
+        else:
+            return ErrorArtifact("Failed to get Run result.")
 
-            status = content.get("status")
+    def _get_structure_run_result_attempt(self, structure_run_url: str) -> TextArtifact | ErrorArtifact:
+        from requests import get, Response
 
-            wait_time = 1
-            wait_attempts = 0
-            while status in ["QUEUED", "RUNNING"] and wait_attempts < self.structure_run_wait_time_attempts:
-                # wait
-                time.sleep(wait_time)
-                wait_time = wait_time * self.structure_run_wait_time_multiplier
-                wait_attempts += 1
+        response: Response = get(structure_run_url, headers=self.headers)
+        response.raise_for_status()
+        content = response.json()
 
-                response = get(url, headers=self.headers)
-                content = response.json()
-                status = content.get("status")
+        status = content.get("status")
 
-            if status != "SUCCEEDED":
-                return ErrorArtifact(content)
+        if status in ("QUEUED", "RUNNING"):
+            raise Exception("Structure Run is still in progress")
 
-            if "output" in content:
-                return TextArtifact(content["output"])
-            else:
-                return ErrorArtifact("No output found in response")
+        if status != "SUCCEEDED":
+            return ErrorArtifact(content)
 
-        except exceptions.RequestException as err:
-            return ErrorArtifact(str(err))
+        if "output" in content:
+            return TextArtifact(content["output"])
+        else:
+            return ErrorArtifact("No output found in response")
 
     def _get_structure_description(self) -> str:
-        from requests import get
-
         if self.description:
             return self.description
         else:
+            from requests import get
+
             url = urljoin(self.base_url.strip("/"), f"/api/structures/{self.structure_id}/")
 
             response = get(url, headers=self.headers).json()
--- tests/unit/tools/test_griptape_cloud_structure_run_client.py
+++ tests/unit/tools/test_griptape_cloud_structure_run_client.py
@@ -8,7 +8,7 @@ class TestGriptapeCloudStructureRunClient:
         from griptape.tools import GriptapeCloudStructureRunClient
 
         mock_response = mocker.Mock()
-        mock_response.text.return_value = "foo bar"
+        mock_response.json.return_value = {"structure_run_id": 1}
         mocker.patch("requests.post", return_value=mock_response)
 
         mock_response = mocker.Mock()
@@ -17,11 +17,8 @@ class TestGriptapeCloudStructureRunClient:
 
         return GriptapeCloudStructureRunClient(base_url="https://api.griptape.ai", api_key="foo bar", structure_id="1")
 
-    def test_submit_structure_run(self, client):
-        assert isinstance(client.submit_structure_run({"values": {"args": ["foo bar"]}}), TextArtifact)
-
-    def test_get_structure_run_result(self, client):
-        assert isinstance(client.get_structure_run_result({"values": {"structure_run_id": "1"}}), TextArtifact)
+    def test_execute_structure_run(self, client):
+        assert isinstance(client.execute_structure_run({"values": {"args": ["foo bar"]}}), TextArtifact)
 
     def test_get_structure_description(self, client):
         assert client._get_structure_description() == "fizz buzz"
