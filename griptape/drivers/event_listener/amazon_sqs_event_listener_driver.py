92e5a11f37e5322c496d33e388947eff2bb419a7
Collin Dutter <collin@griptape.ai>
2024-05-08 12:35:55 -0700

    Update sqs

--- griptape/drivers/event_listener/amazon_sqs_event_listener_driver.py
+++ griptape/drivers/event_listener/amazon_sqs_event_listener_driver.py
@@ -22,8 +22,6 @@ class AmazonSqsEventListenerDriver(BaseEventListenerDriver):
         self.sqs_client.send_message(QueueUrl=self.queue_url, MessageBody=json.dumps(event_payload))
 
     def try_publish_event_payload_batch(self, event_payload_batch: list[dict]) -> None:
-        entries = [
-            {"Id": str(i), "MessageBody": json.dumps(event_payload)}
-            for i, event_payload in enumerate(event_payload_batch)
-        ]
+        entries = [{"MessageBody": json.dumps(event_payload)} for event_payload in event_payload_batch]
+
         self.sqs_client.send_message_batch(QueueUrl=self.queue_url, Entries=entries)
