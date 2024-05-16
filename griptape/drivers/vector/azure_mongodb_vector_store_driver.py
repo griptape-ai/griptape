510fbe9209fdd3e4c90a72388aed286f5eb624af
Collin Dutter <collin@griptape.ai>
2024-05-16 06:14:53 -1000

    Remove pass

--- griptape/drivers/vector/azure_mongodb_vector_store_driver.py
+++ griptape/drivers/vector/azure_mongodb_vector_store_driver.py
@@ -1,10 +1,7 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING, Optional
+from typing import Optional
 from attr import define
 from griptape.drivers import BaseVectorStoreDriver, MongoDbAtlasVectorStoreDriver
-
-if TYPE_CHECKING:
-    pass
 
 
 @define
