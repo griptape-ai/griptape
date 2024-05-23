0447e5cb1882319e97626feb792e4f8f667ee603
matt <matt@griptape.ai>
2024-05-23 18:15:05 -0500

    update defaults

--- griptape/tokenizers/dummy_tokenizer.py
+++ griptape/tokenizers/dummy_tokenizer.py
@@ -7,8 +7,8 @@ from griptape.tokenizers import BaseTokenizer
 @define
 class DummyTokenizer(BaseTokenizer):
     model: None = field(init=False, default=None, kw_only=True)
-    max_input_tokens: None = field(init=False, default=None, kw_only=True)
-    max_output_tokens: None = field(init=False, default=None, kw_only=True)
+    max_input_tokens: int = field(init=False, default=0, kw_only=True)
+    max_output_tokens: int = field(init=False, default=0, kw_only=True)
 
     def count_tokens(self, text: str | list) -> int:
         raise DummyException(__class__.__name__, "count_tokens")
