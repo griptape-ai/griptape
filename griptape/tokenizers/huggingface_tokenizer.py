24faa0b462dc77fee8037ce35b0de949b7a81fb8
matt <matt@griptape.ai>
2024-05-23 18:46:52 -0500

    None

--- griptape/tokenizers/huggingface_tokenizer.py
+++ griptape/tokenizers/huggingface_tokenizer.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING
 from attr import define, field, Factory
 from griptape.tokenizers import BaseTokenizer
 
@@ -10,7 +10,7 @@ if TYPE_CHECKING:
 @define()
 class HuggingFaceTokenizer(BaseTokenizer):
     tokenizer: PreTrainedTokenizerBase = field(kw_only=True)
-    model: Optional[str] = field(init=False, default=None, kw_only=True)
+    model: None = field(init=False, default=None, kw_only=True)
     max_input_tokens: int = field(
         default=Factory(lambda self: self.tokenizer.model_max_length, takes_self=True), kw_only=True
     )
