469a8dc2ca7fe6871cc12d768ca4fdc0bb6b12e3
matt <matt@griptape.ai>
2024-05-28 10:44:23 -0700

    env

--- griptape/drivers/structure_run/base_structure_run_driver.py
+++ griptape/drivers/structure_run/base_structure_run_driver.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+from attr import field
 from attrs import define
 
 from griptape.artifacts import BaseArtifact
@@ -7,6 +8,8 @@ from griptape.artifacts import BaseArtifact
 
 @define
 class BaseStructureRunDriver(ABC):
+    env: dict[str, str] = field(default={}, kw_only=True)
+
     def run(self, *args: BaseArtifact) -> BaseArtifact:
         return self.try_run(*args)
 
