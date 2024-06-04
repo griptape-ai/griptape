f8c55a01841c0e604e7b3b1bd4f9364f6d85c192
Collin Dutter <collin@griptape.ai>
2024-06-04 15:48:25 -0700

    Update cohere prompt driver, add cohere embedding driver, cohere structure config

--- docs/griptape-framework/drivers/embedding-drivers.md
+++ docs/griptape-framework/drivers/embedding-drivers.md
@@ -164,6 +164,23 @@ embeddings = driver.embed_string("Hello world!")
 print(embeddings[:3])
 ```
 
+### Cohere Embeddings
+```python
+import os
+from griptape.drivers import CohereEmbeddingDriver
+
+embedding_driver=CohereEmbeddingDriver(
+    model="embed-english-v3.0",
+    api_key=os.environ["COHERE_API_KEY"],
+    input_type="search_document",
+)
+
+embeddings = embedding_driver.embed_string("Hello world!")
+
+# display the first 3 embeddings
+print(embeddings[:3])
+```
+
 ### Override Default Structure Embedding Driver
 Here is how you can override the Embedding Driver that is used by default in Structures. 
 
---
+++ griptape/drivers/embedding/cohere_embedding_driver.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+from attrs import define, field, Factory
+from griptape.drivers import BaseEmbeddingDriver
+from griptape.tokenizers import CohereTokenizer
+from griptape.utils import import_optional_dependency
+
+if TYPE_CHECKING:
+    from cohere import Client
+
+
+@define
+class CohereEmbeddingDriver(BaseEmbeddingDriver):
+    """
+    Attributes:
+        api_key: Cohere API key.
+        model: 	Cohere model name.
+        client: Custom `cohere.Client`.
+        tokenizer: Custom `CohereTokenizer`.
+        input_type: Cohere embedding input type.
+    """
+
+    DEFAULT_MODEL = "models/embedding-001"
+
+    api_key: str = field(kw_only=True, metadata={"serializable": False})
+    client: Client = field(
+        default=Factory(lambda self: import_optional_dependency("cohere").Client(self.api_key), takes_self=True),
+        kw_only=True,
+    )
+    tokenizer: CohereTokenizer = field(
+        default=Factory(lambda self: CohereTokenizer(model=self.model, client=self.client), takes_self=True),
+        kw_only=True,
+    )
+
+    input_type: str = field(kw_only=True, metadata={"serializable": True})
+
+    def try_embed_chunk(self, chunk: str) -> list[float]:
+        result = self.client.embed(texts=[chunk], model=self.model, input_type=self.input_type)
+
+        if isinstance(result.embeddings, list):
+            return result.embeddings[0]
+        else:
+            raise ValueError("Non-float embeddings are not supported.")
