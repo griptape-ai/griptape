c699b22146a5b1c0cb287b8abea10dc2a8e103b2
matt <matt@griptape.ai>
2024-06-07 14:55:10 -0400

    Update `GriptapeCloudKnowledgeBaseClient` description fetching

--- tests/unit/tools/test_griptape_cloud_knowledge_base_client.py
+++ tests/unit/tools/test_griptape_cloud_knowledge_base_client.py
@@ -31,6 +31,19 @@ class TestGriptapeCloudKnowledgeBaseClient:
             base_url="https://api.griptape.ai", api_key="foo bar", knowledge_base_id="1"
         )
 
+    @pytest.fixture
+    def client_kb_error(self, mocker):
+        from griptape.tools import GriptapeCloudKnowledgeBaseClient
+
+        mock_response = mocker.Mock()
+        mock_response.json.return_value = {}
+        mock_response.status_code = 404
+        mocker.patch("requests.get", return_value=mock_response)
+
+        return GriptapeCloudKnowledgeBaseClient(
+            base_url="https://api.griptape.ai", api_key="foo bar", knowledge_base_id="1"
+        )
+
     def test_query(self, client):
         assert isinstance(client.query({"values": {"query": "foo bar"}}), TextArtifact)
 
@@ -48,3 +61,9 @@ class TestGriptapeCloudKnowledgeBaseClient:
                 str(e)
                 == f"No description found for Knowledge Base {client_no_description.knowledge_base_id}. Please set a description, or manually set the `GriptapeCloudKnowledgeBaseClient.description` attribute."
             )
+
+    def test_get_knowledge_base_kb_error(self, client_kb_error):
+        with pytest.raises(ValueError) as e:
+            client_kb_error._get_knowledge_base_description()
+
+            assert str(e) == f"Error accessing Knowledge Base {client_kb_error.knowledge_base_id}."
