f2342e603e425d92323fc22a1ee2c13661b01394
Andrew French <andrew@afren.ch>
2024-06-17 09:32:45 -0700

    Small improvements to Audio tasks/tools (#843)

--- griptape/tasks/base_audio_generation_task.py
+++ griptape/tasks/base_audio_generation_task.py
@@ -9,4 +9,13 @@ from griptape.tasks import BaseTask
 
 
 @define
-class BaseAudioGenerationTask(BlobArtifactFileOutputMixin, RuleMixin, BaseTask, ABC): ...
+class BaseAudioGenerationTask(BlobArtifactFileOutputMixin, RuleMixin, BaseTask, ABC):
+    def before_run(self) -> None:
+        super().before_run()
+
+        self.structure.logger.info(f"{self.__class__.__name__} {self.id}\nInput: {self.input.to_text()}")
+
+    def after_run(self) -> None:
+        super().after_run()
+
+        self.structure.logger.info(f"{self.__class__.__name__} {self.id}\nOutput: {self.output.to_text()}")
