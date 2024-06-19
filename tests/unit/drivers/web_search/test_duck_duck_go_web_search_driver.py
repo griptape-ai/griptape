f339881f24c06c3eb243ff43b02a38f1e8db90ec
matt <matt@griptape.ai>
2024-06-19 16:56:27 -0500

    rebase

---
+++ tests/unit/drivers/web_search/test_duck_duck_go_web_search_driver.py
@@ -0,0 +1,36 @@
+import pytest
+import json
+from griptape.drivers import DuckDuckGoWebSearchDriver
+from griptape.artifacts import ListArtifact
+
+
+class TestDuckDuckGoWebSearchDriver:
+    @pytest.fixture
+    def driver(self, mocker):
+        mock_response = [
+            {"title": "foo", "href": "bar", "body": "baz"},
+            {"title": "foo2", "href": "bar2", "body": "baz2"},
+        ]
+
+        mocker.patch("duckduckgo_search.DDGS.text", return_value=mock_response)
+
+        return DuckDuckGoWebSearchDriver()
+
+    @pytest.fixture
+    def driver_with_error(self, mocker):
+        mocker.patch("duckduckgo_search.DDGS.text", side_effect=Exception("test_error"))
+
+        return DuckDuckGoWebSearchDriver()
+
+    def test_search_returns_results(self, driver):
+        results = driver.search("test")
+        assert isinstance(results, ListArtifact)
+        output = [json.loads(result.value) for result in results]
+        assert len(output) == 2
+        assert output[0]["title"] == "foo"
+        assert output[0]["url"] == "bar"
+        assert output[0]["description"] == "baz"
+
+    def test_search_raises_error(self, driver_with_error):
+        with pytest.raises(Exception, match="Error searching 'test' with DuckDuckGo: test_error"):
+            driver_with_error.search("test")
