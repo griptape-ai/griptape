5ddf15c67dbf418341cd58992a49a9be26da6743
cjkindel <cjkindel@users.noreply.github.com>
2024-06-20 12:19:11 -0700

    Rename metadata to meta

--- griptape/artifacts/text_artifact.py
+++ griptape/artifacts/text_artifact.py
@@ -13,7 +13,7 @@ class TextArtifact(BaseArtifact):
     value: str = field(converter=str, metadata={"serializable": True})
     encoding: str = field(default="utf-8", kw_only=True)
     encoding_error_handler: str = field(default="strict", kw_only=True)
-    metadata: dict[str, str] = field(converter=BaseArtifact.value_to_dict, default={}, metadata={"serializable": True})
+    meta: dict[str, str] = field(converter=BaseArtifact.value_to_dict, default={}, metadata={"serializable": True})
     _embedding: list[float] = field(factory=list, kw_only=True)
 
     @property
