f3eb1dda4032b9381668fd1038bf0ddb14e9fe1c
Emily Danielson <emily@griptape.ai>
2024-06-25 14:15:18 -0700

    adding tests for chat history

--- tests/unit/drivers/prompt/test_cohere_prompt_driver.py
+++ tests/unit/drivers/prompt/test_cohere_prompt_driver.py
@@ -7,16 +7,16 @@ import pytest
 class TestCoherePromptDriver:
     @pytest.fixture
     def mock_client(self, mocker):
-        mock_client = mocker.patch("cohere.Client").return_value
-        mock_client.chat.return_value = Mock(text="model-output")
+        mock_client = mocker.patch("cohere.Client")
+        mock_client.return_value.chat.return_value = Mock(text="model-output")
 
         return mock_client
 
     @pytest.fixture
     def mock_stream_client(self, mocker):
-        mock_client = mocker.patch("cohere.Client").return_value
+        mock_client = mocker.patch("cohere.Client")
         mock_chunk = Mock(text="model-output", event_type="text-generation")
-        mock_client.chat_stream.return_value = iter([mock_chunk])
+        mock_client.return_value.chat_stream.return_value = iter([mock_chunk])
 
         return mock_client
 
@@ -42,8 +42,41 @@ class TestCoherePromptDriver:
 
         # When
         text_artifact = driver.try_run(prompt_stack)
+        print(f"Called methods: {mock_client}")
 
         # Then
+        expected_message = "assistant-input"
+        expected_history = [
+            {"role": "ASSISTANT", "text": "generic-input"},
+            {"role": "SYSTEM", "text": "system-input"},
+            {"role": "USER", "text": "user-input"},
+        ]
+        mock_client.return_value.chat.assert_called_once_with(
+            message=expected_message,
+            temperature=driver.temperature,
+            stop_sequences=driver.tokenizer.stop_sequences,
+            max_tokens=driver.max_tokens,
+            chat_history=expected_history,
+        )
+        assert text_artifact.value == "model-output"
+
+    def test_try_run_no_history(self, mock_client, prompt_stack):
+        # Given
+        prompt_stack_no_history = PromptStack()
+        prompt_stack_no_history.add_user_input("user-input")
+        driver = CoherePromptDriver(model="command", api_key="api-key")
+
+        # When
+        text_artifact = driver.try_run(prompt_stack_no_history)
+
+        # Then
+        expected_message = "user-input"
+        mock_client.return_value.chat.assert_called_once_with(
+            message=expected_message,
+            temperature=driver.temperature,
+            stop_sequences=driver.tokenizer.stop_sequences,
+            max_tokens=driver.max_tokens,
+        )
         assert text_artifact.value == "model-output"
 
     def test_try_stream_run(self, mock_stream_client, prompt_stack):  # pyright: ignore
