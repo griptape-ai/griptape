80c27cb1fff80a4d3c9200f6a95db28c59de71ca
Vasily Vasinov <vasily@griptape.ai>
2024-06-25 15:06:43 -0600

    VectorStoreClient improvements (#899)

--- griptape/tools/vector_store_client/tool.py
+++ griptape/tools/vector_store_client/tool.py
@@ -1,34 +1,36 @@
 from __future__ import annotations
-from typing import Optional
-from attrs import define, field
+from typing import Callable, Any
+from attrs import define, field, Factory
 from schema import Schema, Literal
-from griptape.artifacts import ErrorArtifact
+from griptape.artifacts import ErrorArtifact, BaseArtifact
 from griptape.artifacts import ListArtifact
 from griptape.drivers import BaseVectorStoreDriver
 from griptape.tools import BaseTool
 from griptape.utils.decorators import activity
 
 
-@define
+@define(kw_only=True)
 class VectorStoreClient(BaseTool):
     """
     Attributes:
         description: LLM-friendly vector DB description.
-        namespace: Vector storage namespace.
         vector_store_driver: `BaseVectorStoreDriver`.
-        top_n: Max number of results returned for the query engine query.
+        query_params: Optional dictionary of vector store driver query parameters.
+        process_query_output_fn: Optional lambda for processing vector store driver query output `Entry`s.
     """
 
     DEFAULT_TOP_N = 5
 
-    description: str = field(kw_only=True)
-    vector_store_driver: BaseVectorStoreDriver = field(kw_only=True)
-    top_n: int = field(default=DEFAULT_TOP_N, kw_only=True)
-    namespace: Optional[str] = field(default=None, kw_only=True)
+    description: str = field()
+    vector_store_driver: BaseVectorStoreDriver = field()
+    query_params: dict[str, Any] = field(factory=dict)
+    process_query_output_fn: Callable[[list[BaseVectorStoreDriver.Entry]], BaseArtifact] = field(
+        default=Factory(lambda: lambda es: ListArtifact([e.to_artifact() for e in es]))
+    )
 
     @activity(
         config={
-            "description": "Can be used to search a vector database with the following description: {{ _self.description }}",
+            "description": "Can be used to search a database with the following description: {{ _self.description }}",
             "schema": Schema(
                 {
                     Literal(
@@ -38,12 +40,10 @@ class VectorStoreClient(BaseTool):
             ),
         }
     )
-    def search(self, params: dict) -> ListArtifact | ErrorArtifact:
+    def search(self, params: dict) -> BaseArtifact:
         query = params["values"]["query"]
 
         try:
-            entries = self.vector_store_driver.query(query, namespace=self.namespace, count=self.top_n)
-
-            return ListArtifact([e.to_artifact() for e in entries])
+            return self.process_query_output_fn(self.vector_store_driver.query(query, **self.query_params))
         except Exception as e:
             return ErrorArtifact(f"error querying vector store: {e}")
