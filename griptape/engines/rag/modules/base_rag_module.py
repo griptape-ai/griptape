a12ad36dab5df2a97af12b74df24156ce871373f
Vasily Vasinov <des.elyon@gmail.com>
2024-06-27 14:59:21 -0600

    All `futures_executor` methods renamed to `futures_executor_fn` and now accept callables instead of futures

--- griptape/engines/rag/modules/base_rag_module.py
+++ griptape/engines/rag/modules/base_rag_module.py
@@ -1,5 +1,7 @@
 from abc import ABC
 from concurrent import futures
+from typing import Callable
+
 from attrs import define, field, Factory
 
 from griptape.utils import PromptStack
@@ -7,7 +9,9 @@ from griptape.utils import PromptStack
 
 @define(kw_only=True)
 class BaseRagModule(ABC):
-    futures_executor: futures.Executor = field(default=Factory(lambda: futures.ThreadPoolExecutor()))
+    futures_executor_fn: Callable[[], futures.Executor] = field(
+        default=Factory(lambda: lambda: futures.ThreadPoolExecutor())
+    )
 
     def generate_query_prompt_stack(self, system_prompt: str, query: str) -> PromptStack:
         return PromptStack(
