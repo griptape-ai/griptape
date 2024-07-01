1cac7be585bfef3e8cc2fd7585313ad62c92f2a5
Vasily Vasinov <des.elyon@gmail.com>
2024-07-01 16:06:14 -0600

    Fix issues with VectorStoreRetrievalModule

--- griptape/engines/rag/modules/retrieval/vector_store_retrieval_rag_module.py
+++ griptape/engines/rag/modules/retrieval/vector_store_retrieval_rag_module.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING, Sequence, Any
-from attrs import define, field
-from griptape.artifacts import TextArtifact
+from typing import TYPE_CHECKING, Sequence, Any, Callable
+from attrs import define, field, Factory
+from griptape.artifacts import TextArtifact, BaseArtifact
 from griptape.engines.rag import RagContext
 from griptape.engines.rag.modules import BaseRetrievalRagModule
 
@@ -13,15 +13,12 @@ if TYPE_CHECKING:
 class VectorStoreRetrievalRagModule(BaseRetrievalRagModule):
     vector_store_driver: BaseVectorStoreDriver = field()
     query_params: dict[str, Any] = field(factory=dict)
+    process_query_output_fn: Callable[[list[BaseVectorStoreDriver.Entry]], BaseArtifact] = field(
+        default=Factory(lambda: lambda es: [e.to_artifact() for e in es])
+    )
 
     def run(self, context: RagContext) -> Sequence[TextArtifact]:
         context_query_params = self.context_param(context, "query_params")
         query_params = self.query_params if context_query_params is None else context_query_params
 
-        results = self.vector_store_driver.query(context.query, **query_params)
-
-        return [
-            artifact
-            for artifact in [r.to_artifact() for r in results]
-            if isinstance(artifact, TextArtifact)
-        ]
+        return self.process_query_output_fn(self.vector_store_driver.query(context.query, **query_params))
