c90a952cf576228c1d3f967083f47c21cc839a58
Vasily Vasinov <des.elyon@gmail.com>
2024-07-06 16:17:49 -0600

    Make RagContext serializable

--- griptape/engines/rag/rag_context.py
+++ griptape/engines/rag/rag_context.py
@@ -2,13 +2,14 @@ from __future__ import annotations
 from typing import Optional, TYPE_CHECKING
 from attrs import define, field
 from griptape.common import Reference
+from griptape.mixins import SerializableMixin
 
 if TYPE_CHECKING:
     from griptape.artifacts import TextArtifact, BaseArtifact
 
 
 @define(kw_only=True)
-class RagContext:
+class RagContext(SerializableMixin):
     """Used by RagEngine stages and module to pass context that individual modules are expected to update in the `run`
     method.
 
@@ -21,12 +22,12 @@ class RagContext:
         output: Final output from the generation stage.
     """
 
-    query: str = field()
-    module_configs: dict[str, dict] = field(factory=dict)
-    before_query: list[str] = field(factory=list)
-    after_query: list[str] = field(factory=list)
-    text_chunks: list[TextArtifact] = field(factory=list)
-    output: Optional[BaseArtifact] = field(default=None)
+    query: str = field(metadata={"serializable": True})
+    module_configs: dict[str, dict] = field(factory=dict, metadata={"serializable": True})
+    before_query: list[str] = field(factory=list, metadata={"serializable": True})
+    after_query: list[str] = field(factory=list, metadata={"serializable": True})
+    text_chunks: list[TextArtifact] = field(factory=list, metadata={"serializable": True})
+    output: Optional[BaseArtifact] = field(default=None, metadata={"serializable": True})
 
     def get_references(self) -> list[Reference]:
         references = []
