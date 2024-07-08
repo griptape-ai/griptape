3246e86a31eea0147d7dba877480ff92f2da8225
Vasily Vasinov <des.elyon@gmail.com>
2024-07-08 08:46:50 -0600

    Add TestTextLoaderRetrievalRagModule

---
+++ tests/unit/engines/rag/modules/retrieval/test_text_loader_retrieval_rag_module.py
@@ -0,0 +1,26 @@
+import pytest
+
+from griptape.drivers import LocalVectorStoreDriver
+from griptape.engines.rag import RagContext
+from griptape.engines.rag.modules import TextLoaderRetrievalRagModule
+from griptape.loaders import WebLoader
+from tests.mocks.mock_embedding_driver import MockEmbeddingDriver
+
+MAX_TOKENS = 50
+
+
+class TestTextLoaderRetrievalRagModule:
+    @pytest.fixture(autouse=True)
+    def mock_trafilatura_fetch_url(self, mocker):
+        mocker.patch("trafilatura.fetch_url", return_value="<html>foobar</html>")
+
+    def test_run(self):
+        embedding_driver = MockEmbeddingDriver()
+
+        module = TextLoaderRetrievalRagModule(
+            loader=WebLoader(max_tokens=MAX_TOKENS, embedding_driver=embedding_driver),
+            vector_store_driver=LocalVectorStoreDriver(embedding_driver=embedding_driver),
+            source="https://www.griptape.ai"
+        )
+
+        assert module.run(RagContext(query="foo"))[0].value == "foobar"
