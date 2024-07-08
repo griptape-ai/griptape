78c0a6cd201283aac4e7905bf3c066770ef13080
Vasily Vasinov <des.elyon@gmail.com>
2024-07-08 10:15:24 -0600

    Update FootnotePrompt system prompt, fix PromptResponseRagModule text chunking logic

--- griptape/engines/rag/modules/response/prompt_response_rag_module.py
+++ griptape/engines/rag/modules/response/prompt_response_rag_module.py
@@ -1,6 +1,7 @@
 from typing import Callable
 from attrs import define, field, Factory
 from griptape.artifacts.text_artifact import TextArtifact
+from griptape.common import Reference
 from griptape.drivers import BasePromptDriver
 from griptape.engines.rag import RagContext
 from griptape.engines.rag.modules import BaseResponseRagModule
@@ -11,45 +12,52 @@ from griptape.utils import J2
 class PromptResponseRagModule(BaseResponseRagModule):
     answer_token_offset: int = field(default=400)
     prompt_driver: BasePromptDriver = field()
-    generate_system_template: Callable[[RagContext], str] = field(
+    generate_system_template: Callable[[RagContext, list[TextArtifact]], str] = field(
         default=Factory(lambda self: self.default_system_template_generator, takes_self=True)
     )
 
     def run(self, context: RagContext) -> RagContext:
         query = context.query
-        text_artifact_chunks = context.text_chunks
+        tokenizer = self.prompt_driver.tokenizer
+        included_chunks = []
+        system_prompt = self.generate_system_template(context, included_chunks)
 
-        if query:
-            tokenizer = self.prompt_driver.tokenizer
-            text_chunks = []
-            system_prompt = self.generate_system_template(context)
-            for artifact in text_artifact_chunks:
-                text_chunks.append(artifact.value)
+        for artifact in context.text_chunks:
+            included_chunks.append(artifact)
 
-                system_prompt = self.generate_system_template(context)
-                message_token_count = self.prompt_driver.tokenizer.count_tokens(
-                    self.prompt_driver.prompt_stack_to_string(self.generate_query_prompt_stack(system_prompt, query))
-                )
+            system_prompt = self.generate_system_template(context, included_chunks)
+            message_token_count = self.prompt_driver.tokenizer.count_tokens(
+                self.prompt_driver.prompt_stack_to_string(self.generate_query_prompt_stack(system_prompt, query))
+            )
 
-                if message_token_count + self.answer_token_offset >= tokenizer.max_input_tokens:
-                    text_chunks.pop()
+            if message_token_count + self.answer_token_offset >= tokenizer.max_input_tokens:
+                included_chunks.pop()
 
-                    system_prompt = self.generate_system_template(context)
+                system_prompt = self.generate_system_template(context, included_chunks)
 
-                    break
+                break
 
-            output = self.prompt_driver.run(self.generate_query_prompt_stack(system_prompt, query)).to_artifact()
+        output = self.prompt_driver.run(self.generate_query_prompt_stack(system_prompt, query)).to_artifact()
 
-            if isinstance(output, TextArtifact):
-                context.output = output
-            else:
-                raise ValueError("Prompt driver did not return a TextArtifact")
+        if isinstance(output, TextArtifact):
+            context.output = output
+        else:
+            raise ValueError("Prompt driver did not return a TextArtifact")
 
         return context
 
-    def default_system_template_generator(self, context: RagContext) -> str:
+    def default_system_template_generator(self, context: RagContext, artifacts: list[TextArtifact]) -> str:
         return J2("engines/rag/modules/response/prompt/system.j2").render(
-            text_chunks=[c.to_text() for c in context.text_chunks],
+            text_chunks=[c.to_text() for c in artifacts],
             before_system_prompt="\n\n".join(context.before_query),
             after_system_prompt="\n\n".join(context.after_query),
         )
+
+    def references_from_artifacts(self, artifacts: list[TextArtifact]) -> list[Reference]:
+        references = []
+
+        for a in artifacts:
+            if a.reference is not None and a.reference not in references:
+                references.append(a.reference)
+
+        return references
