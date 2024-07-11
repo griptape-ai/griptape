cbeb422ff1f44b0c97ec8d52d154d86c0ab5e399
cjkindel <cjkindel@users.noreply.github.com>
2024-07-11 09:58:56 -0700

    reformat tests

--- tests/unit/drivers/vector/test_griptape_cloud_knowledge_base_vector_store_driver.py
+++ tests/unit/drivers/vector/test_griptape_cloud_knowledge_base_vector_store_driver.py
@@ -1,6 +1,5 @@
+import pytest
 import uuid
-from unittest.mock import Mock, patch
-from griptape.drivers import GriptapeCloudKnowledgeBaseVectorStoreDriver
 
 
 class MockResponse:
@@ -12,67 +11,69 @@ class MockResponse:
         return self.json_data
 
 
-test_ids = [str(uuid.uuid4()), str(uuid.uuid4())]
-test_vecs = [[0.1, 0.2, 0.3], [0.4, 0.5, 0.6]]
-test_namespaces = [str(uuid.uuid4()), str(uuid.uuid4())]
-test_metas = [{"key": "value1"}, {"key": "value2"}]
-test_scores = [0.7, 0.8]
-test_entries = {
-    "entries": [
-        {
-            "id": test_ids[0],
-            "vector": test_vecs[0],
-            "namespace": test_namespaces[0],
-            "meta": test_metas[0],
-            "score": test_scores[0],
-        },
-        {
-            "id": test_ids[1],
-            "vector": test_vecs[1],
-            "namespace": test_namespaces[1],
-            "meta": test_metas[1],
-            "score": test_scores[1],
-        },
-    ]
-}
-
-
 class TestGriptapeCloudKnowledgeBaseVectorStoreDriver:
-    def mock_requests_post(*args, **kwargs):
-        return MockResponse(test_entries, 200)
+    test_ids = [str(uuid.uuid4()), str(uuid.uuid4())]
+    test_vecs = [[0.1, 0.2, 0.3], [0.4, 0.5, 0.6]]
+    test_namespaces = [str(uuid.uuid4()), str(uuid.uuid4())]
+    test_metas = [{"key": "value1"}, {"key": "value2"}]
+    test_scores = [0.7, 0.8]
 
-    @patch("requests.post", side_effect=mock_requests_post)
-    def test_query(self, mock_post):
-        driver = GriptapeCloudKnowledgeBaseVectorStoreDriver(api_key="foo", knowledge_base_id="bar")
+    @pytest.fixture
+    def driver(self, mocker):
+        from griptape.drivers import GriptapeCloudKnowledgeBaseVectorStoreDriver
 
+        test_entries = {
+            "entries": [
+                {
+                    "id": self.test_ids[0],
+                    "vector": self.test_vecs[0],
+                    "namespace": self.test_namespaces[0],
+                    "meta": self.test_metas[0],
+                    "score": self.test_scores[0],
+                },
+                {
+                    "id": self.test_ids[1],
+                    "vector": self.test_vecs[1],
+                    "namespace": self.test_namespaces[1],
+                    "meta": self.test_metas[1],
+                    "score": self.test_scores[1],
+                },
+            ]
+        }
+
+        mock_response = mocker.Mock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = test_entries
+        mocker.patch("requests.post", return_value=mock_response)
+
+        return GriptapeCloudKnowledgeBaseVectorStoreDriver(api_key="foo bar", knowledge_base_id="1")
+
+    def test_query(self, driver):
         result = driver.query(
             "some query", count=10, namespace="foo", include_vectors=True, distance_metric="bar", filter={"foo": "bar"}
         )
 
-        assert result[0].id == test_ids[0]
-        assert result[1].id == test_ids[1]
-        assert result[0].vector == test_vecs[0]
-        assert result[1].vector == test_vecs[1]
-        assert result[0].namespace == test_namespaces[0]
-        assert result[1].namespace == test_namespaces[1]
-        assert result[0].meta == test_metas[0]
-        assert result[1].meta == test_metas[1]
-        assert result[0].score == test_scores[0]
-        assert result[1].score == test_scores[1]
+        assert result[0].id == self.test_ids[0]
+        assert result[1].id == self.test_ids[1]
+        assert result[0].vector == self.test_vecs[0]
+        assert result[1].vector == self.test_vecs[1]
+        assert result[0].namespace == self.test_namespaces[0]
+        assert result[1].namespace == self.test_namespaces[1]
+        assert result[0].meta == self.test_metas[0]
+        assert result[1].meta == self.test_metas[1]
+        assert result[0].score == self.test_scores[0]
+        assert result[1].score == self.test_scores[1]
 
-    @patch("requests.post", side_effect=mock_requests_post)
-    def test_query_defaults(self, mock_post):
-        driver = GriptapeCloudKnowledgeBaseVectorStoreDriver(api_key="foo", knowledge_base_id="bar")
-
+    def test_query_defaults(self, driver):
         result = driver.query("some query")
 
-        assert result[0].id == test_ids[0]
-        assert result[1].id == test_ids[1]
-        assert result[0].vector == test_vecs[0]
-        assert result[1].vector == test_vecs[1]
-        assert result[0].namespace == test_namespaces[0]
-        assert result[1].namespace == test_namespaces[1]
-        assert result[0].meta == test_metas[0]
-        assert result[1].meta == test_metas[1]
-        assert result[0].score == test_scores[0]
-        assert result[1].score == test_scores[1]
+        assert result[0].id == self.test_ids[0]
+        assert result[1].id == self.test_ids[1]
+        assert result[0].vector == self.test_vecs[0]
+        assert result[1].vector == self.test_vecs[1]
+        assert result[0].namespace == self.test_namespaces[0]
+        assert result[1].namespace == self.test_namespaces[1]
+        assert result[0].meta == self.test_metas[0]
+        assert result[1].meta == self.test_metas[1]
+        assert result[0].score == self.test_scores[0]
+        assert result[1].score == self.test_scores[1]
