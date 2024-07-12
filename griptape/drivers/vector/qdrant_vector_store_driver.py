c313f0870ef3e69e4b02d7c544c83c916c788f46
cjkindel <cjkindel@users.noreply.github.com>
2024-07-12 14:29:50 -0700

    make format

--- griptape/drivers/vector/qdrant_vector_store_driver.py
+++ griptape/drivers/vector/qdrant_vector_store_driver.py
@@ -106,11 +106,7 @@ class QdrantVectorStoreDriver(BaseVectorStoreDriver):
         query_vector = self.embedding_driver.embed_string(query)
 
         # Create a search request
-        request = {
-            "collection_name": self.collection_name,
-            "query_vector": query_vector,
-            "limit": count,
-        }
+        request = {"collection_name": self.collection_name, "query_vector": query_vector, "limit": count}
         request = {k: v for k, v in request.items() if v is not None}
         results = self.client.search(**request)
 
