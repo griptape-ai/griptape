546899f8dd285221a3b128c5c94bdb0d4706dbb3
Collin Dutter <collin@griptape.ai>
2024-07-15 11:41:36 -0700

    Remove unused methods

--- griptape/artifacts/generic_artifact.py
+++ griptape/artifacts/generic_artifact.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import json
 from typing import Any
 
 from attrs import define, field
@@ -11,31 +10,6 @@ from griptape.artifacts import BaseArtifact
 @define
 class GenericArtifact(BaseArtifact):
     value: Any = field(metadata={"serializable": True})
-
-    @classmethod
-    def value_to_bytes(cls, value: Any) -> bytes:
-        if isinstance(value, bytes):
-            return value
-        else:
-            return str(value).encode()
-
-    @classmethod
-    def value_to_dict(cls, value: Any) -> dict:
-        dict_value = value if isinstance(value, dict) else json.loads(value)
-
-        return {k: v for k, v in dict_value.items()}
-
-    def to_text(self) -> str:
-        return str(self.value)
-
-    def __str__(self) -> str:
-        return self.to_text()
-
-    def __bool__(self) -> bool:
-        return bool(self.value)
-
-    def __len__(self) -> int:
-        return len(self.value)
 
     def __add__(self, other: BaseArtifact) -> BaseArtifact:
         raise NotImplementedError
