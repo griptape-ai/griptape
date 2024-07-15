b1754d2b306300bec6d5dbd0d3459b7948e697fd
Collin Dutter <collin@griptape.ai>
2024-07-15 11:40:38 -0700

    Remove out of scope changes

--- griptape/tasks/image_query_task.py
+++ griptape/tasks/image_query_task.py
@@ -86,13 +86,3 @@ class ImageQueryTask(BaseTask):
         self.output = self.image_query_engine.run(query.value, image_artifacts)
 
         return self.output
-
-    def before_run(self) -> None:
-        super().before_run()
-
-        self.structure.logger.info(f"{self.__class__.__name__} {self.id}\nInput: {self.input.to_text()}")
-
-    def after_run(self) -> None:
-        super().after_run()
-
-        self.structure.logger.info(f"{self.__class__.__name__} {self.id}\nOutput: {self.output.to_text()}")
