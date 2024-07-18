871d716aa2ea7754e4eadc8edd89ee4fd1e2ac10
cjkindel <cjkindel@users.noreply.github.com>
2024-07-18 10:49:33 -0700

    parameterize dd_agent endpoint

--- griptape/drivers/observability/datadog_observability_driver.py
+++ griptape/drivers/observability/datadog_observability_driver.py
@@ -1,3 +1,5 @@
+import os
+
 from attrs import Factory, define, field
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import SpanProcessor
@@ -8,9 +10,12 @@ from griptape.drivers.observability.open_telemetry_observability_driver import O
 
 @define
 class DatadogObservabilityDriver(OpenTelemetryObservabilityDriver):
+    datadog_agent_endpoint: str = field(
+        default=Factory(lambda: os.getenv("DD_AGENT_ENDPOINT", "http://localhost:4318/v1/traces")), kw_only=True
+    )
     span_processor: SpanProcessor = field(
         default=Factory(
-            lambda self: BatchSpanProcessor(OTLPSpanExporter(endpoint="http://localhost:4318/v1/traces")),
+            lambda self: BatchSpanProcessor(OTLPSpanExporter(endpoint=self.base_url)),
             takes_self=True,
         ),
         kw_only=True,
