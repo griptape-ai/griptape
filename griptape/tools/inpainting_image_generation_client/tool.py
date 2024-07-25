e1977ce2f42665221482a1d365cc92931c5c6179
Andrew French <andrew@afren.ch>
2024-07-25 16:38:27 -0700

    Improve performance of image generation tools (#1019)

--- griptape/tools/inpainting_image_generation_client/tool.py
+++ griptape/tools/inpainting_image_generation_client/tool.py
@@ -1,14 +1,14 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, cast
+from pathlib import Path
+from typing import TYPE_CHECKING, cast
 
 from attrs import define, field
 from schema import Literal, Schema
 
 from griptape.artifacts import ErrorArtifact, ImageArtifact
 from griptape.loaders import ImageLoader
-from griptape.mixins import BlobArtifactFileOutputMixin
-from griptape.tools import BaseTool
+from griptape.tools.base_image_generation_client import BaseImageGenerationClient
 from griptape.utils.decorators import activity
 from griptape.utils.load_artifact_from_memory import load_artifact_from_memory
 
@@ -17,7 +17,7 @@ if TYPE_CHECKING:
 
 
 @define
-class InpaintingImageGenerationClient(BlobArtifactFileOutputMixin, BaseTool):
+class InpaintingImageGenerationClient(BaseImageGenerationClient):
     """A tool that can be used to generate prompted inpaintings of an image.
 
     Attributes:
@@ -31,17 +31,11 @@ class InpaintingImageGenerationClient(BlobArtifactFileOutputMixin, BaseTool):
 
     @activity(
         config={
-            "description": "Can be used to modify an image within a specified mask area using image and mask files.",
+            "description": "Modifies an image within a specified mask area using image and mask files.",
             "schema": Schema(
                 {
-                    Literal(
-                        "prompts",
-                        description="A detailed list of features and descriptions to include in the generated image.",
-                    ): list[str],
-                    Literal(
-                        "negative_prompts",
-                        description="A detailed list of features and descriptions to avoid in the generated image.",
-                    ): list[str],
+                    Literal("prompt", description=BaseImageGenerationClient.PROMPT_DESCRIPTION): str,
+                    Literal("negative_prompt", description=BaseImageGenerationClient.NEGATIVE_PROMPT_DESCRIPTION): str,
                     Literal(
                         "image_file",
                         description="The path to an image file to be used as a base to generate variations from.",
@@ -51,40 +45,26 @@ class InpaintingImageGenerationClient(BlobArtifactFileOutputMixin, BaseTool):
             ),
         },
     )
-    def image_inpainting_from_file(self, params: dict[str, Any]) -> ImageArtifact | ErrorArtifact:
-        prompts = params["values"]["prompts"]
-        negative_prompts = params["values"]["negative_prompts"]
+    def image_inpainting_from_file(self, params: dict[str, dict[str, str]]) -> ImageArtifact | ErrorArtifact:
+        prompt = params["values"]["prompt"]
+        negative_prompt = params["values"]["negative_prompt"]
         image_file = params["values"]["image_file"]
         mask_file = params["values"]["mask_file"]
 
-        input_artifact = self.image_loader.load(image_file)
-        mask_artifact = self.image_loader.load(mask_file)
+        input_artifact = self.image_loader.load(Path(image_file).read_bytes())
+        mask_artifact = self.image_loader.load(Path(mask_file).read_bytes())
 
-        output_artifact = self.engine.run(
-            prompts=prompts,
-            negative_prompts=negative_prompts,
-            image=input_artifact,
-            mask=mask_artifact,
+        return self._generate_inpainting(
+            prompt, negative_prompt, cast(ImageArtifact, input_artifact), cast(ImageArtifact, mask_artifact)
         )
-
-        if self.output_dir or self.output_file:
-            self._write_to_file(output_artifact)
-
-        return output_artifact
 
     @activity(
         config={
-            "description": "Can be used to modify an image within a specified mask area using image and mask artifacts in memory.",
+            "description": "Modifies an image within a specified mask area using image and mask artifacts in memory.",
             "schema": Schema(
                 {
-                    Literal(
-                        "prompts",
-                        description="A detailed list of features and descriptions to include in the generated image.",
-                    ): list[str],
-                    Literal(
-                        "negative_prompts",
-                        description="A detailed list of features and descriptions to avoid in the generated image.",
-                    ): list[str],
+                    Literal("prompt", description=BaseImageGenerationClient.PROMPT_DESCRIPTION): str,
+                    Literal("negative_prompt", description=BaseImageGenerationClient.NEGATIVE_PROMPT_DESCRIPTION): str,
                     "memory_name": str,
                     "image_artifact_namespace": str,
                     "image_artifact_name": str,
@@ -94,9 +74,9 @@ class InpaintingImageGenerationClient(BlobArtifactFileOutputMixin, BaseTool):
             ),
         },
     )
-    def image_inpainting_from_memory(self, params: dict[str, Any]) -> ImageArtifact | ErrorArtifact:
-        prompts = params["values"]["prompts"]
-        negative_prompts = params["values"]["negative_prompts"]
+    def image_inpainting_from_memory(self, params: dict[str, dict[str, str]]) -> ImageArtifact | ErrorArtifact:
+        prompt = params["values"]["prompt"]
+        negative_prompt = params["values"]["negative_prompt"]
         image_artifact_namespace = params["values"]["image_artifact_namespace"]
         image_artifact_name = params["values"]["image_artifact_name"]
         mask_artifact_namespace = params["values"]["mask_artifact_namespace"]
@@ -123,24 +103,14 @@ class InpaintingImageGenerationClient(BlobArtifactFileOutputMixin, BaseTool):
             return ErrorArtifact(str(e))
 
         return self._generate_inpainting(
-            prompts,
-            negative_prompts,
-            cast(ImageArtifact, image_artifact),
-            cast(ImageArtifact, mask_artifact),
+            prompt, negative_prompt, cast(ImageArtifact, image_artifact), cast(ImageArtifact, mask_artifact)
         )
 
     def _generate_inpainting(
-        self,
-        prompts: list[str],
-        negative_prompts: list[str],
-        image_artifact: ImageArtifact,
-        mask_artifact: ImageArtifact,
+        self, prompt: str, negative_prompt: str, image_artifact: ImageArtifact, mask_artifact: ImageArtifact
     ) -> ImageArtifact:
         output_artifact = self.engine.run(
-            prompts=prompts,
-            negative_prompts=negative_prompts,
-            image=image_artifact,
-            mask=mask_artifact,
+            prompts=[prompt], negative_prompts=[negative_prompt], image=image_artifact, mask=mask_artifact
         )
 
         if self.output_dir or self.output_file:
