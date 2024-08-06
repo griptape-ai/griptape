9ebd44e51f321c69b1981b657f4bd433a038518c
Collin Dutter <collin@griptape.ai>
2024-08-06 13:25:36 -0700

    Fix type

--- griptape/config/config.py
+++ griptape/config/config.py
@@ -1,13 +1,14 @@
 from attrs import Factory, define, field
 
 from .base_config import BaseConfig
+from .base_driver_config import BaseDriverConfig
 from .events_config import EventsConfig
 from .openai_driver_config import OpenAiDriverConfig
 
 
 @define
 class _Config(BaseConfig):
-    drivers: OpenAiDriverConfig = field(default=Factory(lambda: OpenAiDriverConfig()), kw_only=True)
+    drivers: BaseDriverConfig = field(default=Factory(lambda: OpenAiDriverConfig()), kw_only=True)
     events: EventsConfig = field(default=Factory(lambda: EventsConfig()), kw_only=True)
 
 
