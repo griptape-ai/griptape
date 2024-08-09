4e3dc5f245117df0749175289f454e9ccac164f4
Collin Dutter <collin@griptape.ai>
2024-08-09 12:54:28 -0700

    Add rules to tools

--- griptape/tools/prompt_summary_client/tool.py
+++ griptape/tools/prompt_summary_client/tool.py
@@ -1,27 +1,24 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 from attrs import define, field
 from schema import Literal, Or, Schema
 
 from griptape.artifacts import BaseArtifact, ErrorArtifact, ListArtifact, TextArtifact
+from griptape.engines import PromptSummaryEngine
+from griptape.mixins.rule_mixin import RuleMixin
 from griptape.tools import BaseTool
 from griptape.utils.decorators import activity
 
-if TYPE_CHECKING:
-    from griptape.engines import PromptSummaryEngine
-
 
 @define(kw_only=True)
-class PromptSummaryClient(BaseTool):
+class PromptSummaryClient(BaseTool, RuleMixin):
     """Tool for using a Prompt Summary Engine.
 
     Attributes:
         prompt_summary_engine: `PromptSummaryEngine`.
     """
 
-    prompt_summary_engine: PromptSummaryEngine = field(kw_only=True)
+    prompt_summary_engine: PromptSummaryEngine = field(kw_only=True, default=PromptSummaryEngine())
 
     @activity(
         config={
@@ -55,4 +52,4 @@ class PromptSummaryClient(BaseTool):
             else:
                 return ErrorArtifact("memory not found")
 
-        return self.prompt_summary_engine.summarize_artifacts(artifacts)
+        return self.prompt_summary_engine.summarize_artifacts(artifacts, rulesets=self.all_rulesets)
--- griptape/tools/rag_client/tool.py
+++ griptape/tools/rag_client/tool.py
@@ -1,20 +1,24 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from attrs import Factory, define, field
+from schema import Literal, Or, Schema
 
-from attrs import define, field
-from schema import Literal, Schema
-
-from griptape.artifacts import BaseArtifact, ErrorArtifact
+from griptape.artifacts import BaseArtifact, ErrorArtifact, TextArtifact
+from griptape.engines.rag import RagEngine
+from griptape.engines.rag.modules import (
+    MetadataBeforeResponseRagModule,
+    PromptResponseRagModule,
+    RulesetsBeforeResponseRagModule,
+)
+from griptape.engines.rag.rag_context import RagContext
+from griptape.engines.rag.stages import ResponseRagStage
+from griptape.mixins.rule_mixin import RuleMixin
 from griptape.tools import BaseTool
 from griptape.utils.decorators import activity
 
-if TYPE_CHECKING:
-    from griptape.engines.rag import RagEngine
-
 
 @define(kw_only=True)
-class RagClient(BaseTool):
+class RagClient(BaseTool, RuleMixin):
     """Tool for querying a RAG engine.
 
     Attributes:
@@ -23,19 +27,59 @@ class RagClient(BaseTool):
     """
 
     description: str = field()
-    rag_engine: RagEngine = field()
+    rag_engine: RagEngine = field(
+        default=Factory(
+            lambda self: RagEngine(
+                response_stage=ResponseRagStage(
+                    before_response_modules=[
+                        RulesetsBeforeResponseRagModule(rulesets=self.all_rulesets),
+                        MetadataBeforeResponseRagModule(),
+                    ],
+                    response_module=PromptResponseRagModule(),
+                ),
+            ),
+            takes_self=True,
+        )
+    )
 
     @activity(
         config={
-            "description": "{{ _self.description }}",
-            "schema": Schema({Literal("query", description="A natural language search query"): str}),
+            "description": "Can be used to search content with the following description:  {{ _self.description }}",
+            "schema": Schema(
+                {
+                    Literal("query", description="A natural language search query"): str,
+                    Literal("content"): Or(
+                        str,
+                        Schema(
+                            {
+                                "memory_name": str,
+                                "artifact_namespace": str,
+                            }
+                        ),
+                    ),
+                }
+            ),
         },
     )
     def search(self, params: dict) -> BaseArtifact:
         query = params["values"]["query"]
+        summary = params["values"]["content"]
+
+        if isinstance(summary, str):
+            text_artifacts = [TextArtifact(summary)]
+        else:
+            memory = self.find_input_memory(summary["memory_name"])
+            artifact_namespace = summary["artifact_namespace"]
+
+            if memory is not None:
+                artifacts = memory.load_artifacts(artifact_namespace)
+            else:
+                return ErrorArtifact("memory not found")
+
+            text_artifacts = [artifact for artifact in artifacts if isinstance(artifact, TextArtifact)]
 
         try:
-            result = self.rag_engine.process_query(query)
+            result = self.rag_engine.process(RagContext(query=query, text_chunks=text_artifacts))
 
             if result.output is None:
                 return ErrorArtifact("query output is empty")
