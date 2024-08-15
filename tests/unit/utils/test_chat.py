06b6af350e6277899adb10d3fa1dd4899d657b93
Collin Dutter <collin@griptape.ai>
2024-08-15 14:20:11 -0700

    Set logger level in Chat

--- tests/unit/utils/test_chat.py
+++ tests/unit/utils/test_chat.py
@@ -1,3 +1,7 @@
+import logging
+from unittest.mock import patch
+
+from griptape.config import config
 from griptape.memory.structure import ConversationMemory
 from griptape.structures import Agent
 from griptape.utils import Chat
@@ -5,8 +9,6 @@ from griptape.utils import Chat
 
 class TestConversation:
     def test_init(self):
-        import logging
-
         agent = Agent(conversation_memory=ConversationMemory())
 
         chat = Chat(
@@ -18,6 +20,7 @@ class TestConversation:
             prompt_prefix="Question: ",
             response_prefix="Answer: ",
             output_fn=logging.info,
+            logger_level=logging.INFO,
         )
         assert chat.structure == agent
         assert chat.exiting_text == "foo..."
@@ -26,3 +29,20 @@ class TestConversation:
         assert chat.prompt_prefix == "Question: "
         assert chat.response_prefix == "Answer: "
         assert callable(chat.output_fn)
+        assert chat.logger_level == logging.INFO
+
+    @patch("builtins.input", side_effect=["exit"])
+    def test_chat_logger_level(self, mock_input):
+        agent = Agent(conversation_memory=ConversationMemory())
+
+        chat = Chat(agent)
+
+        logger = logging.getLogger(config.logging.logger_name)
+        logger.setLevel(logging.DEBUG)
+
+        assert logger.getEffectiveLevel() == logging.DEBUG
+
+        chat.start()
+
+        assert logger.getEffectiveLevel() == logging.DEBUG
+        assert mock_input.call_count == 1
