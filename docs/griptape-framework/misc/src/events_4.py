49495dd14463f018d9842b8891f0cc23491b151c
Collin Dutter <collin@griptape.ai>
2024-08-19 15:10:26 -0700

    Fix integration tests (#1080)

--- docs/griptape-framework/misc/src/events_4.py
+++ docs/griptape-framework/misc/src/events_4.py
@@ -1,15 +1,22 @@
-from griptape.structures import Pipeline
-from griptape.tasks import ToolkitTask
+import logging
+
+from griptape.config import config
+from griptape.structures import Agent
 from griptape.tools import PromptSummaryTool, WebScraperTool
 from griptape.utils import Stream
 
-pipeline = Pipeline()
-pipeline.add_tasks(
-    ToolkitTask(
-        "Based on https://griptape.ai, tell me what griptape is.",
-        tools=[WebScraperTool(off_prompt=True), PromptSummaryTool(off_prompt=False)],
-    )
+# Hide Griptape's usual output
+logging.getLogger(config.logging_config.logger_name).setLevel(logging.ERROR)
+
+agent = Agent(
+    input="Based on https://griptape.ai, tell me what griptape is.",
+    tools=[
+        PromptSummaryTool(off_prompt=True),
+        WebScraperTool(off_prompt=False),
+    ],
+    stream=True,
 )
 
-for artifact in Stream(pipeline).run():
+
+for artifact in Stream(agent).run():
     print(artifact.value, end="", flush=True)
