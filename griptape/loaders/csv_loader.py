9a9769598b24b166f946688b15d9d1fe35334e7a
Collin Dutter <collin@griptape.ai>
2024-09-06 10:24:39 -0700

    Fix bad merge

--- griptape/loaders/csv_loader.py
+++ griptape/loaders/csv_loader.py
@@ -6,19 +6,19 @@ from typing import TYPE_CHECKING, Callable, Optional, cast
 
 from attrs import define, field
 
-from griptape.artifacts import ListArtifact, TextArtifact
-from griptape.loaders.base_file_loader import BaseFileLoader
+from griptape.artifacts import CsvRowArtifact, ListArtifact
+from griptape.loaders import BaseFileLoader
 
 
 @define
-class CsvLoader(BaseFileLoader[ListArtifact]):
+class CsvLoader(BaseFileLoader[ListArtifact[CsvRowArtifact]]):
     delimiter: str = field(default=",", kw_only=True)
     encoding: str = field(default="utf-8", kw_only=True)
     formatter_fn: Callable[[dict], str] = field(
         default=lambda value: "\n".join(f"{key}: {val}" for key, val in value.items()), kw_only=True
     )
 
-    def parse(self, data: bytes) -> ListArtifact[TextArtifact]:
+    def parse(self, data: bytes) -> ListArtifact[CsvRowArtifact]:
         reader = csv.DictReader(StringIO(data.decode(self.encoding)), delimiter=self.delimiter)
 
-        return ListArtifact([TextArtifact(row, meta={"row": row_num}) for row_num, row in enumerate(reader)])
+        return ListArtifact([CsvRowArtifact(row, meta={"row": row_num}) for row_num, row in enumerate(reader)])
--- griptape/loaders/sql_loader.py
+++ griptape/loaders/sql_loader.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from attrs import define, field
 
-from griptape.artifacts import ListArtifact, TextArtifact
+from griptape.artifacts import CsvRowArtifact, ListArtifact
 from griptape.drivers import BaseSqlDriver
 from griptape.loaders import BaseLoader
 
@@ -14,5 +14,5 @@ class SqlLoader(BaseLoader[str, list[BaseSqlDriver.RowResult], ListArtifact]):
     def fetch(self, source: str) -> list[BaseSqlDriver.RowResult]:
         return self.sql_driver.execute_query(source) or []
 
-    def parse(self, data: list[BaseSqlDriver.RowResult]) -> ListArtifact[TextArtifact]:
-        return ListArtifact([TextArtifact(row.cells, meta={"row": row_num}) for row_num, row in enumerate(data)])
+    def parse(self, data: list[BaseSqlDriver.RowResult]) -> ListArtifact[CsvRowArtifact]:
+        return ListArtifact([CsvRowArtifact(row.cells, meta={"row": row_num}) for row_num, row in enumerate(data)])
