61660776db0ce7da18b53a3a46e883e631d65526
Collin Dutter <collin@griptape.ai>
2024-09-09 12:17:12 -0700

    Move converters into class

--- griptape/artifacts/json_artifact.py
+++ griptape/artifacts/json_artifact.py
@@ -10,13 +10,6 @@ from griptape.artifacts import BaseArtifact
 Json = Union[dict[str, "Json"], list["Json"], str, int, float, bool, None]
 
 
-def value_to_json(value: Any) -> Json:
-    if isinstance(value, str):
-        return json.loads(value)
-    else:
-        return json.loads(json.dumps(value))
-
-
 @define
 class JsonArtifact(BaseArtifact):
     """Stores JSON data.
@@ -25,7 +18,14 @@ class JsonArtifact(BaseArtifact):
         value: The JSON data. Values will automatically be converted to a JSON-compatible format.
     """
 
-    value: Json = field(converter=value_to_json, metadata={"serializable": True})
+    value: Json = field(converter=lambda value: JsonArtifact.value_to_json(value), metadata={"serializable": True})
+
+    @classmethod
+    def value_to_json(cls, value: Any) -> Json:
+        if isinstance(value, str):
+            return json.loads(value)
+        else:
+            return json.loads(json.dumps(value))
 
     def to_text(self) -> str:
         return json.dumps(self.value)
