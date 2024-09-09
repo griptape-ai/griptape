5b56867660d2fe6f408fc39aa85c9f58f5d983c3
Matt Vallillo <matt@griptape.ai>
2024-09-09 13:18:04 -0400

    Add `AzureOpenAiTextToSpeechDriver` (#1150)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,6 +12,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 - Parameter `meta: dict` on `BaseEvent`.
+- `AzureOpenAiTextToSpeechDriver`.
 
 ### Changed
 - **BREAKING**: Drivers, Loaders, and Engines now raise exceptions rather than returning `ErrorArtifacts`.
---
+++ tests/unit/drivers/text_to_speech/test_azure_openai_text_to_speech_driver.py
@@ -0,0 +1,33 @@
+from unittest.mock import Mock
+
+import pytest
+
+from griptape.drivers import AzureOpenAiTextToSpeechDriver
+
+
+class TestAzureOpenAiTextToSpeechDriver:
+    @pytest.fixture()
+    def mock_speech_create(self, mocker):
+        mock_speech_create = mocker.patch("openai.AzureOpenAI").return_value.audio.speech.create
+        mock_function = Mock(arguments='{"foo": "bar"}', id="mock-id")
+        mock_function.name = "MockTool_test"
+        mock_speech_create.return_value = Mock(
+            content=b"speech",
+        )
+
+        return mock_speech_create
+
+    def test_init(self):
+        assert AzureOpenAiTextToSpeechDriver(azure_endpoint="foobar", azure_deployment="foobar")
+        assert AzureOpenAiTextToSpeechDriver(azure_endpoint="foobar").azure_deployment == "tts"
+
+    def test_run_text_to_audio(self, mock_speech_create):
+        driver = AzureOpenAiTextToSpeechDriver(azure_endpoint="foobar")
+        output = driver.run_text_to_audio(["foo", "bar"])
+        mock_speech_create.assert_called_once_with(
+            input="foo. bar",
+            model=driver.model,
+            response_format=driver.format,
+            voice=driver.voice,
+        )
+        assert output.value == b"speech"
