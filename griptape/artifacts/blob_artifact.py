50dfb460e56241f281d33ac8d6db575d191cc9d0
Collin Dutter <collin@griptape.ai>
2024-09-10 10:47:01 -0700

    Move converter to lambda

--- griptape/artifacts/blob_artifact.py
+++ griptape/artifacts/blob_artifact.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-from typing import Any
-
 from attrs import define, field
 
 from griptape.artifacts import BaseArtifact
@@ -15,18 +13,14 @@ class BlobArtifact(BaseArtifact):
         value: The binary data.
     """
 
-    value: bytes = field(converter=lambda value: BlobArtifact.value_to_bytes(value), metadata={"serializable": True})
+    value: bytes = field(
+        converter=lambda value: value if isinstance(value, bytes) else str(value).encode(),
+        metadata={"serializable": True},
+    )
 
     @property
     def mime_type(self) -> str:
         return "application/octet-stream"
-
-    @classmethod
-    def value_to_bytes(cls, value: Any) -> bytes:
-        if isinstance(value, bytes):
-            return value
-        else:
-            return str(value).encode()
 
     def to_bytes(self) -> bytes:
         return self.value
