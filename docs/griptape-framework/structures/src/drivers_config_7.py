c7f2bb020dfd1c175f5908074db3d381069f9403
Collin Dutter <collin@griptape.ai>
2024-09-12 10:04:36 -0700

    Show mixing and matching Drivers in custom example

--- docs/griptape-framework/structures/src/drivers_config_7.py
+++ docs/griptape-framework/structures/src/drivers_config_7.py
@@ -2,14 +2,15 @@ import os
 
 from griptape.configs import Defaults
 from griptape.configs.drivers import DriversConfig
-from griptape.drivers import AnthropicPromptDriver
+from griptape.drivers import AnthropicPromptDriver, OpenAiEmbeddingDriver
 from griptape.structures import Agent
 
 Defaults.drivers_config = DriversConfig(
     prompt_driver=AnthropicPromptDriver(
         model="claude-3-sonnet-20240229",
         api_key=os.environ["ANTHROPIC_API_KEY"],
-    )
+    ),
+    embedding_driver=OpenAiEmbeddingDriver(),
 )
 
 
