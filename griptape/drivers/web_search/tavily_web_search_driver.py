5bf6ef2c1bfdf4d89c5322f56269128e7e77c8ff
William Price <82848178+william-price01@users.noreply.github.com>
2024-09-23 18:44:45 -0700

    poetry lock --no-update

--- griptape/drivers/web_search/tavily_web_search_driver.py
+++ griptape/drivers/web_search/tavily_web_search_driver.py
@@ -2,12 +2,13 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any
 
-from attrs import Factory, define, field
+from attrs import define, field
 from tavily import BadRequestError
 
 from griptape.artifacts import JsonArtifact, ListArtifact
 from griptape.drivers import BaseWebSearchDriver
 from griptape.utils import import_optional_dependency
+from griptape.utils.decorators import lazy_property
 
 if TYPE_CHECKING:
     from tavily import TavilyClient
@@ -16,11 +17,12 @@ if TYPE_CHECKING:
 @define
 class TavilyWebSearchDriver(BaseWebSearchDriver):
     api_key: str = field(kw_only=True)
-    client: TavilyClient = field(
-        default=Factory(lambda self: import_optional_dependency("tavily").TavilyClient(self.api_key), takes_self=True),
-        kw_only=True,
-    )
     params: dict[str, Any] = field(factory=dict, kw_only=True, metadata={"serializable": True})
+    _client: TavilyClient = field(default=None, kw_only=True, alias="client", metadata={"serializable": False})
+
+    @lazy_property()
+    def client(self) -> TavilyClient:
+        return import_optional_dependency("tavily").TavilyClient(self.api_key)
 
     def search(self, query: str, **kwargs) -> ListArtifact:
         response = self.client.search(query, max_results=self.results_count, **self.params, **kwargs)
