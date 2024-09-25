c0b93a3d3b61142f3f25d9a6575cccc547991fcf
Collin Dutter <collin@griptape.ai>
2024-09-25 09:39:10 -0700

    Add tests

--- tests/unit/structures/test_pipeline.py
+++ tests/unit/structures/test_pipeline.py
@@ -203,6 +203,22 @@ class TestPipeline:
         assert len(second_task.parents) == 1
         assert len(second_task.children) == 0
 
+    def test_nested_tasks(self):
+        pipeline = Pipeline(
+            tasks=[
+                [
+                    PromptTask("parent", id=f"parent_{i}"),
+                    PromptTask("child", id=f"child_{i}", parent_ids=[f"parent_{i}"]),
+                    PromptTask("grandchild", id=f"grandchild_{i}", parent_ids=[f"child_{i}"]),
+                ]
+                for i in range(3)
+            ]
+        )
+
+        pipeline.run()
+        assert pipeline.output_task.id == "grandchild_2"
+        assert len(pipeline.tasks) == 9
+
     def test_insert_task_in_middle(self):
         first_task = PromptTask("test1", id="test1")
         second_task = PromptTask("test2", id="test2")
