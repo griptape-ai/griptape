f2288dd298ce3c8df0c3986a788f7f7e4c1b4a3f
Collin Dutter <collin@griptape.ai>
2024-10-07 15:02:26 -0700

    Fix loaders example (#1232)

--- docs/griptape-framework/data/src/loaders_5.py
+++ docs/griptape-framework/data/src/loaders_5.py
@@ -3,10 +3,10 @@ from pathlib import Path
 
 from griptape.loaders import TextLoader
 
-TextLoader().load("my text")
+TextLoader().load("tests/resources/test.txt")
 
 urllib.request.urlretrieve("https://example-files.online-convert.com/document/txt/example.txt", "example.txt")
 
-TextLoader().load(Path("example.txt").read_text())
+TextLoader().load(Path("example.txt"))
 
-TextLoader().load_collection(["my text", "my other text", Path("example.txt").read_text()])
+TextLoader().load_collection(["tests/resources/test.txt", Path("example.txt")])
