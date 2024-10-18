8de2399b503f2a4acbdf197200e070a0f66d6883
cjkindel <cjkindel@users.noreply.github.com>
2024-10-18 09:32:36 -0700

    make format

--- docs/griptape-framework/drivers/src/amazon_s3_file_manager_driver.py
+++ docs/griptape-framework/drivers/src/amazon_s3_file_manager_driver.py
@@ -10,7 +10,7 @@ amazon_s3_file_manager_driver = AmazonS3FileManagerDriver(
         region_name=os.environ["AWS_DEFAULT_REGION"],
         aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
         aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
-    )
+    ),
 )
 
 # Download File
