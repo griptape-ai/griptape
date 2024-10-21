e6ffc90e2a33044a40f3b09b3a8dc2a751bb0095
CJ Kindel <cjkindel@users.noreply.github.com>
2024-10-21 14:29:25 -0700

    GriptapeCloudFileManagerDriver (#1267)

---
+++ docs/griptape-framework/drivers/src/amazon_s3_file_manager_driver.py
@@ -0,0 +1,24 @@
+import os
+
+import boto3
+
+from griptape.drivers import AmazonS3FileManagerDriver
+
+amazon_s3_file_manager_driver = AmazonS3FileManagerDriver(
+    bucket=os.environ["AMAZON_S3_BUCKET"],
+    session=boto3.Session(
+        region_name=os.environ["AWS_DEFAULT_REGION"],
+        aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+    ),
+)
+
+# Download File
+file_contents = amazon_s3_file_manager_driver.load_file(os.environ["AMAZON_S3_KEY"])
+
+print(file_contents)
+
+# Upload File
+response = amazon_s3_file_manager_driver.save_file(os.environ["AMAZON_S3_KEY"], file_contents.value)
+
+print(response)
