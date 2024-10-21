43d70ad37812c7d5ebd653a560637d6edc097985
matt <matt@griptape.ai>
2024-10-21 15:50:21 -0500

    update example

--- docs/griptape-framework/misc/src/events_chunk_stream.py
+++ docs/griptape-framework/misc/src/events_chunk_stream.py
@@ -4,22 +4,14 @@ from griptape.structures import Pipeline
 from griptape.tasks import ToolkitTask
 from griptape.tools import PromptSummaryTool, WebScraperTool
 
-
-def action_chunk_listener(event: ActionChunkEvent) -> None:
-    if event.tag is not None and event.name is not None and event.path is not None:
-        print(f"{event.name}.{event.tag} ({event.path}) ", end="", flush=True)
-    if event.partial_input is not None:
-        print(event.partial_input, end="", flush=True)
-
-
 EventBus.add_event_listeners(
     [
         EventListener(
-            lambda e: print(e.token, end="", flush=True),
+            lambda e: print(str(e), end="", flush=True),
             event_types=[TextChunkEvent],
         ),
         EventListener(
-            action_chunk_listener,
+            lambda e: print(str(e), end="", flush=True),
             event_types=[ActionChunkEvent],
         ),
     ]
