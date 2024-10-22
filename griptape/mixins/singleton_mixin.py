77b17a0c7a89ff9a86d8d5c58b7304736147273a
Collin Dutter <collin@griptape.ai>
2024-10-22 09:27:09 -0700

    Improve SingletonMixin thread safety

--- griptape/mixins/singleton_mixin.py
+++ griptape/mixins/singleton_mixin.py
@@ -1,10 +1,16 @@
 from __future__ import annotations
 
+import threading
+
+lock = threading.Lock()
+
 
 class SingletonMixin:
-    _instance = None
+    _instances = {}
 
     def __new__(cls, *args, **kwargs) -> SingletonMixin:
-        if not cls._instance:
-            cls._instance = super().__new__(cls, *args, **kwargs)  # noqa: UP008
-        return cls._instance
+        if cls not in cls._instances:
+            with lock:
+                if cls not in cls._instances:
+                    cls._instances[cls] = super().__new__(cls, *args, **kwargs)
+        return cls._instances[cls]
