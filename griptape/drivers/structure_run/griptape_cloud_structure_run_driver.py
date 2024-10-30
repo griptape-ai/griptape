f1d50e6dc613a68a8213cb1db434cb9df4c068c0
Collin Dutter <collin@griptape.ai>
2024-10-30 15:03:22 -0700

    Add back "support" for structures

--- griptape/drivers/structure_run/griptape_cloud_structure_run_driver.py
+++ griptape/drivers/structure_run/griptape_cloud_structure_run_driver.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import os
-from typing import Any, Literal, Optional
+from typing import Literal, Optional
 from urllib.parse import urljoin
 
 from attrs import Factory, define, field
@@ -48,27 +48,51 @@ class GriptapeCloudStructureRunDriver(BaseStructureRunDriver):
             return self._get_run_result(response_json[f"{self.resource_type}_run_id"])
 
     def _get_run_result(self, structure_run_id: str) -> BaseArtifact | InfoArtifact:
-        url = urljoin(self.base_url.strip("/"), f"/api/{self.resource_type}-runs/{structure_run_id}/events")
+        url = urljoin(self.base_url.strip("/"), f"/api/{self.resource_type}-runs/{structure_run_id}")
 
-        result = self._get_run_result_attempt(url)
-        events = result["events"]
-        offset = result["offset"]
+        events, next_offset = self._get_run_events(url)
 
         output = None
         while not output:
-            result = self._get_run_result_attempt(url, offset=offset)
-            events = result["events"]
-            offset = result["next_offset"]
+            events, next_offset = self._get_run_events(url, offset=next_offset)
             for event in events:
-                EventBus.publish_event(BaseEvent.from_dict(event["payload"]))
-                if event["type"] == "FinishStructureRunEvent":
-                    output = BaseArtifact.from_dict(event["payload"]["output_task_output"])
+                event_origin = event["origin"]
+                if event_origin in ("ASSISTANT", "USER"):
+                    EventBus.publish_event(BaseEvent.from_dict(event["payload"]))
+                    if event["type"] == "FinishStructureRunEvent":
+                        output = BaseArtifact.from_dict(event["payload"]["output_task_output"])
+                elif event_origin == "SYSTEM":
+                    if event["type"] == "StructureRunCompleted":
+                        result = self._get_run_result_attempt(url)
+
+                        if "output" in result:
+                            output = BaseArtifact.from_dict(result["output"])
+                        else:
+                            output = InfoArtifact("No output found in response")
+                    elif event["type"] == "StructureRunError":
+                        result = self._get_run_result_attempt(url)
+
+                        raise Exception(result["payload"]["status_detail"])
+
         return output
 
-    def _get_run_result_attempt(self, structure_run_url: str, offset: int = 0) -> Any:
+    def _get_run_events(self, structure_run_url: str, offset: int = 0) -> tuple[list[dict], int]:
         from requests import Response, get
 
-        response: Response = get(structure_run_url, headers=self.headers, params={"offset": offset})
+        response: Response = get(f"{structure_run_url}/events", headers=self.headers, params={"offset": offset})
+        response.raise_for_status()
+
+        response_json = response.json()
+
+        events = response_json["events"]
+        next_offset = response_json["next_offset"]
+
+        return events, next_offset
+
+    def _get_run_result_attempt(self, structure_run_url: str) -> dict:
+        from requests import Response, get
+
+        response: Response = get(structure_run_url, headers=self.headers)
         response.raise_for_status()
 
         return response.json()
