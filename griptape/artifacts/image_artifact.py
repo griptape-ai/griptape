0c37e02e727c0a8e057daa8bab05d088c0e18cd8
Andrew French <andrew@afren.ch>
2024-11-06 18:13:08 -0800

    Reintroduce image/audio artifact naming

--- griptape/artifacts/image_artifact.py
+++ griptape/artifacts/image_artifact.py
@@ -1,5 +1,9 @@
 from __future__ import annotations
 
+import random
+import string
+import time
+
 from attrs import define, field
 
 from griptape.artifacts import BlobArtifact
@@ -19,6 +23,12 @@ class ImageArtifact(BlobArtifact):
     width: int = field(kw_only=True, metadata={"serializable": True})
     height: int = field(kw_only=True, metadata={"serializable": True})
 
+    def __attrs_post_init__(self) -> None:
+        # Generating the name string requires attributes set by child classes.
+        # This waits until all attributes are available before generating a name.
+        if self.name == self.id:
+            self.name = self.make_name()
+
     @property
     def mime_type(self) -> str:
         return f"image/{self.format}"
@@ -28,3 +38,9 @@ class ImageArtifact(BlobArtifact):
 
     def to_text(self) -> str:
         return f"Image, format: {self.format}, size: {len(self.value)} bytes"
+
+    def make_name(self) -> str:
+        entropy = "".join(random.choices(string.ascii_lowercase + string.digits, k=4))
+        fmt_time = time.strftime("%y%m%d%H%M%S", time.localtime())
+
+        return f"image_artifact_{fmt_time}_{entropy}.{self.format}"
