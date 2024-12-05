231abe5cd6115299d708a47c5eac9fbda359b474
Collin Dutter <collin@griptape.ai>
2024-12-05 12:58:24 -0800

    Add branch task

--- griptape/tasks/branch_task.py
+++ griptape/tasks/branch_task.py
@@ -1,23 +1,21 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Callable
+from typing import Callable
 
 from attrs import define, field
 
+from griptape.artifacts import EmptyArtifact, InfoArtifact
 from griptape.tasks import CodeExecutionTask
-
-if TYPE_CHECKING:
-    from griptape.artifacts.info_artifact import InfoArtifact
 
 
 @define
 class BranchTask(CodeExecutionTask):
     on_run: Callable[[BranchTask], InfoArtifact] = field(kw_only=True)
 
-    def try_run(self) -> InfoArtifact:
+    def try_run(self) -> InfoArtifact | EmptyArtifact:
         result = self.on_run(self)
 
-        if result.value not in self.child_ids:
+        if isinstance(result, InfoArtifact) and result.value not in self.child_ids:
             raise ValueError(f"Branch result {result.value} not in child_ids {self.child_ids}")
 
         return result
