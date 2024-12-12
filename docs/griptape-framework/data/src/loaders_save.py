f4d26010e6619ee5f161d8b53dbf8ca544fa6001
Collin Dutter <collin@griptape.ai>
2024-12-12 10:20:41 -0800

    Add BaseFileLoader.save()

---
+++ docs/griptape-framework/data/src/loaders_save.py
@@ -0,0 +1,10 @@
+from griptape.drivers import LocalFileManagerDriver
+from griptape.loaders import TextLoader
+
+loader = TextLoader(file_manager_driver=LocalFileManagerDriver())
+
+data = loader.load("tests/resources/test.txt")
+
+data.value = data.value.replace("foo", "bar")
+
+loader.save("tests/resources/test.txt", data)
