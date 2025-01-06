930f343df5f0343cd086278776a254081d5fef5a
Collin Dutter <collindutter@gmail.com>
2025-01-06 10:33:33 -0800

    Remove response_format in docs

--- docs/griptape-framework/drivers/src/prompt_drivers_3.py
+++ docs/griptape-framework/drivers/src/prompt_drivers_3.py
@@ -1,7 +1,5 @@
 import os
 
-import schema
-
 from griptape.drivers import OpenAiChatPromptDriver
 from griptape.structures import Agent
 
@@ -11,14 +9,6 @@ agent = Agent(
         model="gpt-4o-2024-08-06",
         temperature=0.1,
         seed=42,
-        response_format={
-            "type": "json_schema",
-            "json_schema": {
-                "strict": True,
-                "name": "Output",
-                "schema": schema.Schema({"css_code": str, "relevant_emojies": [str]}).json_schema("Output Schema"),
-            },
-        },
     ),
     input="You will be provided with a description of a mood, and your task is to generate the CSS color code for a color that matches it. Description: {{ args[0] }}",
 )
