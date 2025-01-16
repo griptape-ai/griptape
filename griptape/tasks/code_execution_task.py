e87a80e976943a3596d4833513f43b070fb262f2
Collin Dutter <collin@griptape.ai>
2025-01-16 06:51:13 -0800

    WIP

--- griptape/tasks/code_execution_task.py
+++ griptape/tasks/code_execution_task.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 
 import functools
-from typing import Callable, TypeVar
+from typing import Callable, TypeVar, Generic
 
 from attrs import define, field
 from typing_extensions import ParamSpec
 
-from griptape.artifacts import GenericArtifact
+from griptape.artifacts import GenericArtifact, 
 from griptape.artifacts.base_artifact import BaseArtifact
 from griptape.tasks.base_text_input_task import BaseTextInputTask
 
@@ -29,7 +29,7 @@ class CodeExecutionTask(BaseTextInputTask[A]):
         @functools.wraps(func)
         def wrapper(*args: P.args, **kwargs: P.kwargs) -> CodeExecutionTask[GenericArtifact[R]]:
             def on_run(task: CodeExecutionTask[GenericArtifact[R]]) -> GenericArtifact[R]:
-                output = func(**args, **kwargs)
+                output = func(*args, **kwargs)
                 return GenericArtifact(output)
 
             return CodeExecutionTask(id=func.__name__, on_run=on_run)
