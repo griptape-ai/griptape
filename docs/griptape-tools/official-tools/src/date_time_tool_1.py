d99f7a2ff47f7be75353a0a157fbc73e1ca58b46
Collin Dutter <collin@griptape.ai>
2025-01-24 09:13:21 -0800

    Add new DatetimeTool activities (#1611)

--- docs/griptape-tools/official-tools/src/date_time_tool_1.py
+++ docs/griptape-tools/official-tools/src/date_time_tool_1.py
@@ -1,8 +1,6 @@
 from griptape.structures import Agent
 from griptape.tools import DateTimeTool
 
-# Create an agent with the DateTimeTool tool
 agent = Agent(tools=[DateTimeTool()])
 
-# Fetch the current date and time
-agent.run("What is the current date and time?")
+agent.run("What day is 5 days past christmas in 2026?")
--- griptape/tools/date_time/tool.py
+++ griptape/tools/date_time/tool.py
@@ -1,30 +1,32 @@
-from datetime import datetime
+from __future__ import annotations
 
-from schema import Literal, Schema
+from datetime import datetime, timedelta
+from typing import Optional
+
+import schema
+from attrs import Factory, define, field
 
 from griptape.artifacts import BaseArtifact, ErrorArtifact, TextArtifact
 from griptape.tools import BaseTool
 from griptape.utils.decorators import activity
 
 
+@define
 class DateTimeTool(BaseTool):
+    denylist: list[str] = field(default=Factory(lambda: ["get_relative_datetime"]), kw_only=True)
+
     @activity(config={"description": "Can be used to return current date and time."})
     def get_current_datetime(self) -> BaseArtifact:
-        try:
-            current_datetime = datetime.now()
-
-            return TextArtifact(str(current_datetime))
-        except Exception as e:
-            return ErrorArtifact(f"error getting current datetime: {e}")
+        return TextArtifact(str(datetime.now()))
 
     @activity(
         config={
             "description": "Can be used to return a relative date and time.",
-            "schema": Schema(
+            "schema": schema.Schema(
                 {
-                    Literal(
+                    schema.Literal(
                         "relative_date_string",
-                        description='Relative date in English. For example, "now EST", "20 minutes ago", '
+                        description='Relative date in English compatible with the dateparser library. For example, "now EST", "20 minutes ago", '
                         '"in 2 days", "3 months, 1 week and 1 day ago", or "yesterday at 2pm"',
                     ): str,
                 },
@@ -44,3 +46,47 @@ class DateTimeTool(BaseTool):
                 return ErrorArtifact("invalid date string")
         except Exception as e:
             return ErrorArtifact(f"error getting current datetime: {e}")
+
+    @activity(
+        config={
+            "description": "Can be used to add a timedelta to a datetime.",
+            "schema": schema.Schema(
+                {
+                    schema.Literal(
+                        "timedelta_kwargs",
+                        description='A dictionary of keyword arguments to pass to the timedelta function. For example, {"days": -1, "hours": 2}',
+                    ): dict,
+                    schema.Optional(
+                        schema.Literal(
+                            "iso_datetime",
+                            description='Datetime represented as a string in ISO 8601 format. For example, "2021-01-01T00:00:00". Defaults to the current datetime if not provided.',
+                        )
+                    ): str,
+                },
+            ),
+        },
+    )
+    def add_timedelta(self, timedelta_kwargs: dict, iso_datetime: Optional[str] = None) -> BaseArtifact:
+        if iso_datetime is None:
+            iso_datetime = datetime.now().isoformat()
+        return TextArtifact((datetime.fromisoformat(iso_datetime) + timedelta(**timedelta_kwargs)).isoformat())
+
+    @activity(
+        config={
+            "description": "Can be used to calculate the difference between two datetimes. The difference is calculated as end_datetime - start_datetime.",
+            "schema": schema.Schema(
+                {
+                    schema.Literal(
+                        "start_datetime",
+                        description='Datetime represented as a string in ISO 8601 format. For example, "2021-01-01T00:00:00"',
+                    ): str,
+                    schema.Literal(
+                        "end_datetime",
+                        description='Datetime represented as a string in ISO 8601 format. For example, "2021-01-02T00:00:00"',
+                    ): str,
+                }
+            ),
+        }
+    )
+    def get_datetime_diff(self, start_datetime: str, end_datetime: str) -> BaseArtifact:
+        return TextArtifact(str(datetime.fromisoformat(end_datetime) - datetime.fromisoformat(start_datetime)))
