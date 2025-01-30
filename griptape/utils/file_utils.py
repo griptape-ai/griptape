da707905ac90cf9060e416263e061a3e539d0627
Collin Dutter <collin@griptape.ai>
2025-01-29 18:02:41 -0800

    Accept bytes when getting mime type

--- griptape/utils/file_utils.py
+++ griptape/utils/file_utils.py
@@ -1,13 +1,17 @@
+from __future__ import annotations
+
 import mimetypes
 
 import filetype
 
 
-def get_mime_type(file_path: str) -> str:
-    filetype_guess = filetype.guess(file_path)
+def get_mime_type(file_path_or_bytes: str | bytes) -> str:
+    filetype_guess = filetype.guess(file_path_or_bytes)
 
     if filetype_guess is None:
-        type_, _ = mimetypes.guess_type(file_path)
+        if isinstance(file_path_or_bytes, bytes):
+            return "application/octet-stream"
+        type_, _ = mimetypes.guess_type(file_path_or_bytes)
         if type_ is None:
             return "application/octet-stream"
         else:
