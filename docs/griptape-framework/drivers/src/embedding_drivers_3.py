939f56087cb1fe1a4a4a2e2514fdaada773cdbd4
Collin Dutter <collin@griptape.ai>
2025-02-28 14:26:17 -0800

    feat(drivers-embedding-titan): add support for generating image embeddings

--- docs/griptape-framework/drivers/src/embedding_drivers_3.py
+++ docs/griptape-framework/drivers/src/embedding_drivers_3.py
@@ -1,6 +1,14 @@
 from griptape.drivers.embedding.amazon_bedrock import AmazonBedrockTitanEmbeddingDriver
+from griptape.loaders import ImageLoader
 
-embeddings = AmazonBedrockTitanEmbeddingDriver().embed_string("Hello world!")
+embedding_driver = AmazonBedrockTitanEmbeddingDriver()
+embeddings = embedding_driver.embed("Hello world!")
 
-# display the first 3 embeddings
 print(embeddings[:3])
+
+# Some models support images!
+multi_modal_embedding_driver = AmazonBedrockTitanEmbeddingDriver(model="amazon.titan-embed-image-v1")
+image = ImageLoader().load("tests/resources/cow.png")
+image_embeddings = multi_modal_embedding_driver.embed(image)
+
+print(image_embeddings[:3])
