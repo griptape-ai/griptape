90d8e712b10583558788791e836b471bbc31dfe1
Ian Massingham <ian@griptape.ai>
2025-09-29 18:51:55 +0100

    feat: add VideoUrlArtifact to artifacts module (#2005)
    Included VideoUrlArtifact in the artifacts module and updated the __all__ list accordingly.

---
+++ griptape/artifacts/video_url_artifact.py
@@ -0,0 +1,9 @@
+from griptape.artifacts.url_artifact import UrlArtifact
+
+
+class VideoUrlArtifact(UrlArtifact):
+    """Stores a url to a video.
+
+    Attributes:
+        value: The url.
+    """
