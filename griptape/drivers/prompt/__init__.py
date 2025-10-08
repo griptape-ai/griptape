91696786a86bc2e99adf61171e44f87a108d13d5
matt <matt@griptape.ai>
2025-10-08 17:03:41 -0500

    wip

--- griptape/drivers/prompt/__init__.py
+++ griptape/drivers/prompt/__init__.py
@@ -1,3 +1,4 @@
+from .async_base_prompt_driver import AsyncBasePromptDriver
 from .base_prompt_driver import BasePromptDriver
 
-__all__ = ["BasePromptDriver"]
+__all__ = ["AsyncBasePromptDriver", "BasePromptDriver"]
